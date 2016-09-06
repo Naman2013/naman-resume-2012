a9a80b3e5912bb6b22f268b6fee708003445db85
jjmasse <jmasse860@gmail.com>
2016-09-06 16:26:44 -0400

    feat ( styling and functional display logic )

--- app/components/common/announcement-banner.js
+++ app/components/common/announcement-banner.js
@@ -1,15 +1,26 @@
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
 import style from './announcement-banner.scss';
 
 class AnnouncementBanner extends Component {
   render() {
+
+    const displayBannerStyles = classnames({
+      hide: !this.props.display,
+      announcementBanner: true
+    });
+
     return(
-      <div className={style.annoucementBanner}>
+      <div className={displayBannerStyles}>
         <p className={style.announcement}>
           Conditions in the Canary Islands right now are GOOD and look to remain optimal throughout the weekend.
         </p>
-        <button className={style.button}><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+        <button
+          onClick={this.props.closeBanner}
+          className={style.button}>
+            <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+        </button>
       </div>
     );
   }
--- app/pages/telescope-overview.js
+++ app/pages/telescope-overview.js
@@ -3,11 +3,28 @@ import React, { Component, PropTypes } from 'react';
 import AnnouncementBanner from '../components/common/announcement-banner';
 
 class TelescopeOverview extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayBanner: true
+    };
+  }
+
+  closeBanner() {
+    this.setState({
+      displayBanner: false
+    });
+  }
+
   render() {
     return(
       <div>
 
-        <AnnouncementBanner />
+        <AnnouncementBanner
+          display={this.state.displayBanner}
+          closeBanner={this.closeBanner.bind(this)} />
 
       </div>
     );
