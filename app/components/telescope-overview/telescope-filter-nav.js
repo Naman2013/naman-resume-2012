ee1df2d6cc8075ece78dba13e6f6420ce4567632
jjmasse <jmasse860@gmail.com>
2016-09-06 17:04:00 -0400

    feat ( dialing in more of the filter nav )

--- app/components/telescope-overview/telescope-filter-nav.js
+++ app/components/telescope-overview/telescope-filter-nav.js
@@ -17,7 +17,11 @@ class TelescopeFilterNav extends Component {
           <li className="col-md-3 action-container"><a href="" className={style.button}>All Telescopes</a></li>
           <li className="col-md-3 action-container"><a href="" className={style.button}>Canary Island Telescopes</a></li>
           <li className="col-md-3 action-container"><a href="" className={style.button}>Chili Telescopes</a></li>
-          <li className="col-md-3 push-right universal-time"><span className="light-gray">Universal Time:</span><br />01:26:42</li>
+          <li className="col-md-3 push-right universal-time">
+            <span className="light-gray">Universal Time: </span><span className="time"><b>01:26:42</b></span>
+            <br />
+            <a className="time-action pink" href="">What is UTC?</a>
+          </li>
         </ul>
       </div>
     );
