225e845f61a3eaf50711814e584d9e40c2816f01
jjmasse <jmasse860@gmail.com>
2016-09-06 17:08:54 -0400

    feat ( new active state classname )

--- app/components/telescope-overview/telescope-filter-nav.js
+++ app/components/telescope-overview/telescope-filter-nav.js
@@ -5,18 +5,12 @@ import style from './telescope-filter-nav.scss';
 
 class TelescopeFilterNav extends Component {
   render() {
-
-    const navListStyles = classnames({
-      'clearfix': true,
-      'telescopeNavList': true
-    });
-
     return (
       <div className={style.telescopeFilterNav}>
         <ul className="telescopeNavList clearfix">
-          <li className="col-md-3 action-container"><a href="" className={style.button}>All Telescopes</a></li>
-          <li className="col-md-3 action-container"><a href="" className={style.button}>Canary Island Telescopes</a></li>
-          <li className="col-md-3 action-container"><a href="" className={style.button}>Chili Telescopes</a></li>
+          <li className="col-md-3 action-container"><a href="" className="button active">All Telescopes</a></li>
+          <li className="col-md-3 action-container"><a href="" className="button">Canary Island Telescopes</a></li>
+          <li className="col-md-3 action-container"><a href="" className="button">Chili Telescopes</a></li>
           <li className="col-md-3 push-right universal-time">
             <span className="light-gray">Universal Time: </span><span className="time"><b>01:26:42</b></span>
             <br />
