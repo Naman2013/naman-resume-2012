205105ac1bce0a7bccee8822e90f4d1208a163f4
jjmasse <jmasse860@gmail.com>
2016-09-06 16:57:50 -0400

    feat ( adding universal time )

--- app/components/telescope-overview/telescope-filter-nav.js
+++ app/components/telescope-overview/telescope-filter-nav.js
@@ -14,9 +14,10 @@ class TelescopeFilterNav extends Component {
     return (
       <div className={style.telescopeFilterNav}>
         <ul className="telescopeNavList clearfix">
-          <li className="col-md-3 col-md-offset-1"><a href="" className={style.button}>All Telescopes</a></li>
-          <li className="col-md-3"><a href="" className={style.button}>Canary Island Telescopes</a></li>
-          <li className="col-md-3"><a href="" className={style.button}>Chili Telescopes</a></li>
+          <li className="col-md-3 action-container"><a href="" className={style.button}>All Telescopes</a></li>
+          <li className="col-md-3 action-container"><a href="" className={style.button}>Canary Island Telescopes</a></li>
+          <li className="col-md-3 action-container"><a href="" className={style.button}>Chili Telescopes</a></li>
+          <li className="col-md-3 push-right universal-time"><span className="light-gray">Universal Time:</span><br />01:26:42</li>
         </ul>
       </div>
     );
