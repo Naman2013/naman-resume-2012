37b288b8cd0347ec5b7bfb93f626b8fc3fc33c2d
Alex Bachuk <abachuk@gmail.com>
2016-09-07 22:56:58 -0400

    mission-nav file

---
+++ app/components/missions/mission-nav.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+
+class MissionNav extends Component {
+  render() {
+    return (
+      <div>
+        Mission Nav
+      </div>
+    );
+  }
+}
+
+export default MissionNav;
