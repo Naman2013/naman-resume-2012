839b9f84a730ad436437947158d9ef398a88d4d6
Alex Bachuk <abachuk@gmail.com>
2016-09-08 10:27:30 -0400

    utc component

--- app/components/common/universal-time.js
+++ app/components/common/universal-time.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {Component, Props} from 'react';
+import moment from 'moment';
 import styles from './common.scss';
 
-const UniversalTime = (props) => {
+class UniversalTime extends Component {
 
-  let className = `${styles.universalTime} ${props.extraClass || ''}`;
-  return (
-    <div className={className}>
-      <span className="light-gray">Universal Time: </span>
-      <span className="time"><b>01:26:42</b></span>
-      <br />
-      <a className="time-action" href="#">What is UTC?</a>
-    </div>
-  )
+  componentWillMount() {
+    this.setState({time: moment.utc().format('HH:mm:ss')});
+  }
+
+  componentDidMount() {
+    let intervalCounter = setInterval(this.timer.bind(this), 1000);
+    this.setState({intervalCounter});
+  }
+
+  timer() {    
+    this.setState({time: moment.utc().format('HH:mm:ss')});
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalCounter);
+  }
+
+  render() {
+    let className = `${styles.universalTime} ${this.props.extraClass || ''}`;
+    return (
+      <div className={className}>
+        <span className="light-gray">Universal Time: </span>
+        <span className="time"><b>{this.state.time}</b></span>
+        <br />
+        <a className="time-action" href="#">What is UTC?</a>
+      </div>
+    )
+  }
 };
 
 export default UniversalTime;
