60ff16b9d3758652c0058bed887e0b046eccac1c
jjmasse <jmasse860@gmail.com>
2016-09-09 16:18:39 -0400

    fix ( typo )

--- app/components/telescope-overview/telescope-filter-nav.js
+++ app/components/telescope-overview/telescope-filter-nav.js
@@ -10,7 +10,7 @@ class TelescopeFilterNav extends Component {
         <ul className="telescopeNavList clearfix">
           <li className="col-md-3 action-container"><a href="" className="button active">All Telescopes</a></li>
           <li className="col-md-3 action-container"><a href="" className="button">Canary Island Telescopes</a></li>
-          <li className="col-md-3 action-container"><a href="" className="button">Chili Telescopes</a></li>
+          <li className="col-md-3 action-container"><a href="" className="button">Chile Telescopes</a></li>
           <li className="col-md-3 push-right universal-time">
             <span className="light-gray">Universal Time: </span><span className="time"><b>01:26:42</b></span>
             <br />
