1edcd18e23a629c925540424dd1308a62e7516b3
jjmasse <jmasse860@gmail.com>
2016-09-09 23:02:24 -0400

    feat ( framing the back of the card )

--- app/components/telescope-overview/telescope-cards/card-back.js
+++ app/components/telescope-overview/telescope-cards/card-back.js
@@ -1,10 +1,21 @@
 import React, { Component, PropTypes } from 'react';
+import style from './card-back.scss';
 
 class CardBack extends Component {
   render() {
     return(
-      <div>
-        <h2>Card back</h2>
+      <div className="telescope-card-back">
+        <div className="card-header">
+          <img src="../../../assets/icons/observatory.svg" width="50" height="50" />
+          <h3 className="title">Telescope Name</h3>
+        </div>
+
+        <div className="telescope-specs">
+          <figure className="telescope-image">
+            <img src="../../../assets/images/graphics/cluster.png" width="259" height="180" />
+            <figcaption className="caption">Image taken by member Randy Suess on this telescope.</figcaption>
+          </figure>
+        </div>
       </div>
     );
   }
