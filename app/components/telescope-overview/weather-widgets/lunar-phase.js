b3451059e1a590a0f5fd13e42b0b2b370e7de05d
jjmasse <jmasse860@gmail.com>
2016-09-10 15:15:41 -0400

    feat ( starting the styling for lunar phase )

---
+++ app/components/telescope-overview/weather-widgets/lunar-phase.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+class LunarPhase extends Component {
+  render() {
+    return(
+      <div className="weather-widget">
+        <div className="transparent-background weather-view">
+          <h5 className="title-label">Lunar Phase</h5>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LunarPhase;
