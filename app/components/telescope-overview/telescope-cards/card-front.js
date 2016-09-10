c8f07aa93b367ef328372c6545096d294a042352
jjmasse <jmasse860@gmail.com>
2016-09-10 12:34:53 -0400

    feat ( whitespace and offline logic )

--- app/components/telescope-overview/telescope-cards/card-front.js
+++ app/components/telescope-overview/telescope-cards/card-front.js
@@ -5,44 +5,58 @@ class CardFront extends Component {
   render() {
     return(
       <div className="telescope-card-front">
-      <div className="card-header">
-        <button
-          onClick={this.props.handleFlip}
-          className="flip-card-action">
-          <img src="../../../assets/icons/flip-arrow.svg" />
-        </button>
+        <div className="card-header">
+          <button
+            onClick={this.props.handleFlip}
+            className="flip-card-action">
+            <img src="../../../assets/icons/flip-arrow.svg" />
+          </button>
 
-        <img src="../../../assets/icons/observatory.svg" width="50" height="50" />
-        <h3 className="title">High-Magnification 1</h3>
-        <p className="body">
-          Best for Deep Sky Objects such as galaxies, nebulae, and star clusters.
-        </p>
+          <img src="../../../assets/icons/observatory.svg" width="50" height="50" />
+          <h3 className="title">High-Magnification 1</h3>
+          <p className="body">
+            Best for Deep Sky Objects such as galaxies, nebulae, and star clusters.
+          </p>
 
-        <div className="call-to-action clearfix">
-          <a className="col-md-5 action" href="">Visit Telescope Page</a>
-          <a className="col-md-5 action" href="">Make Reservation</a>
-        </div>
-      </div>
-
-      <div className="card-body">
-        <div className="count-down">
-          <h4 className="counter-text">3:18</h4>
-        </div>
-
-        <div className="image-viewer">
-          <h4 className="title">LIVE Mission</h4>
-          <div className="telescope-image">
-            <img src="../../../assets/images/graphics/galaxy-circle.png" width="245" height="245" />
+          <div className="call-to-action clearfix">
+            <a className="col-md-5 action" href="">Visit Telescope Page</a>
+            <a className="col-md-5 action" href="">Make Reservation</a>
           </div>
-          <h5 className="telescope-image-title">
-            Andromeda Galaxy ( M31 )
-          </h5>
         </div>
 
-        <div className="sponsor">
-          <p>Sponsored by: <img src="../../../assets/images/logos/celestron.png" width="150" /></p>
+        <div className="card-body">
+
+          { /* telescope content */
+            !!this.props.telescopeOnline ?
+              <div>
+                <div className="count-down">
+                  <h4 className="counter-text">3:18</h4>
+                </div>
+
+                <div className="image-viewer">
+                  <h4 className="title">LIVE Mission</h4>
+                  <div className="telescope-image">
+                    <img src="../../../assets/images/graphics/galaxy-circle.png" width="245" height="245" />
+                  </div>
+                  <h5 className="telescope-image-title">
+                    Andromeda Galaxy ( M31 )
+                  </h5>
+                </div>
+              </div>
+
+              :
+
+              <div>
+                Offline...
+              </div>
+          }
+
+
+          <div className="sponsor">
+            <p>Sponsored by: <img src="../../../assets/images/logos/celestron.png" width="150" /></p>
+          </div>
+
         </div>
-      </div>
       </div>
     );
   }
@@ -50,6 +64,7 @@ class CardFront extends Component {
 
 CardFront.propTypes = {
   handleFlip: PropTypes.func,
+  telescopeOnline: PropTypes.bool
 };
 
 export default CardFront;
