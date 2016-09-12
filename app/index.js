66b6a1ad645604d89916ca7d2fbdea333a89e1db
jjmasse <jmasse860@gmail.com>
2016-09-12 17:33:15 -0400

    chore ( commenting sections )

--- app/index.js
+++ app/index.js
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, browserHistory, hashHistory } from 'react-router';
 import configureStore from './store';
 
+// containers
 import App from './containers/App';
 
+// pages
 import Home from './pages/home';
 import TelescopeOverview from './pages/telescope-overview';
 import ReserveMissions from './pages/reserve-missions';
 
+// global styles
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import './styles/app.scss';
 import './styles/interface.css';
