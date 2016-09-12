f5e7f0ff12ad343fd8a258044a903aa6897ece40
jjmasse <jmasse860@gmail.com>
2016-09-12 19:30:35 -0400

    fix ( adding keys to the static content )

--- app/content/home.js
+++ app/content/home.js
@@ -10,16 +10,19 @@ export default {
   },
   RECENT_STUFF: [
     {
+      key: 1,
       title: 'BOB BERMAN LIVE',
       content: `Join author and astronomer as he goes live for a full hour, featuring galactic views of Slooh’s scopes this Thurs. at 5 PM EST.`,
       imageUrl: '../../assets/images/samples/bobpubjpg.png'
     },
     {
+      key: 2,
       title: 'CAPTURING A COMET’S TAIL',
       content: `Learn all about how Slooh Members managed to image comet 67P’s long trail—the source of meteor showers.`,
       imageUrl: '../../assets/images/samples/perseid-meteor-shower.png'
     },
     {
+      key: 3,
       title: 'WATCHING',
       content: `This weekend you’ll get a shot at seeing the Perseids at a new level of intensity. Prep is key. Learn all about what you need here.`,
       imageUrl: '../../assets/images/samples/perseids.png'
