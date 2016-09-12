3aa772a772b70d64acf7fc5e7b354924bea0aa1b
jjmasse <jmasse860@gmail.com>
2016-09-12 19:30:23 -0400

    feat ( new recent video tile components )

---
+++ app/components/home/recent-video-tile.js
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react';
+import style from './recent-video-tile.scss';
+
+class RecentVideoTile extends Component {
+  render() {
+    const inlineStyle = {
+      background: `url(${this.props.imageUrl}) no-repeat center center`,
+      backgroundSize: 'cover',
+      minHeight: '255px'
+    };
+    return(
+      <div className="col-md-4">
+        <div style={inlineStyle} className="recent-video-tile">
+          <h4 className="title">{this.props.title}</h4>
+        </div>
+        <p className="content">{this.props.content}</p>
+      </div>
+    );
+  }
+}
+
+RecentVideoTile.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  imageUrl: PropTypes.string.isRequired
+};
+
+export default RecentVideoTile;
