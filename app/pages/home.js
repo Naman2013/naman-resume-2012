0360a8116c39a5ed41da9d186a635fb97adeee75
jjmasse <jmasse860@gmail.com>
2016-09-14 20:02:06 -0400

    feat ( implements message band )

--- app/pages/home.js
+++ app/pages/home.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 
 import Hero from '../components/home/hero';
 import RecentVideoTile from '../components/home/recent-video-tile';
+import PromoMessageBand from '../components/home/promo-message-band';
 
 import CONTENT from '../content/home';
 
@@ -21,9 +22,12 @@ class Home extends Component {
     return(
       <div>
         {this.fetchStaticHero()}
+
         <div className="clearfix">
           {this.generateRecentVideoTiles()}
         </div>
+
+        <PromoMessageBand message={CONTENT.promoBandContent} />
       </div>
     );
   }
