1128596454fef71947bdd6a0e53c5ac454c06b1d
jjmasse <jmasse860@gmail.com>
2016-09-14 20:07:47 -0400

    feat ( new component for slooh features )

---
+++ app/components/home/slooh-features.js
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react';
+
+class SloohFeatures extends Component {
+  render() {
+    return(
+      <div className="col-md-3 slooh-features-container">
+        <img src={this.props.icon} width="50" />
+        <h5 className="title">{this.props.title}</h5>
+        <p className="content">{this.props.content}</p>
+        <a href={this.props.actionUrl}>{this.props.actionText}</a>
+      </div>
+    );
+  }
+}
+
+SloohFeatures.propTypes = {
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  actionUrl: PropTypes.string.isRequired,
+  actionText: PropTypes.string.isRequired
+};
+
+export default SloohFeatures;
