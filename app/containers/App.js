afbc4a9f3c41565bf43af916cc6a581077d4cbc0
jjmasse <jmasse860@gmail.com>
2016-09-18 11:53:28 -0400

    feat ( first cut of the footer )

--- app/containers/App.js
+++ app/containers/App.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Menu from './Menu';
 import Header from './Header';
+import Footer from '../components/common/footer';
 import { checkUser } from '../modules/User';
 
 const { element, func } = PropTypes;
@@ -29,8 +30,11 @@ export default class App extends Component {
         <Header />
         <Menu source="nav.json" />
         <section className="app-content-container clearfix">
+          <div className="clearfix">
           {this.props.children}
+          </div>
         </section>
+        <Footer />
       </div>
     );
   }
