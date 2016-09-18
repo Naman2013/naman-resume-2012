4228a87d5846b7b86619c242459e665058c86d42
jjmasse <jmasse860@gmail.com>
2016-09-18 11:14:35 -0400

    feat ( setup featured )

--- app/components/home/slooh-extras/featured.js
+++ app/components/home/slooh-extras/featured.js
@@ -1,13 +1,20 @@
 import React, { Component, PropTypes } from 'react';
+import SloohExtrasTile from './slooh-extras-tile';
 
 class Featured extends Component {
   render() {
     return(
-      <div className="slooh-addon-tile">
-        <h1>Hey.</h1>
-      </div>
+      <SloohExtrasTile title={this.props.title}>
+        <div className="slooh-addon-tile">
+          <h1>Hey.</h1>
+        </div>
+      </SloohExtrasTile>
     );
   }
 }
+
+Featured.propTypes = {
+  title: PropTypes.string.isRequired
+};
 
 export default Featured;
