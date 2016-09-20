8b80a3db8825273a60affff3a9e73bb562d2fc90
jjmasse <jmasse860@gmail.com>
2016-09-20 19:35:25 -0400

    feat ( implementing redux store, actions and props )

--- app/pages/telescope-overview.js
+++ app/pages/telescope-overview.js
@@ -1,10 +1,29 @@
 import React, { Component, PropTypes } from 'react';
 
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { getObservatoryList } from '../modules/Telescope-Overview';
+
 import AnnouncementBanner from '../components/common/announcement-banner';
 import TelescopeFilterNav from '../components/telescope-overview/telescope-filter-nav';
 import ObservatoryHero from '../components/telescope-overview/observatory-hero';
 import TelescopeCards from '../components/telescope-overview/telescope-cards/telescope-cards';
 
+
+function mapStateToProps(state) {
+  console.log(state);
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      getObservatoryList
+    }, dispatch)
+  };
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 class TelescopeOverview extends Component {
 
   constructor(props) {
@@ -13,6 +32,10 @@ class TelescopeOverview extends Component {
     this.state = {
       displayBanner: true
     };
+  }
+
+  componentDidMount() {
+    this.props.actions.getObservatoryList();
   }
 
   closeBanner() {
