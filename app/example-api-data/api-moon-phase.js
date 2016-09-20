12ff872914f934621b25d35dc1ad8c2bcea3c68a
jjmasse <jmasse860@gmail.com>
2016-09-20 07:32:54 -0400

    chore ( adding example responses for local testing and development )

---
+++ app/example-api-data/api-moon-phase.js
@@ -0,0 +1,22 @@
+//api/moon/phase
+export default {
+  "ver": "v1",
+  "lang": "en",
+  "obsId": "teide",
+  "widgetUniqueId": "13f80160-7e79-11e6-a635-0eb2b1774883",
+  "title": "LUNAR PHASE",
+  "subtitle": "Waning Gibbous Moon (87%)",
+  "credits": "",
+  "phaseText": "Waning Gibbous Moon",
+  "phaseTextIllum": "Waning Gibbous Moon (87%)",
+  "phaseImageURL": "http:\/\/images-moon.slooh.com\/l21n.gif",
+  "illumPercentage": "87",
+  "timestamp": "1474312021",
+  "expiration": "1474315621",
+  "size": "small",
+  "widgetWidth": "170",
+  "widgetHeight": "100",
+  "apiError": "false",
+  "errorCode": "0",
+  "statusCode": "200"
+};
---
+++ app/example-api-data/api-obs-list.js
@@ -0,0 +1,28 @@
+export default {
+  "ver" : "v1",
+  "lang" : "en",
+  "observatoryListType" : "id",
+  "observatoryListCount" : 2,
+  "observatoryListStatus" : "live",
+  "observatoryListTimestamp": 1474159205,
+  "observatoryList" : [
+    {
+    "obsIndex" : 0,
+    "obsId" : "chile",
+    "obsUniqueId" : "3f332115-7908-11e6-a635-0eb2b1774883",
+    "obsStatus" : "live",
+    "obsMenuName" : "Chile Telescopes",
+    "obsName" : "Chile",
+    "debugObservatoriesId" : 1
+    },
+    -{
+    "obsIndex" : 1,
+    "obsId" : "teide",
+    "obsUniqueId" : "d7f673a5-7908-11e6-a635-0eb2b1774883",
+    "obsStatus" : "live",
+    "obsMenuName" : "Canary Islands Telescopes",
+    "obsName" : "Canary Islands",
+    "debugObservatoriesId" : 2
+    }
+  ]
+}
