c64123dff44b9915c4f767e1ffa8769ff8b05c31
jjmasse <jmasse860@gmail.com>
2016-09-21 19:58:23 -0400

    chore ( cleaning up )

--- app/components/telescope-overview/telescope-filter-nav.js
+++ app/components/telescope-overview/telescope-filter-nav.js
@@ -8,39 +8,21 @@ import style from './telescope-filter-nav.scss';
 class TelescopeFilterNav extends Component {
 
   generateDynamicFilters() {
-
-    return this.props.observatoryList.map( observatory =>  (
-      <FilterLink {...observatory} />
+    return this.props.observatoryList.map( (observatory, index) =>  (
+      <FilterLink key={index} {...observatory} />
     ));
-
-    return null
-    // return this.props.observatoryList
-    //   .map(observatoryFilter =>
-    //     <li
-    //       className="col-md-3 action-container">
-    //         <a href={`/#telescope-overview/${observatoryFilter.obsUniqueId}`} className="button">
-    //           { observatoryFilter.obsMenuName }
-    //         </a>
-    //     </li>);
   }
 
   render() {
-
-    console.log();
-    console.log(this.props);
 
     return (
       <div className={style.telescopeFilterNav}>
         <ul className="telescopeNavList clearfix">
 
           <li className="col-md-3 action-container"><a href="" className="button active">All Telescopes</a></li>
+
           {this.generateDynamicFilters()}
-          {
-            /*
-              <li className="col-md-3 action-container"><a href="" className="button">Canary Island Telescopes</a></li>
-              <li className="col-md-3 action-container"><a href="" className="button">Chile Telescopes</a></li>
-            */
-          }
+
           <li className="col-md-3 push-right universal-time">
             <UniversalTime extraClass="telescope-filters-timer" />
           </li>
