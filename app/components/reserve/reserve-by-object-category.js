21e3d7a19fb8b82eb5d6cfcc731952ce334ab5d6
Alex Bachuk <abachuk@gmail.com>
2016-09-27 09:18:32 -0400

    adding columns

---
+++ app/components/reserve/reserve-by-object-category.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReserveObjectsCategory = (props) => {
+  return (
+    <div className="object-categories">
+      Categories
+    </div>
+  )
+}
+
+export default ReserveObjectsCategory;
--- app/pages/reserve/reserve-by-objects.js
+++ app/pages/reserve/reserve-by-objects.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
+import ReserveObjectsCategory from '../../components/reserve/reserve-by-object-category';
 
 class ReserveObjects extends Component {
   render() {
     return (
-      <div>Objects</div>
+      <div className="reserve-by-object-page">
+        <div className="row">
+          <div className="col-md-4">
+            <h2><span>1</span> Select Category</h2>
+            <ReserveObjectsCategory />
+          </div>
+          <div className="col-md-4">
+            <h2><span>2</span> Choose Specific Object</h2>
+            <ReserveObjectsCategory />
+          </div>
+          <div className="col-md-4">
+            <h2><span>3</span> Object Summary</h2>
+            <ReserveObjectsCategory />
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
