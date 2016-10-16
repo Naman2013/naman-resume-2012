d83e5e788adb50430d4cfcffb42a30f90c196356
jjmasse <jmasse860@gmail.com>
2016-10-16 13:46:21 -0400

    feat ( adding subtitles to promo message band )

--- app/components/common/headers/promo-message-band.js
+++ app/components/common/headers/promo-message-band.js
@@ -6,13 +6,15 @@ class PromoMessageBand extends Component {
     return(
       <div className="promo-message-band">
         <h4 className="title">{this.props.message}</h4>
+        <h5 className="sub-title">{this.props.subtitle}</h5>
       </div>
     );
   }
 }
 
 PromoMessageBand.propTypes = {
-  message: PropTypes.string
+  message: PropTypes.string,
+  subtitle: PropTypes.string,
 };
 
 export default PromoMessageBand;
