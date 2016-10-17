b4f6b0d3c7153801dfae7d791f5d9a9ffede1d1d
jjmasse <jmasse860@gmail.com>
2016-10-17 18:44:53 -0400

    refactor ( binding events and delegating this work to the viewer )

---
+++ app/components/common/interactive-viewer/interactive-viewer.js
@@ -0,0 +1,80 @@
+import React, { Component, PropTypes } from 'react';
+import style from './interactive-viewer.scss';
+
+class InteractivePanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentX: 0,
+      currentY: 0,
+      currentZ: 0,
+    };
+  }
+
+  /** control api */
+  zoom(factor) {
+    this.setState({
+      currentZ: 0,
+    });
+  }
+
+  pan(factor) {
+    this.setState({
+      currentX: 0,
+      currentY: 0,
+    });
+  }
+
+  /** event api's */
+  handleMouseScroll(event) {
+    event.preventDefault();
+    // TODO: zoom in or zoom out by step of scroll
+  }
+
+  handleDrag(event) {
+    // TODO: control panning
+    console.log(event);
+  }
+
+  handleOnMouseEnter(event) {
+    console.log('mouse enter occurred');
+  }
+
+  handleOnMouseLeave(event) {
+    console.log('mouse left... farewell.');
+  }
+
+  render() {
+
+    const { children } = this.props;
+
+      /* mouseScroll={this.handleMouseScroll.bind(this)}
+      mouseDown={this.handleMouseDown.bind(this)}
+      mouseEnter={this.handleMouseEnter.bind(this)}
+      mouseLeave={this.handleMouseLeave.bind(this)} */
+
+    return(
+      <div
+        onDrag={this.handleDrag.bind(this)}
+        onMouseEnter={this.handleOnMouseEnter.bind(this)}
+        onMouseLeave={this.handleOnMouseLeave.bind(this)}
+        className="interactive-panel">
+
+        <div className="icons">
+          <img src={'/assets/images/icons/icon-magnification-minus.png'} className="icon minus" />
+          <img src={'/assets/images/icons/icon-magnification-plus.png'} className="icon plus" />
+          <img src={'/assets/images/icons/icon-snapshot.png'} className="icon snapshot" />
+          <img src={'/assets/images/icons/icon-circular-view.png'} className="icon circular-view" />
+          <img src={'/assets/images/icons/icon-screen-view.png'} className="icon screen-view" />
+        </div>
+
+        <div className="viewer-content">
+          {children}
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default InteractivePanel;
