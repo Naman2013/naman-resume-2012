f9b9686a812a553584e5df947a2fd9109f0adb56
jjmasse <jmasse860@gmail.com>
2016-10-17 08:56:56 -0400

    feat ( stubbing out interactive panel wrapper component )

---
+++ app/components/common/interactive-panel/interactive-panel.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+class InteractivePanel extends Component {
+  render() {
+    return(
+      <div className="interactive-panel">
+
+      </div>
+    );
+  }
+}
+
+export default InteractivePanel;
