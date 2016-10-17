96a9ec403f60bc4dc4153e1e6a1861ae62ce7148
jjmasse <jmasse860@gmail.com>
2016-10-17 09:04:25 -0400

    feat ( roughing out interactive panel API )

--- app/components/common/interactive-panel/interactive-panel.js
+++ app/components/common/interactive-panel/interactive-panel.js
@@ -1,9 +1,48 @@
 import React, { Component, PropTypes } from 'react';
 
 class InteractivePanel extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentX: 0,
+      currentY: 0,
+      currentZ: 0,
+    };
+  }
+
+  /**
+    api
+  */
+  zoomIn(factor) {}
+
+  zoomOut(factor) {}
+
+  pan(factor) {}
+
+  /**
+    event api's
+  */
+  handleMouseScroll(event) {
+    event.preventDefault();
+    // TODO: zoom in or zoom out by step of scroll
+  }
+
+  handleMouseDown(event) {
+    // TODO: control panning
+  }
+
+  handleMouseUp(event) {
+    // TODO: tear down panning?
+  }
+
   render() {
     return(
-      <div className="interactive-panel">
+      <div
+        mouseScroll={this.handleMouseScroll.bind(this)}
+        mouseDown={this.handleMouseDown.bind(this)}
+        mouseUp={this.handleMouseUp.bind(this)}
+        className="interactive-panel">
 
       </div>
     );
