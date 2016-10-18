7e8c47b1a40586f57e1996f0c859a4abcd4787fa
jjmasse <jmasse860@gmail.com>
2016-10-17 21:58:56 -0400

    feat ( progress on zoom functionality )

--- app/components/common/interactive-viewer/interactive-viewer.js
+++ app/components/common/interactive-viewer/interactive-viewer.js
@@ -1,24 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 import style from './interactive-viewer.scss';
 
+const ZOOM_MULTIPLIER = 0.5;
+
 class InteractivePanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentX: 0,
       currentY: 0,
-      currentZ: 0,
+      currentScale: 1,
       enableMove: false,
     };
   }
 
   /** control api */
-  zoom(factor) {
-    this.setState({
-      currentZ: 0,
-    });
-  }
-
   pan(factor) {
     this.setState({
       currentX: 0,
@@ -27,8 +23,14 @@ class InteractivePanel extends Component {
   }
 
   /** event api's */
+  handleZoomClick(event) {
+    const { currentScale } = this.state;
+    this.setState({
+      currentScale: currentScale + ZOOM_MULTIPLIER,
+    });
+  }
+
   handleMouseDown(event) {
-    // TODO: control panning
     this.setState({
       enableMove: true,
     });
@@ -50,28 +52,40 @@ class InteractivePanel extends Component {
   render() {
 
     const { children } = this.props;
+    const { currentScale } = this.state;
+
+    const viewerContentStyle = {
+      'transform': `scale(${currentScale})`,
+      'transformStyle': 'flat',
+    };
 
     return(
-      <div
-        onMouseDown={this.handleMouseDown.bind(this)}
-        onMouseUp={this.handleMouseUp.bind(this)}
-        onMouseMove={this.handleMouseMove.bind(this)}
-        className="interactive-panel">
+      <div className="interactive-viewer-container">
 
         <div className="icons">
-
           <img src={'/assets/images/icons/icon-magnification-minus.png'} className="icon minus" />
-          <div className="icon plus"><span className="icon glyphicon-plus"></span></div>
-
+          <button
+            onClick={this.handleZoomClick.bind(this)}
+            className="icon plus">
+              <span className="icon glyphicon-plus"></span>
+          </button>
           <img src={'/assets/images/icons/icon-snapshot.png'} className="icon snapshot" />
           <img src={'/assets/images/icons/icon-circular-view.png'} className="icon circular-view" />
           <img src={'/assets/images/icons/icon-screen-view.png'} className="icon screen-view" />
         </div>
 
-        <div className="viewer-content">
-          {children}
-        </div>
+        <div
+          onMouseDown={this.handleMouseDown.bind(this)}
+          onMouseUp={this.handleMouseUp.bind(this)}
+          onMouseMove={this.handleMouseMove.bind(this)}
+          className="interactive-panel">
 
+          <div
+            style={viewerContentStyle}
+            className="viewer-content">
+            {children}
+          </div>
+        </div>
       </div>
     );
   }
