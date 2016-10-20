b61b426e1605b10d5a58a6d21eb4859da414ebcb
jjmasse <jmasse860@gmail.com>
2016-10-20 08:47:49 -0400

    feat ( introducing the post tile )

---
+++ app/pages/best-of-slooh/blog-post-tile.js
@@ -0,0 +1,62 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import style from './blog-post-tile.scss';
+
+const BlogPostTile = ({
+  topic,
+  topicSymbol,
+  title,
+  coverPhoto,
+  relatedObject,
+  relatedObjectUniqueId,
+  relatedObjectIcon,
+  user }) => {
+
+  const postTileStyle = {
+    backgroundImage: `url(${coverPhoto})`,
+  };
+
+  return (
+    <div
+      style={postTileStyle}
+      className="slooh-blog-post-tile col-md-6">
+
+        <div className="topic-heading">
+          <h3>
+            <img src={topicSymbol} width="50" /> {topic}
+          </h3>
+        </div>
+
+        <div className="tile-body">
+          <h2 className="title">{title}</h2>
+
+          <div className="user-profile-snapshot">
+            <div className="col-md-2">
+              <img src={user.photo} />
+            </div>
+
+            <div className="col-md-10">
+              <h4 className="username">
+                {user.name} <span className="account-level">${user.accountType}</span>
+              </h4>
+            </div>
+
+            <div>
+              See more about <Link to="">{relatedObject} <img src={relatedObjectIcon} width="30" /></Link>
+            </div>
+          </div>
+        </div>
+
+    </div>
+  );
+};
+
+BlogPostTile.propTypes = {
+  topic: PropTypes.string,
+  topicSymbol: PropTypes.string,
+  title: PropTypes.string,
+  coverPhoto: PropTypes.string,
+  user: PropTypes.object,
+};
+
+export default BlogPostTile;
