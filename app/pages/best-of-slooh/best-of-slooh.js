349db93cf305db4a3db907fb4b942e24e050cfc9
jjmasse <jmasse860@gmail.com>
2016-10-20 08:47:38 -0400

    feat ( painting posts based on content )

--- app/pages/best-of-slooh/best-of-slooh.js
+++ app/pages/best-of-slooh/best-of-slooh.js
@@ -1,14 +1,22 @@
 import React, { Component, PropTypes } from 'react';
+import BlogPostTile from './blog-post-tile';
+import CONTENT from '../../content/best-of-slooh';
 
 class BestOfSlooh extends Component {
   render() {
     return(
-      <div className="">
-        Best of Slooh...
+      <div className="best-of-slooh">
+        {
+          this.props.blogPosts.map(post => (
+            <BlogPostTile {...post} />
+          ))
+        }
       </div>
     );
   }
 }
+
+BestOfSlooh.defaultProps = CONTENT;
 
 BestOfSlooh.propTypes = {
   blogPosts: PropTypes.array,
