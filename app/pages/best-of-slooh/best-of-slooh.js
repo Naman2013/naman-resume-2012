3b2a495f2954a7fe022b55c61adcbf4d41792c25
jjmasse <jmasse860@gmail.com>
2016-10-20 08:53:23 -0400

    feat ( tiling the tiles )

--- app/pages/best-of-slooh/best-of-slooh.js
+++ app/pages/best-of-slooh/best-of-slooh.js
@@ -1,11 +1,15 @@
 import React, { Component, PropTypes } from 'react';
-import BlogPostTile from './blog-post-tile';
+import BlogPostTile from '../../components/common/blog-post-tile/blog-post-tile';
+
+import style from './best-of-slooh.scss';
 import CONTENT from '../../content/best-of-slooh';
+
+
 
 class BestOfSlooh extends Component {
   render() {
     return(
-      <div className="best-of-slooh">
+      <div className="best-of-slooh col-md-12">
         {
           this.props.blogPosts.map(post => (
             <BlogPostTile {...post} />
