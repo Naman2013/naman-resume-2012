0abff91c131bbe3fdae63feb71b0c9172d92c937
jjmasse <jmasse860@gmail.com>
2016-10-21 17:48:39 -0400

    feat ( some destructuring and documenting )

--- app/components/common/by-user-tag/by-user-tag.js
+++ app/components/common/by-user-tag/by-user-tag.js
@@ -1,10 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import style from './by-user-tag.scss';
 
+/**
+  THEMES
+  By default 'dark' is the default theme
+  provide any of the following theme strings to control the style for your needs
+
+  dark ( good for sitting on top of light colored things )
+  light ( good for sitting on top of dark colored things )
+*/
+
 class ByUserTag extends Component {
-  const variationStyle = {
-    
-  };
+
+  const {
+    version,
+    photo,
+    name,
+    accountType,
+    city,
+    state,
+    country,
+    memberSince } = this.props;
+
   render() {
     return(
       <div className="slooh-by-user-tag">
@@ -24,8 +41,12 @@ class ByUserTag extends Component {
   }
 }
 
+ByUserTag.defaultProps = {
+  version: 'dark'
+};
+
 ByUserTag.propTypes = {
-  version: PropTypes.string,
+  theme: PropTypes.string,
   photo: PropTypes.string,
   name: PropTypes.string,
   accountType: PropTypes.string,
