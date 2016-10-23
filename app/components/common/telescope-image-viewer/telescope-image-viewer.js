4d7e65d33abd9cb002eec301d342a1bf7929f0a7
jjmasse <jmasse860@gmail.com>
2016-10-23 18:29:36 -0400

    doc ( adding todos )

--- app/components/common/telescope-image-viewer/telescope-image-viewer.js
+++ app/components/common/telescope-image-viewer/telescope-image-viewer.js
@@ -2,6 +2,12 @@ import React, { Component, PropTypes } from 'react';
 import InteractiveViewer from '../interactive-viewer/interactive-viewer';
 import style from './telescope-image-viewer.scss';
 
+/**
+  TODO: implement sse images
+  TODO: implement telescope video feed
+  TODO: determine whether to display image or video
+*/
+
 class HighMagnification extends Component {
   render() {
     return(
