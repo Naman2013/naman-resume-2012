ddbe13450bbd490effdaf6f18354614015df886d
jjmasse <jmasse860@gmail.com>
2016-10-23 18:34:29 -0400

    chore ( working on analyzing the data coming into the image viewer component )

--- app/components/common/telescope-image-viewer/telescope-image-viewer.js
+++ app/components/common/telescope-image-viewer/telescope-image-viewer.js
@@ -1,8 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import InteractiveViewer from '../interactive-viewer/interactive-viewer';
+import TelescopeImageLoader from '../telescope-image-loader/telescope-image-loader';
 import style from './telescope-image-viewer.scss';
 
 /**
+  TODO: pass in and validate the current telescope data
   TODO: implement sse images
   TODO: implement telescope video feed
   TODO: determine whether to display image or video
@@ -10,12 +12,18 @@ import style from './telescope-image-viewer.scss';
 
 class HighMagnification extends Component {
   render() {
+
+    console.log('the current telescope is...');
+    console.log(this.props);
+
+    // test image...
+    //<img width="100%" src={'/assets/images/graphics/magnification-scene.jpg'}  />
     return(
       <div
         className={`telescope-image-viewer ${this.props.className}`}>
 
         <InteractiveViewer>
-          <img width="100%" src={'/assets/images/graphics/magnification-scene.jpg'}  />
+          <TelescopeImageLoader />
         </InteractiveViewer>
 
       </div>
