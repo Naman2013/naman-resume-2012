2dcd07986ebca341a0a3197e5170cf23ef6f7683
jjmasse <jmasse860@gmail.com>
2016-10-23 16:11:59 -0400

    feat ( new header type )

--- app/components/common/headers/object-title-bar.js
+++ app/components/common/headers/object-title-bar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from './object-title-bar.scss';
 
 const ObjectTitleBar = ({ icon, title, telescopeUniqueId, objectUniqueId }) => {
 
--- app/pages/object-post/object-post.js
+++ app/pages/object-post/object-post.js
@@ -3,6 +3,7 @@ import style from './object-post.scss';
 
 import AnnouncementBanner from '../../components/common/announcement-banner/announcement-banner';
 import ObjectTitleBar from '../../components/common/headers/object-title-bar';
+import ByUserTag from '../../components/common/by-user-tag/by-user-tag';
 
 import ContentMainImage from '../../components/object-post/content-main-image';
 
@@ -10,17 +11,23 @@ class ObjectPost extends Component {
   render() {
     return(
       <div className="slooh-object-post clearfix">
-        <AnnouncementBanner />
+        <AnnouncementBanner obsId={`teide`} />
 
         <ObjectTitleBar />
 
         { /* left column */ }
         <div className="col-xs-8">
+
           <ContentMainImage
             imageSource={`foo.jpg`}
             authorName={`Sarah Blake`} />
 
           <h2 className="post-title">My new Gemini painting is finally finished!</h2>
+
+          <div className="col-xs-6">
+            <ByUserTag theme={`light`} />
+          </div>
+
         </div>
 
 
