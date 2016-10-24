26ca1d395eb72d4418d3044d915efc6eb4af2c5e
jjmasse <jmasse860@gmail.com>
2016-10-23 20:30:17 -0400

    feat ( applying clipping )

--- app/components/common/telescope-image-viewer/telescope-image-viewer.js
+++ app/components/common/telescope-image-viewer/telescope-image-viewer.js
@@ -11,33 +11,53 @@ import generateSseImageLoader from '../../../utils/generate-sse-image-source';
 */
 
 class TelescopeImageViewer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clipped: false,
+    };
+  }
+
+  toggleClipping(event) {
+    const { clipped } = this.state;
+    this.setState({
+      clipped: !clipped,
+    });
+  }
+
   render() {
 
-    console.log('the current telescope is...');
-    console.log(this.props);
+    // console.log('the current telescope is...');
+    // console.log(this.props);
 
     const {
       telePort,
       teleSystem,
       teleId,
-      teleFade,
-      clipped } = this.props;
+      teleFade, } = this.props;
+
+    const { clipped } = this.state;
 
     const teleThumbWidth = '875';
     const imageSource = generateSseImageLoader(teleSystem, telePort);
+    const isClipped = clipped ? 'clipped' : '';
+
+    console.log(isClipped);
 
     return(
       <div
-        className={`telescope-image-viewer ${this.props.className}`}>
+        className={`telescope-image-viewer ${isClipped} ${this.props.className}`}>
 
-        <InteractiveViewer>
+        <InteractiveViewer
+          handleClipping={this.toggleClipping.bind(this)}>
 
           <TelescopeImageLoader
             imageSource={imageSource}
             teleId={teleId}
             teleThumbWidth={teleThumbWidth}
             teleFade={teleFade}
-            clipped={clipped} />
+            clipped={false} />
 
         </InteractiveViewer>
 
