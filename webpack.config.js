e6ff9d5cbb9223d92f9255d7b89f1c16ea36afc3
jjmasse <jmasse860@gmail.com>
2016-10-30 19:34:28 -0400

    chore ( more proxyyyyy )

--- webpack.config.js
+++ webpack.config.js
@@ -170,6 +170,12 @@ module.exports = {
         secure: true,
         pathRewrite: { '/:3104/': '' },
       },
+      '/:3001/**': {
+        target: 'https://mars.slooh.com:3001',
+        changeOrigin: true,
+        secure: true,
+        pathRewrite: { '/:3001/': '' },
+      },
       '/util/**': {
         target: 'https://mars.slooh.com',
         changeOrigin: true,
