33b753f893074641f5d0e43c1dd1c144523d1125
jjmasse <jmasse860@gmail.com>
2016-11-01 07:45:48 -0400

    chore ( adding telescope offline component )

---
+++ app/components/telescope-details/telescope-offline/telescope-offline.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './telescope-offline.scss';
+
+const TelescopeOffline = ({ imageSource }) => (
+  <div>
+    <h2>Telescope is offline...</h2>
+  </div>
+);
+
+export default TelescopeOffline;
