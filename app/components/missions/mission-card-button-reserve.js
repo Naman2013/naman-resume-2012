94e67b0b835451b2155ce18285e9b2c3332114dc
jjmasse <jmasse860@gmail.com>
2016-11-10 20:24:53 -0500

    refactor ( refactoring much of this reserve mission component, this is marked for deprecation )

--- app/components/missions/mission-card-button-reserve.js
+++ app/components/missions/mission-card-button-reserve.js
@@ -1,15 +1,43 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 import styles from './mission-card.scss';
 
-const MissionCardButtonReserve = (props) => {
+class MissionCardButtonReserve extends Component {
 
-  return (
-    <a
-        className={styles.piggybackCta}
-        href=""
-        onClick={event => props.openModal(props.card, 'reserve', event)}
-      >Reserve</a>
-  )
+  constructor(props) {
+    super(props);
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+  }
+
+  handleOpenModal(event) {
+    event.preventDefault();
+    const { openModal, card } = this.props;
+    openModal(card, 'reserve', event);
+
+    // TODO: call getNextPiggyback
+    // resolve the promise, and toggle the result
+
+    // TODO: on result of the API, either open the modal, OR open the "login or signup modal"
+  }
+
+  render() {
+    const { openModal, card } = this.props;
+
+    return(
+      <Link
+          className={ styles.piggybackCta }
+          to="#"
+          onClick={ this.handleOpenModal }>
+          Reserve
+      </Link>
+    );
+  }
 }
+
+MissionCardButtonReserve.propTypes = {
+  card: PropTypes.object,
+  openModal: PropTypes.func,
+};
 
 export default MissionCardButtonReserve;
