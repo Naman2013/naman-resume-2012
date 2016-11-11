6520846ec117cdb6e5056093ee04619391dcb2b3
aneudy1702 <elrey1702@gmail.com>
2016-11-10 20:24:48 -0500

    fix(#133899511) fixed path where sky

--- app/components/telescope-details/where-sky/where-sky.js
+++ app/components/telescope-details/where-sky/where-sky.js
@@ -23,10 +23,10 @@ class TelescopeWhereSky extends React.Component {
           <p>Zoom and pan to get a sense of where we’re pointing this thing.</p>
         </div>
         <div className="content">
-          <img src={'/assets/images/graphics/starfield.png'} className="main-image" />
-          <img src={'/assets/images/icons/icon-magnification-plus.png'} className="zoom-in" />
-          <img src={'/assets/images/icons/icon-magnification-minus.png'} className="zoom-out" />
-          <img src={'/assets/images/icons/icon-screen-view-no-text.png'} className="full-view" />
+          <img src={'assets/images/graphics/starfield.png'} className="main-image" />
+          <img src={'assets/images/icons/icon-magnification-plus.png'} className="zoom-in" />
+          <img src={'assets/images/icons/icon-magnification-minus.png'} className="zoom-out" />
+          <img src={'assets/images/icons/icon-screen-view-no-text.png'} className="full-view" />
         </div>
       </div>
     );
