4658821608326f915d173ed77f71625c7b32a6fc
aneudy1702 <elrey1702@gmail.com>
2016-11-10 20:47:47 -0500

    fix(#133899511) fixed path social icons

--- app/components/menu/MenuSocial.js
+++ app/components/menu/MenuSocial.js
@@ -5,10 +5,10 @@ export default class MenuSocial extends Component {
     return (
       <div>
         <div className="menu-social-icons">
-          <p><a><img src="../../../assets/icons/menu/social-fb.png" /></a></p>
-          <p><a><img src="../../../assets/icons/menu/social-twitter.png" /></a></p>
-          <p><a><img src="../../../assets/icons/menu/social-google.png" /></a></p>
-          <p><a><img src="../../../assets/icons/menu/social-instagram.svg" /></a></p>
+          <p><a><img src="assets/icons/menu/social-fb.png" /></a></p>
+          <p><a><img src="assets/icons/menu/social-twitter.png" /></a></p>
+          <p><a><img src="assets/icons/menu/social-google.png" /></a></p>
+          <p><a><img src="assets/icons/menu/social-instagram.svg" /></a></p>
         </div>
       </div>
     );
