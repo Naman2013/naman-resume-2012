d58e92e7eeb75208a65a744a483dc8c52c48d397
jjmasse <jmasse860@gmail.com>
2016-11-12 08:57:31 -0500

    feat ( new api call to get piggyback )

--- app/modules/Piggyback.js
+++ app/modules/Piggyback.js
@@ -2,6 +2,12 @@ import createReducer from './utils/createReducer';
 import createAction from './utils/createAction';
 import axios from 'axios';
 
-export function grabPiggyback() {
-  return;
+/**
+  see https://docs.google.com/document/d/1nYo6_O87gWCqyoD3NJ98cbA5Cpxo-8ksB3Dw3PbjAa0/
+  for /api/reservation/grabPiggyback documentation
+*/
+export function grabPiggyback(mission) {
+  const { cid, at, token } = user;
+
+  return axios.post('/api/reservation/grabPiggyback', mission);
 }
