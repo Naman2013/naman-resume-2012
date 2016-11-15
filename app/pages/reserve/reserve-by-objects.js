dc2fa6d6ad3487ee1a565934e3ee37f8865b1397
aneudy1702 <elrey1702@gmail.com>
2016-11-14 22:49:39 -0500

    added click handlers

--- app/pages/reserve/reserve-by-objects.js
+++ app/pages/reserve/reserve-by-objects.js
@@ -4,17 +4,25 @@ import styles from '../../components/reserve/reserve-by-object.scss';
 import testData from './reserve-by-objects-data.js'
 
 class ReserveObjects extends Component {
+
+  onItemClick(itemType) {
+    return (item) => {
+      console.log('clicked: ', item);
+      // TODO: find out how to update the state. redux?
+    }
+  }
+
   render() {
     return (
       <div className={styles.reserveObjectPage}>
         <div className="row">
           <div className="col-md-4">
             <h2><span>1</span> Select Category</h2>
-            <ReserveObjectsCategory />
+            <ReserveObjectsCategory items={testData.categories} onClickHandler={this.onItemClick('category')}/>
           </div>
           <div className="col-md-4">
             <h2><span>2</span> Choose Specific Object</h2>
-            <ReserveObjectsCategory />
+            <ReserveObjectsCategory onClickHandler={this.onItemClick('object')} />
           </div>
           <div className="col-md-4">
             <h2><span>3</span> Object Summary</h2>
