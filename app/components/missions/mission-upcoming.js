f39d282e8574b875547aea3e3b3b93d2231b4a4d
aneudy1702 <elrey1702@gmail.com>
2016-11-14 19:21:15 -0500

    added default data for upcoming mission to illustrate its schema

--- app/components/missions/mission-upcoming.js
+++ app/components/missions/mission-upcoming.js
@@ -6,13 +6,13 @@ const getContent = (upcomingMission) => {
     return (
       <div>
         <div className="cardsubTitle">
-          <img className={styles.cardIcon} src="assets/icons/Jupiter.svg" />
-          <h3>Andromeda Galaxy (M31)</h3>
+          <img className={styles.cardIcon} src={upcomingMission.missionIcon} />
+          <h3>{upcomingMission.missionTitle}</h3>
         </div>
 
         <div className="upcoming-mission-date">
-          <strong>Thursday, October 18th</strong><br />
-          10:05pm EST  ·  7:05pm PST  ·  03:05 UTC
+          <strong>{upcomingMission.missionDate}</strong><br />
+          {upcomingMission.missionTime}
         </div>
 
         <a href="#" className="btn btn-primary">View Reservations</a>
@@ -31,7 +31,7 @@ const getWidgetContent = (upcomingMission, user) => {
         <h2>{user.name}&rsquo;s Upcoming Mission</h2>
       </div>
       {
-        getContent() 
+        getContent(upcomingMission) 
       }      
     </div>      
   );
@@ -39,7 +39,12 @@ const getWidgetContent = (upcomingMission, user) => {
 
 const MissionUpcoming = (props) => {
   const {
-    upcomingMission,
+    upcomingMission = {
+      missionTitle: 'Andromeda Galaxy (M31)',
+      missionIcon: 'assets/icons/Jupiter.svg',
+      missionDate: 'Thursday, October 18th',
+      missionTime: '10:05pm EST  ·  7:05pm PST  ·  03:05 UTC'
+    },
     user = {
       name: 'Peter',
       avatarUrl: 'assets/images/graphics/peter.jpg'
