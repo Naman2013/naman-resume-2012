c40a12ff206ccd9b3cbf46d91a07337976300656
aneudy1702 <elrey1702@gmail.com>
2016-11-16 05:03:52 -0500

    added object summary placeholder

---
+++ app/components/reserve/reserve-by-object-summary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './reserve-by-object.scss';
+
+const ReserveObjectsCategory = (props) => {
+  return (
+    <div className={styles.objectCategories}>
+      Summary...
+    </div>
+  );
+}
+
+export default ReserveObjectsCategory;
