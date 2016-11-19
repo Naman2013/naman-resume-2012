5de7e8df62652f2f5ce6b5b0057ca2033b4eb604
aneudy1702 <elrey1702@gmail.com>
2016-11-19 12:49:34 -0500

    refactored ReserveObjectsCategory to handle selected state

--- app/components/reserve/reserve-by-object-category.js
+++ app/components/reserve/reserve-by-object-category.js
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import _ from 'lodash';
 import styles from './reserve-by-object.scss';
+import classnames from 'classnames';
 
-const ReserveObjectsCategory = ({ items = [], onClickHandler }) => {
-  return (
-    <div className={styles.objectCategories}>
-      <ul>
-        {
-          _.map(items, (item, i) => {
-            return (
-              <li key={i} onClick={onClickHandler(item)} className="item">
-                <img className="icon" src={item.categoryIcon} /> {item.title}
-              </li>   
-            );
-          })
-        }
-      </ul>
-    </div>
-  )
+
+class ReserveObjectsCategory extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { selectedCategory: {} };
+  }
+
+  handleClickEvent(item) {
+    const preparedHandler = this.props.onClickHandler(item);
+    
+    return (evt) => {
+      this.setState({
+        selectedCategory: item
+      });            
+
+      preparedHandler(evt);
+    }    
+  }
+
+  render() {
+    const { items = [] } = this.props;
+    const { selectedCategory } = this.state;
+
+    return (
+      <div className={styles.objectCategories}>
+        <ul>
+          {
+            _.map(items, (item, i) => {
+              
+              // TODO: replace selectedCategory.title === item.title with id comparisons
+              const elementsStyles = classnames({
+                item: true,
+                selected: selectedCategory.title === item.title
+              });
+              
+              return (
+                <li key={i} onClick={this.handleClickEvent(item)} className={elementsStyles}>
+                  <img className="icon" src={item.categoryIcon} /> {item.title}
+                </li>   
+              );
+            })
+          }
+        </ul>
+      </div>
+    );
+  }  
 }
 
 export default ReserveObjectsCategory;
