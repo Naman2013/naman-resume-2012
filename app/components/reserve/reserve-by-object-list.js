3c871166b503513e98c61a3c3d5f1201a96db7cd
aneudy1702 <elrey1702@gmail.com>
2016-11-19 12:50:22 -0500

    refactored ReserveObjectsList to handle selected state

--- app/components/reserve/reserve-by-object-list.js
+++ app/components/reserve/reserve-by-object-list.js
@@ -1,32 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
 import _ from 'lodash';
 import styles from './reserve-by-object.scss';
+import classnames from 'classnames';
 
-const ReserveObjectsList = ({ objects = [], onClickHandler }) => {
-  return (
-    <div className={styles.objectList}>
-      <ul className="no-padding">
-        {
-          _.map(objects, (obj, i) => {
-            return (
-              <li key={i} className="sub-category">
-                <span className="sub-category-title">{obj.title}</span>
-                <ul className="object-items">
-                {
-                  _.map(obj.items, (item, i) => {
-                    return (
-                      <li className="object-item" key={i} onClick={onClickHandler(item)}>{item.title}</li>
-                    );
-                  })
-                }
-                </ul>
-              </li>   
-            );
-          })
-        }
-      </ul>
-    </div>
-  );
+class ReserveObjectsList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedItem: {}
+    }
+  }
+
+  handleClickEvent(item) {
+    const preparedHandler = this.props.onClickHandler(item);
+
+    return (evt) => {
+      preparedHandler(evt);
+
+      this.setState({
+        selectedItem: item
+      });
+    }
+  }
+  
+  render() {
+    const { objects = [] } = this.props;
+    const { selectedItem = {} } = this.state;
+
+    return (
+      <div className={styles.objectList}>
+        <ul className="no-padding">
+          {
+            _.map(objects, (obj, i) => {
+              return (
+                <li key={i} className="sub-category">
+                  <span className="sub-category-title">{obj.title}</span>
+                  <ul className="object-items">
+
+                  {
+                    _.map(obj.items, (item, i) => {
+                      
+                      // TODO: replace selectedItem.title === item.title with id comparisons
+                      const elementStyles = classnames({
+                        'object-item': true,
+                        selected: selectedItem.title === item.title
+                      });
+
+                      return (
+                        <li
+                          className={elementStyles}
+                          key={i}
+                          onClick={this.handleClickEvent(item)}>
+                          {item.title}
+                        </li>
+                      );
+                    })
+                  }
+
+                  </ul>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    );
+  } 
 }
 
 export default ReserveObjectsList;
