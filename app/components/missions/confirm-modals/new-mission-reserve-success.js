ffa0e0b9b5763cf83f6315d4647ae19a72e60121
jjmasse <jmasse860@gmail.com>
2016-11-20 16:08:12 -0500

    chore ( stubbing out new component for success messaging )

---
+++ app/components/missions/confirm-modals/new-mission-reserve-success.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+class NewMissionReserveSuccess extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default NewMissionReserveSuccess;
