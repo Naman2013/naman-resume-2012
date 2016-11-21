8211305080f01d3baa75caa82ec306c7c6cb6b8a
aneudy1702 <elrey1702@gmail.com>
2016-11-20 20:52:05 -0500

    added appropriate object name spacing to object summary

--- app/components/reserve/reserve-by-object-summary.js
+++ app/components/reserve/reserve-by-object-summary.js
@@ -4,9 +4,9 @@ import styles from './reserve-by-object.scss';
 const TextBox = ({ object, clearBrowse, scheduleMission }) => {
   return (
     <div className={styles.objectSummary}>
-      <span className="title">{object.title}</span>
+      <span className="title">{object.item.title}</span>
 
-      <p>{object.summary}</p>
+      <p>{object.item.summary}</p>
       
       <section>
         <a className="btn-primary" onClick={scheduleMission}>Schedule Mission</a>
