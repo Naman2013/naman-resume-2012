13ed6b98d64134cb5d138f51983992958caee1e2
jjmasse <jmasse860@gmail.com>
2016-11-22 19:38:01 -0500

    chore ( clearing out super agent stuff )

--- app/utils/superagent.js
+++
@@ -1,8 +0,0 @@
-import superagent from 'superagent-use';
-import prefix from 'superagent-prefix';
-import promise from 'superagent-promise-plugin';
-
-superagent.use(prefix('https://saturn.slooh.com:444'));
-superagent.use(promise);
-
-export default superagent;
