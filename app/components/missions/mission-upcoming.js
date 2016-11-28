4721ff670cb7a6e9ad21eef42cea1eb4f78ca4d4
jjmasse <jmasse860@gmail.com>
2016-11-27 20:34:37 -0500

    chore ( destructuring the mission response )

--- app/components/missions/mission-upcoming.js
+++ app/components/missions/mission-upcoming.js
@@ -7,18 +7,19 @@ import { fetchUsersUpcomingMissions } from '../../modules/Users-Upcoming-Mission
 import styles from './mission-sidebar.scss';
 
 const NoContentAvailable = () => <span className="no-upcoming">No upcoming missions</span>;
+const FetchingMissions = () => <span className="no-upcoming">Fetching your next mission...</span>;
 
-const UpcomingContent = ({ content }) => {
+const UpcomingContent = ({ missionTitle, missionStart, objectIconUrl, objectTitle }) => {
   return (
     <div>
       <div className="cardsubTitle">
-        <img className={styles.cardIcon} src={content.missionIcon} />
-        <h3>{content.missionTitle}</h3>
+        <img className={styles.cardIcon} src={objectIconUrl} />
+        <h3>{objectTitle}</h3>
       </div>
 
       <div className="upcoming-mission-date">
-        <strong>{content.missionDate}</strong><br />
-        {content.missionTime}
+        <strong>Thursday, October 18th</strong><br />
+        10:05pm EST  ·  7:05pm PST  ·  03:05 UTC
       </div>
 
       <a href="#" className="btn btn-primary">View Reservations</a>
@@ -27,42 +28,65 @@ const UpcomingContent = ({ content }) => {
 }
 
 const mapStateToProps = ( state, ownProps ) => ({
-  user: state.user,
   usersUpcomingMission: state.usersUpcomingMission,
 });
 
 const mapDispatchToProps = ( dispatch ) => ({
   actions: bindActionCreators({
-
+    fetchUsersUpcomingMissions,
   }, dispatch),
 });
 
 @connect( mapStateToProps, mapDispatchToProps )
 class MissionUpcoming extends Component {
-  render() {
-    const upcomingMission = {
-      missionTitle: 'Andromeda Galaxy (M31)',
-      missionIcon: 'assets/icons/Jupiter.svg',
-      missionDate: 'Thursday, October 18th',
-      missionTime: '10:05pm EST  ·  7:05pm PST  ·  03:05 UTC'
-    };
 
-    const user = {
-      name: 'Peter',
-      avatarUrl: 'assets/images/graphics/peter.jpg'
-    };
+  componentWillMount() {
+    this.props.actions.fetchUsersUpcomingMissions();
+  }
+
+  renderUpcomingMission() {
+    if(this.props.usersUpcomingMission) {
+      console.log(this.props.usersUpcomingMission);
+      const { missionList } = this.props.usersUpcomingMission.upcomingMission;
+      const currentMission = missionList[0];
+
+      const {
+        displayName,
+        avatarURL,
+        objectTitle,
+        objectIconURL,
+        missionStart,
+        userHasReservation } = currentMission;
+
+        return(
+          <div>
+            <div className="widget-header">
+              <img height="50" src={avatarURL} />
+              <h2>{displayName}&rsquo;s Upcoming Mission</h2>
+            </div>
+            {
+              userHasReservation ?
+                <UpcomingContent content={upcomingMission} />
+                :
+                <NoContentAvailable />
+            }
+          </div>
+        );
+    }
+
+    return( <NoContentAvailable /> );
+  }
+
+  render() {
+    const { fetchingMissions } = this.props.usersUpcomingMission;
 
     return (
       <div className="widget-container mission-upcoming">
-        <div className="widget-header">
-          <img src={user.avatarUrl} />
-          <h2>{user.name}&rsquo;s Upcoming Mission</h2>
-        </div>
         {
-          upcomingMission ?
-            <UpcomingContent content={upcomingMission} />
+          fetchingMissions ?
+            <FetchingMissions />
             :
-            <NoContentAvailable />
+            this.renderUpcomingMission()
         }
       </div>
     );
