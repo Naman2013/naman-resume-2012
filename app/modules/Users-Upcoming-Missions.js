6c21b25cd2ff2ea0c84678194074a52a21266d4e
jjmasse <jmasse860@gmail.com>
2016-11-27 19:32:28 -0500

    refactor ( changing name to match state association )

--- app/modules/Users-Upcoming-Missions.js
+++ app/modules/Users-Upcoming-Missions.js
@@ -6,7 +6,7 @@ const FETCH_USERS_UPCOMING_MISSIONS_START = 'FETCH_USERS_UPCOMING_MISSIONS_START
 const FETCH_USERS_UPCOMING_MISSIONS_SUCCESS = 'FETCH_USERS_UPCOMING_MISSIONS_SUCCESS';
 const FETCH_USERS_UPCOMING_MISSIONS_FAIL = 'FECTH_USERS_UPCOMING_MISSIONS_FAIL';
 
-export const fetchUsersUpcomingMissions = () => ( dispatch, getState ) {
+export const fetchUsersUpcomingMissions = () => ( dispatch, getState ) => {
   const { cid, at, token } = getState.user;
   return axios.post('/api/reservation/getNextUserMission', {
     cid,
@@ -17,28 +17,48 @@ export const fetchUsersUpcomingMissions = () => ( dispatch, getState ) {
   .catch( error => dispatch( fetchUsersUpcomingMissionsFail( error ) ) );
 };
 
+const fetchUsersUpcomingMissionsSuccess = ( payload ) => ({
+  type: FETCH_USERS_UPCOMING_MISSIONS_SUCCESS,
+  payload,
+});
+
+const fetchUsersUpcomingMissionsFail = ( error ) => ({
+  type: FETCH_USERS_UPCOMING_MISSIONS_FAIL,
+  error,
+});
+
 
 
 const initialState = {
   fetchingMissions: false,
   fetchingMissionsFailed: false,
   upcomingMission: null,
+  upcomingMissionError: null,
 };
 
 export default createReducer(initialState, {
-  [FETCH_USERS_UPCOMING_MISSIONS_START]( state ) {
+  [FETCH_USERS_UPCOMING_MISSIONS_START]() {
     return {
-      ...state,
+      fetchingMissions: true,
+      fetchingMissionsFailed: false,
+      upcomingMission: null,
+      upcomingMissionError: null,
     };
-  }
+  },
   [FETCH_USERS_UPCOMING_MISSIONS_SUCCESS]( state, { payload } ) {
     return {
-      ...state,
+      fetchingMissions: false,
+      fetchingMissionsFailed: false,
+      upcomingMission: payload,
+      upcomingMissionError: null,
     };
-  }
-  [FETCH_USERS_UPCOMING_MISSIONS_FAIL]( state ) {
+  },
+  [FETCH_USERS_UPCOMING_MISSIONS_FAIL]( state, { error } ) {
     return {
-      ...state,
+      fetchingMissions: false,
+      fetchingMissionsFailed: true,
+      upcomingMission: null,
+      upcomingMissionError: error,
     };
-  }
+  },
 });
