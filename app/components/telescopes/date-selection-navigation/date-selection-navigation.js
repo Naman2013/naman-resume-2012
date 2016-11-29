0694137cb0f08b083509653958951e69d59ad269
jjmasse <jmasse860@gmail.com>
2016-11-29 18:24:41 -0500

    chore ( staging content and buttons )

--- app/components/telescopes/date-selection-navigation/date-selection-navigation.js
+++ app/components/telescopes/date-selection-navigation/date-selection-navigation.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import style from './date-selection-navigation.scss';
 
 class DateSelectionNavigation extends Component {
 
   render() {
     return (
-      <div className="dates-selection">
-        <div className="arrow-container col">
-          <i className="icon arrow previous"></i> Back
+      <div className="reserve-by-telescope-date-selection-navigation">
+
+        <div className="progress-time-action">
+          <button className="button">
+            <i className="fa fa-chevron-circle-left"></i> Back
+          </button>
         </div>
-        <div className="when col">
 
-          <div>
-            <span className="subs">The night of</span>
-          </div>
-
-          <div>
-            <span className="date">Wednesday, August 11, 2016</span>
-          </div>
-
-          <div>
-            <span className="subs">Last updated 21 seconds ago.</span>
-          </div>
+        <div className="current-date-content">
+          <h3 className="title">The night of</h3>
+          <p className="current-time">Wednesday, August 11, 2016</p>
+          <h5 className="last-updated">Last updated 21 seconds ago.</h5>
         </div>
-        <div className="arrow-container col">
-          Next Day <i className="icon arrow next"></i>
+
+        <div className="progress-time-action">
+          <button className="button">
+            Next Day <i className="fa fa-chevron-circle-right"></i>
+          </button>
         </div>
 
       </div>
