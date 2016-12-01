4481a267f8efd8ed3bc6575f7d1f036463f06d52
jjmasse <jmasse860@gmail.com>
2016-12-01 08:59:43 -0500

    chore ( adding in content and spacing for piggyback on mission row )

--- app/components/telescopes/listings/partials/mission-time.js
+++ app/components/telescopes/listings/partials/mission-time.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const MissionTime = () => {
   return (
-    <div className="col-md-2 dates-column">
-      <div>
-          7:15 EST
-          4:14 PST / 00:15 UTC
-      </div>
+    <div className="mission-time">
+      <span className="highlight">7:15 EST</span><br />
+      4:14 PST / 00:15 UTC
     </div>
   );
 }
