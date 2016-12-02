85ada1b9aa8616db22542fb06bc59a6022fc8be4
jjmasse <jmasse860@gmail.com>
2016-12-02 08:05:04 -0500

    feat ( open and close / animation tweaks )

--- app/components/telescopes/listings/reservation-slots/available-mission.js
+++ app/components/telescopes/listings/reservation-slots/available-mission.js
@@ -3,51 +3,75 @@ import classnames from 'classnames';
 
 import MissionTime from '../partials/mission-time';
 
+
 class AvailableMission extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleBrowserObjectsClick = this.handleBrowserObjectsClick.bind(this);
+    this.handleCloseForm = this.handleCloseForm.bind(this);
+  }
+
+  handleBrowserObjectsClick(event) {
+    event.preventDefault();
+
+    // TODO: set internal state for browse by objects?
+    this.props.toggleFormDisplay();
+  }
+
+  handleCloseForm(event) {
+    event.preventDefault();
+    this.props.toggleFormDisplay();
+  }
+
   render() {
+
+    const { formOpen } = this.props;
 
     const containerClassnames = classnames({
       'telescope-listings-item-inline-reservation': 1,
       'available': 1,
-      'expanded': 1,
+      'expanded': formOpen,
     });
 
     return(
       <li className={containerClassnames}>
 
         <div className="above-the-fold-content clearfix">
+          <div className="content">
+            <div className="close-button">
+              <button onClick={this.handleCloseForm} className="action">
+                <span className="fa fa-close"></span>
+              </button>
+            </div>
 
-          <div className="close-button">
-            <button className="action">
-              <span className="fa fa-close"></span>
-            </button>
-          </div>
+            <div className="col-xs-2">
+              <MissionTime />
+            </div>
 
-          <div className="col-xs-2">
-            <MissionTime />
-          </div>
+            <div className="col-xs-4 slot-description">
+              <img className="slot-logo" src="assets/icons/alien-head.png" height="40" alt=""/>
+              <span className="slot-name">This slot could be yours.</span>
+            </div>
 
-          <div className="col-xs-4 slot-description">
-            <img className="slot-logo" src="assets/icons/alien-head.png" height="40" alt=""/>
-            <span className="slot-name">This slot could be yours.</span>
-          </div>
-
-          <div className="col-xs-6 reservation-options-content">
-            <ul className="reservation-options">
-              <li className="option">
-                <button className="action">Browser objects</button>
-              </li>
-              <li className="option">
-                <button className="action">Select by catelog #</button>
-              </li>
-              <li className="option">
-                <button className="action">Enter coordinate</button>
-              </li>
-            </ul>
+            <div className="col-xs-6 reservation-options-content">
+              <ul className="reservation-options">
+                <li className="option">
+                  <button onClick={this.handleBrowserObjectsClick} className="action">Browser objects</button>
+                </li>
+                <li className="option">
+                  <button className="action">Select by catelog #</button>
+                </li>
+                <li className="option">
+                  <button className="action">Enter coordinate</button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
 
-        <div className="divider-line"></div>
+
 
         <div className="reservation-form-container">
 
@@ -67,5 +91,14 @@ class AvailableMission extends Component {
     );
   }
 }
+
+AvailableMission.defaultProps = {
+  formOpen: false,
+};
+
+AvailableMission.propTypes = {
+  toggleFormDisplay: PropTypes.func.isRequired,
+  formOpen: PropTypes.bool.isRequired,
+};
 
 export default AvailableMission;
