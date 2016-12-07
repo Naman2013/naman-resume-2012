cbc86d9bd5fbbddceccaed171463a99d008640d4
jjmasse <jmasse860@gmail.com>
2016-12-07 18:22:14 -0500

    refactor ( no need for this file )

--- app/modules/reserve-by-telescope/reserve-by-telescope.js
+++
@@ -1,8 +0,0 @@
-import {
-  getObservatoryList,
-  getCurrentObservatory } from '../Telescope-Overview';
-
-export const setupReserveByTelescope = (obsId) => (dispatch, getState) => {
-  dispatch(getObservatoryList(obsId));
-  console.log('finished...');
-};
