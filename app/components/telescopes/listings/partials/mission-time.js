5a0d436f41ad106ec4cbac23ba199c5ba375d3c2
jjmasse <jmasse860@gmail.com>
2016-12-08 13:56:09 -0500

    feat ( formatting time )

--- app/components/telescopes/listings/partials/mission-time.js
+++ app/components/telescopes/listings/partials/mission-time.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import moment from 'moment-timezone';
 
-const MissionTime = () => {
+const MissionTime = ({ startTime }) => {
+  const FORMAT = 'h:mm z';
+  const formattedUTCDate = new Date(startTime * 1000);
+  const EST_start_time = moment.tz(formattedUTCDate, 'America/New_York').format(FORMAT);
+  const PST_start_time = moment.tz(formattedUTCDate, 'America/Los_Angeles').format(FORMAT);
+  const UTC_start_time = moment.utc(formattedUTCDate).format(FORMAT);
   return (
     <div className="mission-time">
-      <span className="highlight">7:15 EST</span><br />
-      4:14 PST / 00:15 UTC
+      <span className="highlight">{EST_start_time}</span><br />
+      {PST_start_time} / {UTC_start_time}
     </div>
   );
 }
