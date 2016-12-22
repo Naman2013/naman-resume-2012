81a0e52b4b253f099a7bdbd650b82d236e1c0eda
jjmasse <jmasse860@gmail.com>
2016-12-22 16:42:29 -0500

    feat ( mission validation and mission control updates )

--- app/components/missions/mission-updates.js
+++ app/components/missions/mission-updates.js
@@ -3,19 +3,46 @@ import MissionUpdatePost from './mission-update-post';
 import styles from './mission-sidebar.scss';
 import moment from 'moment';
 
+const BetweenState = ({ message }) => (
+  <div className="no-mission-updates">
+    <h5 className="message">{message}</h5>
+  </div>
+);
+
 class MissionUpdates extends Component {
+  renderResponse() {
+    const { updates } = this.props;
+
+    if(!updates) {
+      return(
+        <BetweenState message="fetching updates..." />
+      );
+    }
+
+    if(updates.length === 0) {
+      return(
+        <BetweenState message="No mission control updates are available at this time." />
+      );
+    }
+
+    return(
+      updates.map(update => {
+        if(update.author) {
+          return <MissionUpdatePost update={update} key={update.uniqueId} />
+        }
+      })
+    );
+
+  }
 
   render() {
+    const { updates } = this.props;
+
     return (
       <div className="widget-container mission-updates-widget">
         <h2 className="title">Mission Control Updates</h2>
-
         {
-          this.props.updates ? this.props.updates.map(update => {
-            if(update.author) {
-              return <MissionUpdatePost update={update} key={update.uniqueId} />
-            }
-          }) : 'waiting...'
+          this.renderResponse()
         }
       </div>
     )
--- app/constants/reservation-api-error-codes.js
+++ app/constants/reservation-api-error-codes.js
@@ -1,6 +1,9 @@
+const DEFAULT_SUBTITLE = `There was a problem reserving your mission.`;
+
 export default {
   '23': {
-    errorTitle: 'Invalid mission',
-    errorMessage: 'It seems that the mission you are trying to request is no longer valid.  Please refresh and try again.',
+    title: 'Invalid mission',
+    subTitle: DEFAULT_SUBTITLE,
+    message: 'It seems that the mission you are trying to request is no longer valid.  Please refresh and try again.',
   },
 };
