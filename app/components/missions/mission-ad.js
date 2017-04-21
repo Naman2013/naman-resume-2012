7066b47bd2acfa0dd209a4ec4dea97f9997d41e4
jjmasse <jmasse860@gmail.com>
2017-04-20 23:28:03 -0400

    feat ( now displaying priority ads before randomized )

--- app/components/missions/mission-ad.js
+++ app/components/missions/mission-ad.js
@@ -9,21 +9,27 @@ import { getRandomAdvertisementIndex } from '../../modules/utils';
 
 const { string, number } = PropTypes;
 
+// TODO: refactor to dynamically determine how many "priority ads" are to be displayed
+const PRIORITY_ADS = 3;
+
 export const images300x250 = [
-  'assets/images/graphics/ad-banner_300x250_book.gif',
-  'assets/images/graphics/ad-banner_300x250_gift.gif',
   // temporary
   'assets/images/graphics/PASS_LA_300x250.jpg',
   'assets/images/graphics/PASS_LA_1_300x250.jpg',
   'assets/images/graphics/PASS_LA_2_300x250.jpg',
+
+  'assets/images/graphics/ad-banner_300x250_book.gif',
+  'assets/images/graphics/ad-banner_300x250_gift.gif',
 ];
+
 const images300x600 = [
-  'assets/images/graphics/ad-banner_300x600_book.gif',
-  'assets/images/graphics/ad-banner_300x600_gift.gif',
   // temporary
   'assets/images/graphics/PASS_LA_300x600.jpg',
   'assets/images/graphics/PASS_LA_1_300x600.jpg',
   'assets/images/graphics/PASS_LA_2_300x600.jpg',
+
+  'assets/images/graphics/ad-banner_300x600_book.gif',
+  'assets/images/graphics/ad-banner_300x600_gift.gif',
 ];
 
 const generateRandomImage = (size, i) => {
@@ -37,15 +43,20 @@ const generateRandomImage = (size, i) => {
 
 const getUrl = (i) => {
   const urls = [
-    'https://www.amazon.com/dp/0997621109', // book
-    'https://www.amazon.com/dp/B01MDNJXIR', // gift
     // temporary
     'https://www.amazon.co.uk/Passengers-Chris-Pratt/dp/B01N0R2CW7/ref=as_li_ss_tl?s=instant-video&ie=UTF8&qid=1491493702&sr=1-1&keywords=passengers+2016&linkCode=sl1&tag=sonypassengers-21&linkId=549de0a304f43e8d1eed3087819e0983',
     'https://www.amazon.co.uk/Passengers-Chris-Pratt/dp/B01N0R2CW7/ref=as_li_ss_tl?s=instant-video&ie=UTF8&qid=1491493702&sr=1-1&keywords=passengers+2016&linkCode=sl1&tag=sonypassengers-21&linkId=549de0a304f43e8d1eed3087819e0983',
     'https://www.amazon.co.uk/Passengers-Chris-Pratt/dp/B01N0R2CW7/ref=as_li_ss_tl?s=instant-video&ie=UTF8&qid=1491493702&sr=1-1&keywords=passengers+2016&linkCode=sl1&tag=sonypassengers-21&linkId=549de0a304f43e8d1eed3087819e0983',
+
+    'https://www.amazon.com/dp/0997621109', // book
+    'https://www.amazon.com/dp/B01MDNJXIR', // gift
   ];
   return urls[i];
 };
+
+const mapStateToProps = ({ adStats }) => ({
+  adsDisplayed: adStats.adsDisplayed,
+});
 
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators({
@@ -53,18 +64,20 @@ const mapDispatchToProps = dispatch => ({
   }, dispatch),
 });
 
-@connect(null, mapDispatchToProps)
+@connect(mapStateToProps, mapDispatchToProps)
 class MissionAd extends Component {
   constructor(props) {
     super(props);
     this.props.actions.incrementAdDisplayCounter();
   }
   render() {
-    const { size, index } = this.props;
+    const { size, index, adsDisplayed } = this.props;
+    const displayAdIndex = (adsDisplayed <= PRIORITY_ADS) ? adsDisplayed - 1 : index;
+
     return (
       <div className="mission-ad widget-container">
-        <Link href={getUrl(index)} target="_blank">
-          <img alt="advertisement" src={generateRandomImage(size, index)} />
+        <Link href={getUrl(displayAdIndex)} target="_blank">
+          <img alt="advertisement" src={generateRandomImage(size, displayAdIndex)} />
         </Link>
         <p>Advertisement</p>
       </div>
@@ -75,9 +88,11 @@ class MissionAd extends Component {
 MissionAd.defaultProps = {
   size: '300x250',
   number: getRandomAdvertisementIndex(),
+  adsDisplayed: 0,
 };
 
 MissionAd.propTypes = {
+  adsDisplayed: number,
   size: string,
   index: number,
 };
