3f04bcc189635dba32a9e3b822d0832b9ba16d20
jjmasse <jmasse860@gmail.com>
2017-05-04 17:53:06 -0400

    fix ( apostrophe )

--- app/components/telescope-details/neoview/NeoViewDescription.js
+++ app/components/telescope-details/neoview/NeoViewDescription.js
@@ -6,7 +6,7 @@ export default function NeoViewDescription() {
     <div>
       <p className={s.neoviewDescription}>
         <span className={s.neoviewDescriptionHighlight}>What is this?</span>
-         Slooh&apost;s telescopes go through a complex process of taking long
+         Slooh&apos;s telescopes go through a complex process of taking long
          exposures through various filters, ultimately combining that data into
          a single color image stream.  Ever see The Matrix?  Think of this as
          the &quot;Neo View&quot; as the exposure is being processed.
