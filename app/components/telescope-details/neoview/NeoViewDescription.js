4e15525d0c9fa4a09c4e0a44b8a8943cb17a54e9
jjmasse <jmasse860@gmail.com>
2017-05-04 18:04:39 -0400

    fix ( whitespace in the description content )

--- app/components/telescope-details/neoview/NeoViewDescription.js
+++ app/components/telescope-details/neoview/NeoViewDescription.js
@@ -5,11 +5,7 @@ export default function NeoViewDescription() {
   return (
     <div>
       <p className={s.neoviewDescription}>
-        <span className={s.neoviewDescriptionHighlight}>What is this?</span>
-         Slooh&apos;s telescopes go through a complex process of taking long
-         exposures through various filters, ultimately combining that data into
-         a single color image stream.  Ever see The Matrix?  Think of this as
-         the &quot;Neo View&quot; as the exposure is being processed.
+        <span className={s.neoviewDescriptionHighlight}>What is this?</span> Slooh&apos;s telescopes go through a complex process of taking long exposures through various filters, ultimately combining that data into a single color image stream.  Ever see The Matrix?  Think of this as the &quot;Neo View&quot; as the exposure is being processed.
       </p>
     </div>
   );
