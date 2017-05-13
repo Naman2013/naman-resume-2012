6045c20fb3be676938ed2754e6bf9396aa97979f
jjmasse <jmasse860@gmail.com>
2017-05-12 20:37:37 -0400

    feat ( storing the last url the user saw prior to entering the up sell flow )

--- app/modules/authorization/actions.js
+++ app/modules/authorization/actions.js
@@ -10,6 +10,14 @@ export const RESET_ERROR_STATE = 'RESET_ERROR_STATE';
 
 export const VALIDATE_RESPONSE = 'VALIDATE_RESPONSE';
 
+// URL to return to when the user successfully signs in
+export const SET_SIGN_IN_RETURN_URL = 'SET_SIGN_IN_RETURN_URL';
+
+const setSignInReturnURL = signInReturnURL => ({
+  type: SET_SIGN_IN_RETURN_URL,
+  signInReturnURL,
+});
+
 const fetchErrorsStart = () => ({
   type: FETCH_ERRORS_START,
 });
@@ -67,6 +75,7 @@ export const fetchErrors = () => (dispatch, getState) => {
       }
 
       if (responseType === LOGIN_UPSELL) {
+
         dispatch(push('/registration/sign-in'));
       }
 
@@ -86,7 +95,9 @@ export const validateResponseAccess = apiResponse => (dispatch) => {
 
   const { apiError, errorCode, statusCode, loginError } = apiResponse;
   if (statusCode === UNAUTHORIZED_STATUS_CODE || statusCode === EXPIRED_ACCOUNT_STATUS_CODE) {
+
     if (typeof loginError === 'undefined') {
+      dispatch(setSignInReturnURL(window.location.hash));
       if (errorCode === BAD_LOGIN_SESSION_CODE) {
         destroySession();
       }
