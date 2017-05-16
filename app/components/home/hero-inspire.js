9afa0c6b505f23186413a3717aa0ef3ee032556f
jjmasse <jmasse860@gmail.com>
2017-05-16 08:50:15 -0400

    feat ( fading video in when we know it has loaded )

--- app/components/home/hero-inspire.js
+++ app/components/home/hero-inspire.js
@@ -4,58 +4,89 @@
   this version introduces a video background to sit behind the primary
   call to action
   */
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 import { Link } from 'react-router';
 import style from './hero-inspire.scss';
 
 import ScrollForMore from '../common/scroll-for-more';
 
-function HeroInspire({
-  heroEventId,
-  heroEventIsLive,
-  heroImageURL,
-  heroHeadline,
-  heroSubheadline,
-  heroFactoidText,
-  heroFactoidIconURL,
-  heroButtonText,
-  heroButtonURL,
-}) {
-  const heroContainerStyle = {
-    background: `url(${heroImageURL}) center/cover no-repeat`,
+class HeroInspire extends Component {
+  state = {
+    videoLoaded: false,
   };
 
-  const buttonUrl = heroEventId === 0 ? heroButtonURL :
-    // construct link for space situation room
-    heroEventIsLive ? '/shows/situation-room' :
-    // construct link for video event page
-    `/shows/event-details/${heroEventId}`;
+  componentDidMount() {
+    this.videoRenderedTimer = setInterval(() => {
+      const video = document.getElementById('heroInspireVideoBackground');
+      if (video.readyState === 4) {
+        this.setState({
+          videoLoaded: true,
+        });
+        clearInterval(this.videoRenderedTimer);
+      }
+    }, 500);
+  }
 
-  return (
-    <div
-      style={heroContainerStyle}
-      className="hero-container-inspire"
-    >
-      <video className={style.videoBackground} playsinline autoPlay muted loop>
-        <source src="assets/video/stars-high-720.webm" type="video/webm" />
-        <source src="assets/video/stars-high-720.mp4" type="video/mp4" />
-      </video>
+  componentWillUnmount() {
+    clearInterval(this.videoRenderedTimer);
+  }
 
-      <div className={style.contentContainer}>
-        <h2 className="title">Space for everyone</h2>
-        <h3 className="sub-title">Online Telescopes, LIVE Streaming Events, Original Video Programming, and more.</h3>
+  render() {
+    const {
+      heroEventId,
+      heroEventIsLive,
+      heroImageURL,
+      heroHeadline,
+      heroSubheadline,
+      heroFactoidText,
+      heroFactoidIconURL,
+      heroButtonText,
+      heroButtonURL,
+    } = this.props;
 
-        <div className="call-to-action">
-          <Link className="action" to="/">
-            Register Free Today
-          </Link>
+    const { videoLoaded } = this.state;
+
+    const heroContainerStyle = {
+      background: 'black',
+    };
+
+    const videoClassnames = classnames(`${style.videoBackground}`, {
+      maxOpacity: videoLoaded,
+    });
+
+    const buttonUrl = heroEventId === 0 ? heroButtonURL :
+      // construct link for space situation room
+      heroEventIsLive ? '/shows/situation-room' :
+      // construct link for video event page
+      `/shows/event-details/${heroEventId}`;
+
+    return (
+      <div
+        style={heroContainerStyle}
+        className="hero-container-inspire"
+      >
+        <video id="heroInspireVideoBackground" className={videoClassnames} playsinline autoPlay muted>
+          <source src="assets/video/stars-high-720.webm" type="video/webm" />
+          <source src="assets/video/stars-high-720.mp4" type="video/mp4" />
+        </video>
+
+        <div className={style.contentContainer}>
+          <h2 className="title">Space for everyone</h2>
+          <h3 className="sub-title">Online Telescopes, LIVE Streaming Events, Original Video Programming, and more.</h3>
+
+          <div className="call-to-action">
+            <Link className="action" to="/">
+              Register Free Today
+            </Link>
+          </div>
         </div>
+
+
+        <ScrollForMore />
       </div>
-
-
-      <ScrollForMore />
-    </div>
-  );
+    );
+  }
 }
 
 HeroInspire.propTypes = {
