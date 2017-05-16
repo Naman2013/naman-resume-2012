1a6b553be765f7582f0edd087405894616af6e31
jjmasse <jmasse860@gmail.com>
2017-05-15 22:02:26 -0400

    feat ( adding video tag and referencing content )

--- app/components/home/hero-inspire.js
+++ app/components/home/hero-inspire.js
@@ -36,6 +36,10 @@ function HeroInspire({
       style={heroContainerStyle}
       className="hero-container-inspire"
     >
+      <video className={style.videoBackground} playsinline autoPlay muted loop>
+        <source src="assets/video/stars-lo.webm" type="video/webm" />
+        <source src="assets/video/stars-lo.mp4" type="video/mp4" />
+      </video>
 
       <div className={style.contentContainer}>
         <h2 className="title">Space for everyone</h2>
