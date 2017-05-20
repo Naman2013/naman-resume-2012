9dc59db7f77333ca5536019715c14dab6e5e1b9a
jjmasse <jmasse860@gmail.com>
2017-05-20 12:54:51 -0400

    chore ( adding test code )

--- app/components/common/google-ads/GoogleAd.test.js
+++ app/components/common/google-ads/GoogleAd.test.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import GoogleAd from './GoogleAd';
 
+const TEST_DATA = {
+  targetAdID: 'advertisement-target',
+};
 
 test('a div with the target ad ID is generated', () => {
+  const googleAd = shallow(
+    <GoogleAd
+      targetDivID={TEST_DATA.targetAdID}
+    />
+  );
 
+  console.log(googleAd);
 });
