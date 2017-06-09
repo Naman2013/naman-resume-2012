0ee58e52621ff26b8ebe8002fe837a6c2298664a
jjmasse <jmasse860@gmail.com>
2017-06-09 08:17:23 -0400

    chore ( whitespace )

--- app/modules/User.js
+++ app/modules/User.js
@@ -11,10 +11,10 @@ export const removeUser = createAction(REMOVE_USER);
 
 export function store(user) {
   localStorage.setItem('user', JSON.stringify(user));
-  document.cookie = cookie.serialize('cid', user.cid, { domain: '.slooh.com' });
-  document.cookie = cookie.serialize('token', user.token, { domain: '.slooh.com' });
-  document.cookie = cookie.serialize('at', user.at, { domain: '.slooh.com' });
-  document.cookie = cookie.serialize('fname', user.fname, { domain: '.slooh.com' });
+  document.cookie = cookie.serialize('cid', user.cid, { domain: 'slooh.com' });
+  document.cookie = cookie.serialize('token', user.token, { domain: 'slooh.com' });
+  document.cookie = cookie.serialize('at', user.at, { domain: 'slooh.com' });
+  document.cookie = cookie.serialize('fname', user.fname, { domain: 'slooh.com' });
 
   return (dispatch) => {
     dispatch(set(user));
@@ -23,10 +23,10 @@ export function store(user) {
 
 export function destroySession() {
   localStorage.removeItem('user');
-  document.cookie = cookie.serialize('cid', '', { domain: '.slooh.com', expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT') });
-  document.cookie = cookie.serialize('token', '', { domain: '.slooh.com', expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT') });
-  document.cookie = cookie.serialize('at', '', { domain: '.slooh.com', expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT') });
-  document.cookie = cookie.serialize('fname', '', { domain: '.slooh.com', expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT') });
+  document.cookie = cookie.serialize('cid', '', { domain: 'slooh.com', expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT') });
+  document.cookie = cookie.serialize('token', '', { domain: 'slooh.com', expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT') });
+  document.cookie = cookie.serialize('at', '', { domain: 'slooh.com', expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT') });
+  document.cookie = cookie.serialize('fname', '', { domain: 'slooh.com', expires: new Date('Thu, 01 Jan 1970 00:00:01 GMT') });
 }
 
 export const logout = () => {
