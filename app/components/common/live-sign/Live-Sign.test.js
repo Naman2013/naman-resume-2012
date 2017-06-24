fd0a833b407e91fb1341dd3f4c15cd4c8639a647
Will Acheson <w.acheson@we-are-mea.com>
2017-06-23 20:54:20 -0400

    cleaned up comment in live sign test

--- app/components/common/live-sign/Live-Sign.test.js
+++ app/components/common/live-sign/Live-Sign.test.js
@@ -2,13 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import LiveSign from './live-sign';
 
-// const TEST_DATA = {
-//   targetDivID: 'advertisement-target',
-//   adURL: 'slooh.com',
-//   adWidth: 300,
-//   adHeight: 250,
-// };
-
 it('renders correctly', () => {
   const liveSign = renderer.create(
     <LiveSign />,
