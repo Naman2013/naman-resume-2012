9c88292bddd46cfacac868dad89db90b7663c979
jjmasse <jmasse860@gmail.com>
2017-07-10 19:54:42 -0400

    chore ( adding test stubs )

--- app/components/telescope-details/UpcomingMissions/Mission.test.js
+++ app/components/telescope-details/UpcomingMissions/Mission.test.js
@@ -2,4 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Mission from './Mission';
 
-describe('<Mission />');
+describe('<Mission />', () => {
+  it('should render pending mission', () => {
+    return;
+  });
+
+  it('should render an upcoming scheduled mission', () => {
+    return;
+  });
+});
