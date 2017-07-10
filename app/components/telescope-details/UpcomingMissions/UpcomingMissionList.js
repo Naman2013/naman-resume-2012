eef3691b09cd1c5566199f9bdd149ee17557350c
jjmasse <jmasse860@gmail.com>
2017-07-10 17:53:55 -0400

    chore ( generating missions and passing properties and generating keys )

--- app/components/telescope-details/UpcomingMissions/UpcomingMissionList.js
+++ app/components/telescope-details/UpcomingMissions/UpcomingMissionList.js
@@ -2,50 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Mission from './Mission';
 
-/**
-"nextMissionAvailable" : true,
-"nextScheduledMissionId" : 1621823,
-"nextStart" : 1499569800,
-"nextTitle" : Pending: Target name is not yet available,
-"nextObjectIconURL" : https:\/\/vega.slooh.com\/icons\/reservations\/not_available_w.svg,
-"upcomingMissionCount" : 3,
-"upcomingMissionArray" : -[
--{
-"upcomingMissionIndex" : 0,
-"upcomingMissionAvailable" : true,
-"upcomingScheduledMissionId" : 1621823,
-"upcomingStart" : 1499569800,
-"upcomingTitle" : Pending: Target name is not yet available,
-"upcomingObjectIconURL" : https:\/\/vega.slooh.com\/icons\/reservations\/not_available_w.svg
-},
--{
-"upcomingMissionIndex" : 1,
-"upcomingMissionAvailable" : true,
-"upcomingScheduledMissionId" : 1621824,
-"upcomingStart" : 1499570100,
-"upcomingTitle" : Pending: Target name is not yet available,
-"upcomingObjectIconURL" : https:\/\/vega.slooh.com\/icons\/reservations\/not_available_w.svg
-},
--{
-"upcomingMissionIndex" : 2,
-"upcomingMissionAvailable" : true,
-"upcomingScheduledMissionId" : 1621825,
-"upcomingStart" : 1499570400,
-"upcomingTitle" : Pending: Target name is not yet available,
-"upcomingObjectIconURL" : https:\/\/vega.slooh.com\/icons\/reservations\/not_available_w.svg
-}
-]
-*/
-
 const propTypes = {
-  missions: PropTypes.arrayOf({
+  missions: PropTypes.arrayOf(PropTypes.shape({
     upcomingMissionIndex: PropTypes.number,
     upcomingMissionAvailable: PropTypes.bool,
     upcomingScheduledMissionId: PropTypes.number,
     upcomingStart: PropTypes.number,
     upcomingTitle: PropTypes.string,
     upcomingObjectIconURL: PropTypes.string,
-  }),
+  })),
 };
 
 const defaultProps = {
@@ -55,15 +20,17 @@ const defaultProps = {
 const UpcomingMissionList = ({ missions }) => (
   <div>
     <ul>
-      <li className="mission">
-        <Mission />
-      </li>
-      <li className="mission">
-        <Mission />
-      </li>
-      <li className="mission">
-        <Mission />
-      </li>
+      {
+        missions.map(mission => (
+          <li className="mission" key={`${mission.upcomingStart}-${mission.upcomingMissionIndex}`}>
+            <Mission
+              upcomingStart={mission.upcomingStart}
+              upcomingTitle={mission.upcomingTitle}
+              upcomingObjectIconURL={mission.upcomingObjectIconURL}
+            />
+          </li>
+        ))
+      }
     </ul>
   </div>
 );
--- app/components/telescope-details/UpcomingMissions/UpcomingMissionList.test.js
+++ app/components/telescope-details/UpcomingMissions/UpcomingMissionList.test.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import UpcomingMissionList from './UpcomingMissionList';
-import Mission from './Mission';
 
-const setOfMissions = [];
+const MISSIONS = [
+  {
+    upcomingMissionIndex: 0,
+    upcomingMissionAvailable: true,
+    upcomingScheduledMissionId: 1621824,
+    upcomingStart: 1499570100,
+    upcomingTitle: 'Pending: Target name is not yet available',
+    upcomingObjectIconURL: 'https://vega.slooh.com/icons/reservations/not_available_w.svg',
+  },
+  {
+    upcomingMissionIndex: 1,
+    upcomingMissionAvailable: true,
+    upcomingScheduledMissionId: 1621825,
+    upcomingStart: 1499570400,
+    upcomingTitle: 'Pending: Target name is not yet available',
+    upcomingObjectIconURL: 'https://vega.slooh.com/icons/reservations/not_available_w.svg',
+  },
+];
 
 describe('<UpcomingMissions />', () => {
-  it('should render three list items', () => {
-    const upcomingWrapper = shallow(<UpcomingMissionList />);
-    expect(upcomingWrapper.find('.mission')).toHaveLength(3);
+  it('should render the missions given to it', () => {
+    const upcomingWrapper = shallow(<UpcomingMissionList missions={MISSIONS} />);
+    expect(upcomingWrapper.find('.mission')).toHaveLength(2);
   });
 });
