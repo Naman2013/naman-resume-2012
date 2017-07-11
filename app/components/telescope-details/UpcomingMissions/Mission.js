c786bccdf2ae625bcec72c9c7e0ec84e6bd38d6d
jjmasse <jmasse860@gmail.com>
2017-07-11 08:48:26 -0400

    feat ( maturing the mission component and adding snapshot test )

--- app/components/telescope-details/UpcomingMissions/Mission.js
+++ app/components/telescope-details/UpcomingMissions/Mission.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Mission = () => (
+function backgroundImage(imageURL) {
+  return {
+    backgroundImage: `url(${imageURL})`,
+  };
+}
+
+const propTypes = {
+  upcomingTitle: PropTypes.string.isRequired,
+  upcomingObjectIconURL: PropTypes.string.isRequired,
+};
+
+const Mission = ({ upcomingTitle, upcomingObjectIconURL }) => (
   <div>
-    <p>Mission....</p>
+    <div className="imageIcon" style={backgroundImage(upcomingObjectIconURL)} />
+    <div className="missionDetails">
+      <h4 className="title">{upcomingTitle}</h4>
+    </div>
   </div>
 );
+
+
+Mission.propTypes = propTypes;
 
 export default Mission;
--- app/components/telescope-details/UpcomingMissions/Mission.test.js
+++ app/components/telescope-details/UpcomingMissions/Mission.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Mission from './Mission';
 
-describe('<Mission />', () => {
-  it('should render pending mission', () => {
-    return;
-  });
+const pendingMission = {
+  upcomingTitle: 'Andromeda Galaxy',
+  upcomingObjectIconURL: 'https://vega.slooh.com/icons/reservations/not_available_w.svg',
+};
 
-  it('should render an upcoming scheduled mission', () => {
-    return;
+describe('<Mission />', () => {
+  it('should render a missions title and image', () => {
+    const tree = renderer.create(
+      <Mission
+        upcomingTitle={pendingMission.upcomingTitle}
+        upcomingObjectIconURL={pendingMission.upcomingObjectIconURL}
+      />,
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 });
