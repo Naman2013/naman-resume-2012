51e587172b4ce31e59814699158251ef48b03a11
jmasse <jmasse860@gmail.com>
2017-07-16 13:16:06 -0400

    chore (adding snap test to the offline component)

--- app/components/telescope-overview/telescope-cards/telescope-offline.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import TelescopeOffline from './telescope-offline';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<TelescopeOffline />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
