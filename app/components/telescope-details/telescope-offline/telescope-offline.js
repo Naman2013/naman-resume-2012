202a01d410b58574ec9128e2c84bc3eeef0c5af1
jmasse <jmasse860@gmail.com>
2017-07-16 12:44:33 -0400

    feat(adding and styling the offline message)

--- app/components/telescope-details/telescope-offline/telescope-offline.js
+++ app/components/telescope-details/telescope-offline/telescope-offline.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { white } from '../../../styles/variables/colors';
+import { white, red } from '../../../styles/variables/colors';
 
 const propTypes = {
   imageSource: PropTypes.string.isRequired,
@@ -11,11 +11,15 @@ const defaultProps = {
   offlineStatusMessage: '',
 };
 
-const TelescopeOffline = ({ imageSource }) => (
+const TelescopeOffline = ({ imageSource, offlineStatusMessage }) => (
   <div className="telescope-details-offline">
     <h2>OFFLINE</h2>
     <div>
       <img alt="Telescope is currently offline" height="350" src={imageSource} />
+    </div>
+
+    <div className="offlineMessage">
+      <h5>{offlineStatusMessage}</h5>
     </div>
 
     <style jsx>{`
@@ -23,6 +27,16 @@ const TelescopeOffline = ({ imageSource }) => (
         background: ${white};
         text-align: center;
         padding: 10px 0 40px;
+      }
+
+      .offlineMessage {
+        color: ${red};
+        text-align: center;
+        padding-top: 20px;
+      }
+
+      .offlineMessage h5 {
+        font-size: 18px;
       }
     `}</style>
   </div>
