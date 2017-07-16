8bdce3bb1d04e00400462a2c879bc7985686857c
jmasse <jmasse860@gmail.com>
2017-07-16 12:38:52 -0400

    chore (refactoring of the telescope offline status message for telescope details)

--- app/components/telescope-details/telescope-offline/telescope-offline.js
+++ app/components/telescope-details/telescope-offline/telescope-offline.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import style from './telescope-offline.scss';
+import PropTypes from 'prop-types';
+import { white } from '../../../styles/variables/colors';
 
-const TelescopeOffline = ({ imageSource }) => {
-  return(
-    <div className="telescope-details-offline">
-      <h2>OFFLINE</h2>
-      <div>
-        <img height="350" src={imageSource} />
-      </div>
-    </div>
-  );
+const propTypes = {
+  imageSource: PropTypes.string.isRequired,
+  offlineStatusMessage: PropTypes.string,
 };
+
+const defaultProps = {
+  offlineStatusMessage: '',
+};
+
+const TelescopeOffline = ({ imageSource }) => (
+  <div className="telescope-details-offline">
+    <h2>OFFLINE</h2>
+    <div>
+      <img alt="Telescope is currently offline" height="350" src={imageSource} />
+    </div>
+
+    <style jsx>{`
+      .telescope-details-offline {
+        background: ${white};
+        text-align: center;
+        padding: 10px 0 40px;
+      }
+    `}</style>
+  </div>
+);
+
+TelescopeOffline.propTypes = propTypes;
+TelescopeOffline.defaultProps = defaultProps;
 
 export default TelescopeOffline;
