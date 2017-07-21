6dd75446978c8638a919e3541e85bf4b87f03632
jmasse <jmasse860@gmail.com>
2017-07-21 13:17:22 -0400

    chore(proptype access)

--- app/components/form/TextareaField.js
+++ app/components/form/TextareaField.js
@@ -1,7 +1,8 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
-const {object, string} = PropTypes;
+const { object, string } = PropTypes;
 
 export default class renderField extends Component {
     static propTypes = {
