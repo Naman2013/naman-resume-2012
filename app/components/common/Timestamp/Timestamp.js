571762866292af8350b2c3616d5dbc4fe6df48a0
jmasse <jmasse860@gmail.com>
2017-07-22 11:02:52 -0400

    chore(new component for showing timestamps in various timezones)

---
+++ app/components/common/Timestamp/Timestamp.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment-timezone';
+
+const propTypes = {
+  timestampInSeconds: PropTypes.number.isRequired,
+};
+
+const Timestamp = ({ timestampInSeconds }) => {
+  if (!timestampInSeconds) return null;
+  const convertedTimeStamp = timestampInSeconds * 1000;
+  const easternTime = moment.tz(convertedTimeStamp, 'America/New_York').format('h:mma z');
+  const pacificTime = moment.tz(convertedTimeStamp, 'America/Los_Angeles').format('h:mma z');
+  const utcTime = moment.utc(convertedTimeStamp).format('HH:mm z');
+
+  return (
+    <span>{easternTime} · {pacificTime} · {utcTime}</span>
+  );
+};
+
+Timestamp.propTypes = propTypes;
+
+export default Timestamp;
