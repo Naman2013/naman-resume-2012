a37b945b70432cc8e5a53ff382692e922261ddaa
jjmasse <jmasse860@gmail.com>
2017-07-24 07:06:08 -0400

    fix(by user tag css is now unnested based on legacy scss rules)

--- app/components/common/by-user-tag/by-user-tag.style.js
+++ app/components/common/by-user-tag/by-user-tag.style.js
@@ -2,9 +2,10 @@ import { black, white, lightTurqoise, red } from '../../../styles/variables/colo
 import { backgroundImageCover, borderRadius } from '../../../styles/mixins/utilities';
 
 export default `
-.slooh-by-user-tag {
-  display: flex;
-  align-items: center;
+  .slooh-by-user-tag {
+    display: flex;
+    align-items: center;
+  }
 
   .profile-photo,
   .profile-name {
@@ -23,38 +24,39 @@ export default `
   .profile-name {
     padding: 0 0 0 10px;
     margin: 0;
-
-      .username {
-        font-size: 1.2em;
-        font-weight: 300;
-
-        &.dark {
-          color: ${black};
-        }
-        &.light {
-          color: ${white};
-        }
-
-        .user-details {
-          font-size: 0.65em;
-
-          &.dark {
-            color: ${lightTurqoise};
-          }
-          &.light {
-            color: ${black};
-          }
-        }
-      }
-
-      .account-level {
-        font-size: 0.5em;
-        padding-left: 10px;
-        color: ${red};
-        font-weight: 600;
-        text-transform: uppercase;
-        white-space: nowrap;
-      }
   }
-}
+
+  .username {
+    font-size: 1.2em;
+    font-weight: 300;
+  }
+
+  .username.dark {
+    color: ${black};
+  }
+
+  .username.light {
+    color: ${white};
+  }
+
+  .user-details {
+    font-size: 0.65em;
+  }
+
+  .user-details.dark {
+    color: ${lightTurqoise};
+  }
+
+  .user-details.light {
+    color: ${black};
+  }
+
+  .account-level {
+    font-size: 0.5em;
+    padding-left: 10px;
+    color: ${red};
+    font-weight: 600;
+    text-transform: uppercase;
+    white-space: nowrap;
+  }
 `;
