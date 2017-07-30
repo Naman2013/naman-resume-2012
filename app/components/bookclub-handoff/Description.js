b354fd9c159dfbbcceec5694a3613869c6e49a26
jmasse <jmasse860@gmail.com>
2017-07-30 10:21:14 -0400

    chore(updated description URLs to use AWS)

--- app/components/bookclub-handoff/Description.js
+++ app/components/bookclub-handoff/Description.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 import Modal from 'react-modal';
 import PersonDetail from '../../components/person-detail/PersonDetail';
 import { darkBlueGray, gray } from '../../styles/variables/colors';
@@ -10,61 +10,61 @@ const CONTRIBUTORS = [ // order matters
     firstName: 'Karen',
     lastName: 'Stevens',
     bio: 'Karen Stevens exchanged her urban planning career for motherhood, moving to Connecticut to raise her children. A speedy and omnivorous reader, she has expanded her love of reading to encompass editing and writing. The thrill of the universe’s limitless horizons and how they relate and influence human culture has become a bit of an obsession and she is eager to share that excitement with Slooh’s community.',
-    photo: 'assets/images/bookclub/headshots/Karen.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Karen.jpg',
   },
   {
     firstName: 'Margaret',
     lastName: 'Atwood',
     bio: 'Canadian author Margaret Atwood (born in 1939) has produced over 40 books - novels, poetry, short stories, commentary and criticism. She has called some of her work “speculative fiction” rather than science fiction; it often explores feminism, environmentalism, and social justice. Atwood’s critical acclaim has been matched by her popularity. Her 1985 novel of a near future dystopia, The Handmaid’s Tale, has been adapted for the screen several times.',
-    photo: 'assets/images/bookclub/headshots/Atwood.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Atwood.jpg',
   },
   {
     firstName: 'J.G.',
     lastName: 'Ballard',
     bio: 'J.G. Ballard (1930-2009) is the British author of short stories, novels, and essays. He drew on his childhood in Shangai during World War II in the novel Empire of the Sun, which Stephen Spielberg adapted into film. Ballard’s distinctive literary obsessions and themes have resulted in the adjective “Ballardian”, which Collins English Dictionary describes as “dystopian modernity, bleak man-made landscapes and the psychological effects of technological, social or environmental developments.”  ',
-    photo: 'assets/images/bookclub/headshots/Ballard.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Ballard.jpg',
   },
   {
     firstName: 'Ray',
     lastName: 'Bradbury',
     bio: 'Over a nearly seventy year career, beloved American author Ray Bradbury (1920-2012) produced many now-classic works of science fiction, including Fahrenheit 451 and The Martian Chronicles. His works have been translated into over 40 languages and adapted for film and television.',
-    photo: 'assets/images/bookclub/headshots/Bradbury.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Bradbury.jpg',
   },
   {
     firstName: 'Don',
     lastName: 'DeLillo',
     bio: `Don DeLillo was born in New York in 1936. Among the finest American writers working today, he is a novelist, playwright, essayist, and short story writer. His work began to receive a wide audience in the eighties with White Noise, Libra and Underworld. DeLillo employs complex language to explore the state of the modern individual and society. In a 2012 interview with the Chicago Tribune he said, “I think my work is influenced by the fact that we're living in dangerous times. If I could put it in a sentence, in fact, my work is about just that: living in dangerous times.”`,
-    photo: 'assets/images/bookclub/headshots/Delillo.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Delillo.jpg',
   },
   {
     firstName: 'Phillip',
     lastName: 'Dick',
     bio: 'Works including Ubik, The Man in the High Castle, and Do Androids Dream of Electric Sheep? established Phillip K. Dick’s (1928-1982) reputation as an original thinker and hugely influential science fiction author. His writing often explored the nature of reality and personal identity. A chaotic personal life, drug use, and mental instability didn’t slow his output: during his short but prolific career he produced 45 novels and 121 short stories. Many of his works have been adapted for film and television, giving Dick an outsize influence on popular culture.',
-    photo: 'assets/images/bookclub/headshots/Dick.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Dick.jpg',
   },
   {
     firstName: 'Primo',
     lastName: 'Levi',
     bio: 'Italian writer and Holocaust survivor Primo Levi (1919-1987) was educated as a chemist.  During World War II he survived Auschwitz and within a year of his liberation, he had returned to Italy, found work as a chemist and begun to write an account of his survival, If This is a Man. While continuing to work as a chemist, he composed books, poetry, short stories and essays. “He was profoundly in touch with the most endearing human events and with the most contemptible,” Phillip Roth wrote of him.',
-    photo: 'assets/images/bookclub/headshots/Levi.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Levi.jpg',
   },
   {
     firstName: 'John',
     lastName: 'Updike',
     bio: 'During his long career, American novelist, short story writer, poet, and critic John Updike (1932-2009) found both popular and critical success. Two-time winner of the Pulitzer, Updike is best known for his Rabbit series. His beautifully crafted prose often explored the emotional state of the middle class. ',
-    photo: 'assets/images/bookclub/headshots/Updike.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Updike.jpg',
   },
   {
     firstName: 'Kurt',
     lastName: 'Vonnegut',
     bio: 'Kurt Vonnegut was born in Indianapolis in 1922. With his sixth novel, the anti-war Slaughterhouse-Five, he found wide commercial and critical success. It was based on his World War II experiences and made famous the refrain, “So it goes.” His work often employed science fiction elements and biting humor as social commentary. Vonnegut died in 2007.',
-    photo: 'assets/images/bookclub/headshots/Vonnegut.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Vonnegut.jpg',
   },
   {
     firstName: 'Virginia',
     lastName: 'Woolf',
     bio: 'The author of modernist classics To the Lighthouse, Mrs. Dalloway, Orlando and The Waves, Virginia Woolf is considered one the twentieth century’s most important writers. Born in 1882 to a well-connected London family, Woolf became part of the Bloomsbury Group of British intellectuals and artists and published her first novel in her early thirties. Her fertile creative phases alternated with periods of depression and she took her own life in 1941.',
-    photo: 'assets/images/bookclub/headshots/Woolf.jpg',
+    photo: 'https://vega.slooh.com/assets/images/bookclub/headshots/Woolf.jpg',
   },
 ];
 
