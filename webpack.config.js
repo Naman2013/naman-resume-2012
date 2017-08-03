b52f6abe078a862c6ab642d845663f5bfd159eaf
jjmasse <jmasse860@gmail.com>
2017-08-03 16:27:30 -0400

    experiment(messing with webpack configuration)

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,7 @@ module.exports = {
   },
   output: {
     path: `${__dirname}/dist`,
+    publicPath: path.join(__dirname, '/assets/'),
     filename: '[name].js',
     sourceMapFilename: '[name].js.map',
   },
@@ -139,7 +140,9 @@ module.exports = {
   ],
   devtool: 'cheap-module-eval-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname + '/'),
+    contentBase: path.join(__dirname, '/dist'),
+    publicPath: path.join(__dirname, '/assets/'),
+    compress: true,
     historyApiFallback: true,
     proxy: {
       '/api/**': {
