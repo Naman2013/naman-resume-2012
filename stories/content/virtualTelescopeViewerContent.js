c41d1509c1165f5d53aa99452f463b9a05e734ed
jjmasse <jmasse860@gmail.com>
2017-09-19 19:20:03 -0400

    chore(adding content for continuing work with the new viewer)

---
+++ stories/content/virtualTelescopeViewerContent.js
@@ -0,0 +1,18 @@
+export default {
+  unixTimestamp: Math.floor(new Date().getTime() / 1000),
+  coordinateArray: [
+    'Celestial Coordinates:',
+    'Right Ascension: 6h 39m 08s',
+    'Declination: +8&deg; 44&apost; 17&quot;',
+  ],
+  missionData: [
+    'Type: reflection nebula',
+    'Constellation: Monoceros',
+    'Magnitude: 10.00',
+    'Size: 3.33',
+    'Distance: 2,500 light years',
+  ],
+  objectTitleShort: 'Hubble\'s Variable Nebula (NGC 2261)',
+  processing: 'Processing: Default',
+  schedulingMember: 'Scheduling Member: SamuelD.2004',
+};
