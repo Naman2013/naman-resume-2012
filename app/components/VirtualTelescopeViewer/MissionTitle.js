7337b90a780f0aec9823bea982089f507d4c4fc2
jjmasse <jmasse860@gmail.com>
2017-09-21 08:57:20 -0400

    fix(when there is no mission title, we no longer display the green border)

--- app/components/VirtualTelescopeViewer/MissionTitle.js
+++ app/components/VirtualTelescopeViewer/MissionTitle.js
@@ -10,29 +10,30 @@ const defaultProps = {
   title: '',
 };
 
-const MissionTitle = ({ title }) => (
-  <div className="root">
-    {
-      title &&
-        <h5 className="content">{title}</h5>
-    }
+const MissionTitle = ({ title }) => {
+  if (!title) { return null; }
 
-    <style jsx>{`
-      .root {
-        text-align: center;
-        border: 1px solid ${brightGreen};
-        width: 40%;
-        margin: 0 auto 20px auto;
-        padding: 5px 10px;
-      }
+  return (
+    <div className="root">
+      <h5 className="content">{title}</h5>
 
-      .content {
-        padding: 0;
-        margin: 0;
-      }
-    `}</style>
-  </div>
-);
+      <style jsx>{`
+        .root {
+          text-align: center;
+          border: 1px solid ${brightGreen};
+          width: 40%;
+          margin: 0 auto 20px auto;
+          padding: 5px 10px;
+        }
+
+        .content {
+          padding: 0;
+          margin: 0;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 MissionTitle.propTypes = propTypes;
 MissionTitle.defaultProps = defaultProps;
