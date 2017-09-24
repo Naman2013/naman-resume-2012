bd205f03683d0ab44e79a92436999f0f9308bfa6
jmasse <jmasse860@gmail.com>
2017-09-24 10:35:52 -0400

    chore(fixing up the app core container component)

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,12 +1,21 @@
-import React, { Component, cloneElement } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import NotificationSystem from 'react-notification-system';
-import notificationStyles from '../components/common/notification-center/inline-styles';
 import Menu from './Menu';
 import Header from '../components/common/header';
 import Footer from '../components/common/footer';
 import { fetchEvents } from '../modules/upcoming-events/upcoming-events-actions';
+
+const propTypes = {
+  isLanding: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+  fetchEvents: PropTypes.func.isRequired,
+};
+
+const defaultProps = {
+  isLanding: false,
+};
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({
@@ -25,48 +34,17 @@ class App extends Component {
   constructor(props) {
     super(props);
     props.fetchEvents();
-    this.notificationSystem = null;
-  }
-
-  componentDidMount() {
-    this.notificationSystem = this.refs.notificationSystem;
-  }
-
-  notifySuccess({ title, message }) {
-    this.notificationSystem.addNotification({
-      title,
-      message,
-      level: 'success',
-      autoDismiss: 0,
-    });
-  }
-
-  notifyError({ title, message }) {
-    this.notificationSystem.addNotification({
-      title,
-      message,
-      level: 'error',
-      autoDismiss: 0,
-    });
   }
 
   render() {
     const { isLanding } = this.props;
     return (
       <div className={`wrapper ${isLanding ? 'is-landing' : null}`}>
-        <NotificationSystem ref="notificationSystem" style={notificationStyles} />
         <Header />
         <Menu source="nav.json" />
         <section className="app-content-container clearfix">
           <div className="clearfix">
-            {
-              cloneElement(this.props.children, {
-                notification: {
-                  success: this.notifySuccess,
-                  error: this.notifyError,
-                },
-              })
-            }
+            { this.props.children }
           </div>
         </section>
         <Footer />
@@ -74,5 +52,8 @@ class App extends Component {
     );
   }
 }
+
+App.propTypes = propTypes;
+App.defaultProps = defaultProps;
 
 export default App;
