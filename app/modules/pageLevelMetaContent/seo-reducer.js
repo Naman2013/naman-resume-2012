44082ec4464733cdcbd04aa7c656a5d52151e3dc
jmasse <jmasse860@gmail.com>
2017-09-24 12:28:09 -0400

    feat(setting up a page title action)

--- app/modules/pageLevelMetaContent/seo-reducer.js
+++ app/modules/pageLevelMetaContent/seo-reducer.js
@@ -1,7 +1,16 @@
 import createReducer from '../utils/createReducer';
 
+import { SET_PAGE_TITLE } from './seo-actions';
+
 export const initialState = {
   pageTitle: 'Slooh',
 };
 
-export default createReducer(initialState, {});
+export default createReducer(initialState, {
+  [SET_PAGE_TITLE](state, { title }) {
+    return ({
+      ...state,
+      title,
+    });
+  },
+});
