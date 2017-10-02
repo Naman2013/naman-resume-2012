692759d0dc316a538035560c0205b4b8915a6915
jjmasse <jmasse860@gmail.com>
2017-10-02 17:57:52 -0400

    feat(fixed up the button interaction bit)

--- app/components/telescope-details/neoview/neoview.js
+++ app/components/telescope-details/neoview/neoview.js
@@ -132,6 +132,9 @@ export default class Neoview extends Component {
             background: transparent;
             padding: 0;
             margin: 0;
+            width: 100%;
+            height: 100%;
+            text-align: left;
           }
         `}</style>
       </div>
