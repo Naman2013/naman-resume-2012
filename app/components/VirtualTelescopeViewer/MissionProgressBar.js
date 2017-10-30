f85cd3c8d2bf76c6fa948c8d8795b28cde363e60
jjmasse <jmasse860@gmail.com>
2017-10-30 17:34:01 -0400

    feat(adding DOM fragment that represents the fill of the progress bar)

--- app/components/VirtualTelescopeViewer/MissionProgressBar.js
+++ app/components/VirtualTelescopeViewer/MissionProgressBar.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { brightGreen } from '../../styles/variables/colors';
 
-const MissionProgressBar = () => (
-  <div className="root">
-    <style jsx>{`
-      .root {
-        border: 1px solid ${brightGreen};
-        height: 100px;
-        width: 5px;
-      }
-    `}</style>
-  </div>
-);
+/**
+  X/Y * 100 = Z
+  X = elapsed time
+  Y = duration
+  Z = number in percentage completed
+*/
+
+class MissionProgressBar extends Component {
+  render() {
+    return (
+      <div className="root">
+        <div className="progress-bar" />
+
+        <style jsx>{`
+          .root {
+            border: 1px solid ${brightGreen};
+            height: 100px;
+            width: 5px;
+            transform: rotate(180deg);
+          }
+
+          .progress-bar {
+            width: 100%;
+            background-color: ${brightGreen}
+            height: 20%;
+
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
 
 export default MissionProgressBar;
