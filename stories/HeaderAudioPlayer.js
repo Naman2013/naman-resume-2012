e55a2b32d72e410cfbce9ea413cd255b2a6a58c5
jjmasse <jmasse860@gmail.com>
2017-11-13 08:25:47 -0500

    chore(setting up audio player story)

---
+++ stories/HeaderAudioPlayer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Header Audio Player', module)
+  .add('LIVE audio', () => (
+    <div>
+      <h1>Audio player story configured...</h1>
+    </div>
+  ));
--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,6 @@
 import './UpcomingMissionList';
 import './Headers';
+import './HeaderAudioPlayer';
 import './Timestamp';
 import './VirtualTelescopeViewer';
 import './Tabs';
