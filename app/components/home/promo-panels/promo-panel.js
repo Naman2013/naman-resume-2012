d072cfedb06dacaa48dc1bee6705135d37790dd2
Todd Reisel <treisel@coastalbt.com>
2017-11-19 23:15:33 -0500

    promo/info panel button/link routing.

--- app/components/home/promo-panels/promo-panel.js
+++ app/components/home/promo-panels/promo-panel.js
@@ -1,78 +1,83 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import useAbsoluteURL from '../../../utils/useAbsoluteURL';
+import purgeHashURL from '../../../utils/purgeHashURL';
+
 import { white, lightGray, darkBlueGray } from '../../../styles/variables/colors';
 
 /********************************************************************
 * Class: PromoPanel
 * Description: An individual promotional or informational panel
 ********************************************************************/
+function generateLink(URL = '', content = '') {
+  if (useAbsoluteURL(URL)) {
+    return (
+      <a className="button btn-primary" href={URL}>{content}</a>
+    );
+  }
+
+  return (
+    <Link className="button btn-primary" to={purgeHashURL(URL)}>{content}</Link>
+  );
+}
+
 class PromoPanel extends Component {
   render() {
-	const inlineStyle_PromoContainer = {
-    position: 'relative',
-		minHeight: '500px',
-    maxHeight: '500px',
-		minWidth: '100%',
-    marginTop: '0px',
-    marginBottom: '0px',
-    paddingTop: '0px',
-    paddingBottom: '0px',
-    backgroundColor: `${lightGray}`,
-	};
 
-  const inlineStyle_Heading = {
-		color: `${this.props.headingColorRGB}`,
-    textAlign: 'center',
-    marginTop: '0px',
-    paddingTop: '20px',
-	};
+  	const inlineStyle_PromoContainer = {
+      position: 'relative',
+  		minHeight: '500px',
+      maxHeight: '500px',
+  		minWidth: '100%',
+      marginTop: '0px',
+      marginBottom: '0px',
+      paddingTop: '0px',
+      paddingBottom: '0px',
+      backgroundColor: `${lightGray}`,
+  	};
 
-	const inlineStyle_SubHeading = {
-		color: `${this.props.subheadColorRGB}`,
-    textAlign: 'center',
-    marginTop: '0px',
-    paddingTop: '20px',
-	};
-
-
-  const inlineStyle_promo = {
-    background: `url(${this.props.imageURL}) center center no-repeat`,
-    minHeight: '500px',
-    maxHeight: '500px',
-    minWidth: '100%',
-  };
-
-  const inlineStyle_checkItOutButton_Promo = {
-      position: 'absolute',
-      top: '80%',
+    const inlineStyle_Heading = {
+  		color: `${this.props.headingColorRGB}`,
       textAlign: 'center',
-      fontSize: '1.2em',
-      fontWeight: 'bold',
-      marginLeft: 'auto',
-      marginBottom: 'auto',
-  };
+      marginTop: '0px',
+      paddingTop: '20px',
+  	};
 
-  const inlineStyle_checkItOutButton_Info = {
-      position: 'absolute',
-      top: '80%',
-      fontSize: '1.2em',
-      fontWeight: 'bold',
+  	const inlineStyle_SubHeading = {
+  		color: `${this.props.subheadColorRGB}`,
       textAlign: 'center',
-      marginLeft: 'auto',
-      marginBottom: 'auto',
-  };
+      marginTop: '0px',
+      paddingTop: '20px',
+  	};
 
-  const inlineStyle_info = {
-    position: 'absolute',
-    minHeight: '400px',
-    maxHeight: '400px',
-    minWidth: '60%',
-    maxWidth: '60%',
-    marginLeft: '20%',
-    marginRight: '20%',
-    top: '10%',
-    backgroundColor: `${white}`,
-  };
+    const inlineStyle_promo = {
+      background: `url(${this.props.imageURL}) center center no-repeat`,
+      minHeight: '500px',
+      maxHeight: '500px',
+      minWidth: '100%',
+    };
+
+    const inlineStyle_checkItOutButton_PromoDIV = {
+      position: 'relative',
+      minWidth: '100%',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      textAlign: 'center',
+      marginTop: '20%',
+    }
+
+    const inlineStyle_info = {
+      position: 'absolute',
+      minHeight: '400px',
+      maxHeight: '400px',
+      minWidth: '60%',
+      maxWidth: '60%',
+      marginLeft: '20%',
+      marginRight: '20%',
+      top: '10%',
+      backgroundColor: `${white}`,
+    };
 
     return (
     	<div style={inlineStyle_PromoContainer}>
@@ -80,7 +85,9 @@ class PromoPanel extends Component {
     		      <div style={inlineStyle_promo}>
             		<h2 style={inlineStyle_Heading}>{this.props.heading}</h2>
     		        <h3 style={inlineStyle_SubHeading}>{this.props.subhead}</h3>
-                <a style={inlineStyle_checkItOutButton_Promo} href={this.props.buttonLink} className="button btn-primary">{this.props.buttonText}</a>
+                <div style={inlineStyle_checkItOutButton_PromoDIV}>
+                  {generateLink(this.props.buttonLink, this.props.buttonText)}
+                </div>
           		</div>
     	      }
 
@@ -88,7 +95,7 @@ class PromoPanel extends Component {
     		      <div style={inlineStyle_info}>
             		<h2 style={inlineStyle_Heading}>{this.props.heading}</h2>
     		        <h3 style={inlineStyle_SubHeading}>{this.props.subhead}</h3>
-                <a style={inlineStyle_checkItOutButton_Info} href={this.props.buttonLink} className="button btn-primary">{this.props.buttonText}</a>
+                {generateLink(this.props.buttonLink, this.props.buttonText)}
           		</div>
     	      }
     	</div>
