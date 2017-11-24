f7ef8c08bc81cbce9dae751720c23d4a40c6ab9a
jmasse <jmasse860@gmail.com>
2017-11-24 12:38:05 -0500

    feat(expiration of the service call is now wired up)

--- app/modules/get-audio-player/actions.js
+++ app/modules/get-audio-player/actions.js
@@ -1,12 +1,29 @@
+import moment from 'moment';
 import getAudioPlayer from '../../services/shows/get-audio-player';
+import store from '../../store';
 
 export const START_FETCH_PLAYER = 'START_FETCH_PLAYER';
 export const SUCCESS_FETCH_PLAYER = 'SUCCESS_FETCH_PLAYER';
 export const FAIL_FETCH_PLAYER = 'FAIL_FETCH_PLAYER';
 
-const globalFetchTimer = null;
+let expiresTimer = null;
 
-function setExpiresTimer() {}
+function refreshTimer(pageURL, expires = 0) {
+  clearTimeout(expiresTimer);
+  const convertedExpiresToMilli = expires * 1000;
+  const now = moment.utc().valueOf();
+  const time = convertedExpiresToMilli - now;
+
+  if (time > 100) {
+    expiresTimer = window.setTimeout(() => {
+      store.dispatch(fetchPlayer({ pageURL }));
+    }, time);
+  }
+}
+
+function setExpiresTimer(pageURL, { expires }) {
+  refreshTimer(pageURL, expires);
+}
 
 const startFetchPlayer = () => ({
   type: START_FETCH_PLAYER,
@@ -25,12 +42,13 @@ const failFetchPlayer = payload => ({
 export const fetchPlayer = ({ pageURL }) => (dispatch, getState) => {
   const { at, cid, token } = getState().user;
   dispatch(startFetchPlayer());
+
   getAudioPlayer({ at, cid, token, pageSource: pageURL })
     .then((result) => {
+      setExpiresTimer(pageURL, result.data);
       dispatch(successFetchPlayer(result.data));
     })
     .catch((error) => {
-      console.log(error);
       dispatch(failFetchPlayer(error));
     });
 };
