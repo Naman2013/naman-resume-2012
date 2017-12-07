ad71a1f587f2c62f5b314a897150a001976ef8a6
jjmasse <jmasse860@gmail.com>
2017-12-07 10:59:56 -0500

    chore(updating CTA to Check Mission)

--- app/components/common/recommendations/PiggybackStatus.js
+++ app/components/common/recommendations/PiggybackStatus.js
@@ -120,7 +120,7 @@ class PiggybackStatus extends Component {
 
     if (!piggybackAvailable && !newMissionMode) {
       return (
-        <button onClick={handleLoadReservationClick} className="btn-primary">Make Reservation</button>
+        <button onClick={handleLoadReservationClick} className="btn-primary">Check Reservation</button>
       );
     }
 
