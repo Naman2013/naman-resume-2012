b1aaf3e68523ca3df1712ceae9b8869e0649f0a1
jmasse <jmasse860@gmail.com>
2017-12-09 12:09:44 -0500

    feat(adding open graph meta data) - new routine that generates open graph meta tags - configuring some default content - applying the meta management to the static page container

--- app/components/PageMetaManagement/PageMetaManagement.js
+++ app/components/PageMetaManagement/PageMetaManagement.js
@@ -3,24 +3,43 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 
-import { initialState } from '../../modules/pageLevelMetaContent/seo-reducer';
+import { initialState as defaultApplicationMetaData } from '../../modules/pageLevelMetaContent/seo-reducer';
+
+const SUPPORT_OPEN_GRAPH_PROPERTIES = ['title', 'description', 'image', 'type'];
 
 const propTypes = {
   pageTitle: PropTypes.string,
+  og: PropTypes.shape({
+    title: PropTypes.string,
+  }),
 };
 
-const defaultProps = { ...initialState };
+const defaultProps = { ...defaultApplicationMetaData };
 
 const mapStateToProps = ({ pageLevelMetaContent }) => ({
   pageLevelMetaContent,
 });
 
+function generateOpenGraphTags(ogTags = {}) {
+  return SUPPORT_OPEN_GRAPH_PROPERTIES.map(
+    graphProperty => ogTags[graphProperty] &&
+      <meta
+        key={`og:${graphProperty}`}
+        property={`og:${graphProperty}`}
+        content={ogTags[graphProperty]}
+      />,
+    );
+}
+
 @connect(mapStateToProps, null)
 class PageMetaManagement extends Component {
   render() {
     return (
-      <Helmet>
+      <Helmet
+        titleTemplate="%s | Slooh.com"
+      >
         <title>{this.props.pageTitle}</title>
+        { generateOpenGraphTags(this.props.og) }
       </Helmet>
     );
   }
