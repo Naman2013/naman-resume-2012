d7043398783f0ddb2e5d12b9f9e09867b91d94b6
jmasse <jmasse860@gmail.com>
2017-12-11 18:01:47 -0500

    chore(adding reset action and finishing adding vanilla meta fields)

--- app/modules/pageLevelMetaContent/seo-actions.js
+++ app/modules/pageLevelMetaContent/seo-actions.js
@@ -1,4 +1,9 @@
 export const SET_PAGE_TITLE = 'SET_PAGE_TITLE';
+export const RESET_PAGE_META = 'RESET_PAGE_META';
+
+export const resetPageMeta = () => ({
+  type: RESET_PAGE_META,
+});
 
 export const setPageTitle = title => ({
   type: SET_PAGE_TITLE,
