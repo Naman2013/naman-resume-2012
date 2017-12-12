60a1cfe3e5629ac2c6b35f1fb86213895a507377
jjmasse <jmasse860@gmail.com>
2017-12-12 08:28:46 -0500

    feat(new actions designed to set page level open graph and standard tags)

--- app/modules/pageLevelMetaContent/seo-actions.js
+++ app/modules/pageLevelMetaContent/seo-actions.js
@@ -1,5 +1,17 @@
 export const SET_PAGE_TITLE = 'SET_PAGE_TITLE';
+export const SET_STANDARD_META = 'SET_STANDARD_META';
+export const SET_OPEN_GRAPH_META = 'SET_OPEN_GRAPH_META';
 export const RESET_PAGE_META = 'RESET_PAGE_META';
+
+export const setOpenGraphMeta = payload => ({
+  type: SET_OPEN_GRAPH_META,
+  payload,
+});
+
+export const setStandardMeta = payload => ({
+  type: SET_STANDARD_META,
+  payload,
+});
 
 export const resetPageMeta = () => ({
   type: RESET_PAGE_META,
