d542b688d3629319c37dfe575b988a0b8b85e23e
jjmasse <jmasse860@gmail.com>
2017-12-12 08:19:51 -0500

    chore(resetting page meta on page change)

--- app/route-functions/globalOnRouteUpdate.js
+++ app/route-functions/globalOnRouteUpdate.js
@@ -1,7 +1,9 @@
 import store from '../store';
 import { deactivateMenu } from '../modules/menu/actions';
+import { resetPageMeta } from '../modules/pageLevelMetaContent/seo-actions';
 
 export default function globalOnRouteUpdate() {
   window.scrollTo(0, 0);
   store.dispatch(deactivateMenu());
+  store.dispatch(resetPageMeta());
 }
