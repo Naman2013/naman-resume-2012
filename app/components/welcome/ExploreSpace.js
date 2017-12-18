8dd849660a6ec7b76f37fee7723ba37346a21e7d
Todd Reisel <treisel@coastalbt.com>
2017-12-18 11:32:08 -0500

    welcome page: explore space, ready for first look.

--- app/components/welcome/ExploreSpace.js
+++ app/components/welcome/ExploreSpace.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { black, white, lightGray, darkBlueGray } from '../../styles/variables/colors';
 
 class ExploreSpace extends Component {
   constructor(props) {
@@ -11,6 +12,11 @@ class ExploreSpace extends Component {
       <div className="explorespace-container">
         <div className="explorespace-container-overlay">
           <div className="explorespace-container-overlayimage"/>
+          <div className="explorepace-titlecontainer">
+            <h3 className="subtitle explorespace-title-1">LEARN TO</h3>
+            <h2 className="subtitle explorespace-title-2">EXPLORE</h2>
+            <h1 className="title big explorespace-title-3">SPACE</h1>
+          </div>
           <div className="explorespace-sphereimagecontainer">
             <img src="../../../assets/images/welcome/Spheres.png"/>
             <br/>
@@ -19,12 +25,31 @@ class ExploreSpace extends Component {
         </div>
 
         <style jsx>{`
+
+          .explorespace-title-1 {
+            color: ${white};
+            font-size: 1em;
+            font-weight: bold;
+          }
+
+          .explorespace-title-2 {
+            color: ${white};
+            font-size: 2em;
+            font-weight: bold;
+          }
+
+          .explorespace-title-3 {
+            color: ${white};
+            font-size: 5em;
+            font-weight: bold;
+          }
+
           .explorespace-container {
-            background-image: -webkit-linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryNight_Dark.png);
-            background-image:  -moz-linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryNight_Dark.png);
-            background-image:  -o-linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryNight_Dark.png);
-            background-image:  -ms-linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryNight_Dark.png);
-            background-image:  linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryNight_Dark.png);
+            background-image: -webkit-linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryBG.png);
+            background-image:  -moz-linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryBG.png);
+            background-image:  -o-linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryBG.png);
+            background-image:  -ms-linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryBG.png);
+            background-image:  linear-gradient(rgba(0,0,0, 0) 0%,rgba(0,0,0, 1) 100%), url(../../../assets/images/welcome/StarryBG.png);
             min-height: 768px;
             height: 768px;
             z-index: 0;
@@ -50,11 +75,15 @@ class ExploreSpace extends Component {
             display: inline-block;
             bottom: 0;
             position: absolute;
-            background-blend-mode: multiply;
+          }
+
+          .explorepace-titlecontainer {
+            min-width: 100%;
+            text-align: center;
           }
 
           .explorespace-sphereimagecontainer {
-            z-index: 3;
+            z-index: 4;
             margin-left: auto;
             margin-right: auto;
             min-width: 100%;
