ed6224c36d0955f24d842e7312402e035bac695d
jmasse <jmasse860@gmail.com>
2017-12-27 09:49:30 -0500

    chore(changing the piggyback hover content)

--- app/constants/defaults.js
+++ app/constants/defaults.js
@@ -1,4 +1,4 @@
 export default {
   META_COVER_IMAGE: 'https://vega.slooh.com/icons/sharing/slooh_logo_sky_Square.jpg',
-  PIGGYBACK_SHORT_DESCRIPTION: 'By piggybacking another member’s mission, you can collect images from it even when you can’t be here during the mission to take the photos yourself',
+  PIGGYBACK_SHORT_DESCRIPTION: 'The pictures from this mission will be auto saved to your My Pictures and you will receive email notification when they are ready.',
 };
