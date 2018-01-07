de3f93bb2bb5384beab9048970349d276abac49d
jmasse <jmasse860@gmail.com>
2018-01-07 15:24:58 -0500

    chore(adding logging for testing)

--- app/monitoring.js
+++ app/monitoring.js
@@ -1,5 +1,15 @@
 import requestRestart from './utils/crash-application';
 
 window.onerror = function sloohWindowError(event, source, lineno, colno, error) {
+  console.log(event);
+  console.log('=========');
+  console.log(source);
+  console.log('=========');
+  console.log(lineno);
+  console.log('=========');
+  console.log(colno);
+  console.log('=========');
+  console.log(error);
+  console.log('=========');
   requestRestart();
 };
