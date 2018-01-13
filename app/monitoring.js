c6a115f743cfd7809605baa781fc6ef1a76176e4
jmasse <jmasse860@gmail.com>
2018-01-13 13:30:32 -0500

    feat(adding generic warning to notify that the application had in fact detected a crash)

--- app/monitoring.js
+++ app/monitoring.js
@@ -1,6 +1,7 @@
 import requestRestart from './utils/crash-application';
 
 window.onerror = function sloohWindowError(msg, url, lineNo, columnNo, error) {
+  console.warn('CRASH: Application crash detected.');
   const BLACK_LIST = ['bootstrap'];
   for (let i = 0; i < BLACK_LIST.length; i += 1) {
     if (msg.indexOf(BLACK_LIST[i].length) >= 0) {
