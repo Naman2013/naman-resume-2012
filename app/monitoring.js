cd3702c577ffeb6156a6a43ad87cb3a256cc7541
jmasse <jmasse860@gmail.com>
2018-01-13 13:55:42 -0500

    fix(now failing for all errors)

--- app/monitoring.js
+++ app/monitoring.js
@@ -2,11 +2,5 @@ import requestRestart from './utils/crash-application';
 
 window.onerror = function sloohWindowError(msg, url, lineNo, columnNo, error) {
   console.warn('CRASH: Application crash detected.');
-  const BLACK_LIST = ['bootstrap'];
-  for (let i = 0; i < BLACK_LIST.length; i += 1) {
-    if (msg.indexOf(BLACK_LIST[i].length) >= 0) {
-      requestRestart();
-      break;
-    }
-  }
+  requestRestart();
 };
