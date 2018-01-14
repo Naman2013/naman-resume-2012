a9c5db23b3ffaa67133116f1f988b3fe4ac2beec
jmasse <jmasse860@gmail.com>
2018-01-14 12:01:10 -0500

    doc(adding TODOs for these new expires components)

--- app/components/common/expiring-components/expire-service.js
+++ app/components/common/expiring-components/expire-service.js
@@ -3,6 +3,11 @@
   calls an API when mounted
   expects the API to return an expiration time and server timestamp
   when expiration occurs will refetch from the API
+
+  TODO: use axios to make service call
+  TODO: set timeout based on timestamp and expires
+  TODO: refetch API data on expires
+  TODO: tear down the timeout when the component unmounts
 */
 
 import React, { Component, cloneElement } from 'react';
--- app/components/common/expiring-components/expire.js
+++ app/components/common/expiring-components/expire.js
@@ -2,6 +2,9 @@
   when the server timestamp and the expiration
   timestamps are known and we would like a callback
   to be called on expiration
+
+  TODO: use expires and timestamp to create timer
+  TODO: on timer expiration, call the callback
 */
 
 import React, { Component } from 'react';
@@ -11,11 +14,13 @@ class Expire extends Component {
   static propTypes = {
     expires: PropTypes.number.isRequired,
     timestamp: PropTypes.number.isRequired,
+    onExpires: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
   };
 
   render() {
     return (
-
+      this.props.children
     );
   }
 }
