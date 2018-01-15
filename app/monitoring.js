37859f3ab63ed92bce723ddc29a5c45dfde1e38a
jmasse860@gmail.com <jmasse860@gmail.com>
2018-01-15 09:43:23 -0500

    chore(commenting the crash method)

--- app/monitoring.js
+++ app/monitoring.js
@@ -2,5 +2,5 @@ import requestRestart from './utils/crash-application';
 
 window.onerror = function sloohWindowError(msg, url, lineNo, columnNo, error) {
   console.warn('CRASH: Application crash detected.');
-  requestRestart();
+  // requestRestart();
 };
