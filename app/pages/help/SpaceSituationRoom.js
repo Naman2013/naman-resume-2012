6ea080c3176dee07b11ec489d3a2e2c325ff528a
Edward <ed@slooh.com>
2018-01-16 17:35:18 -0500

    remove contact buttons

--- app/pages/help/SpaceSituationRoom.js
+++ app/pages/help/SpaceSituationRoom.js
@@ -6,9 +6,6 @@ const Community = () => (
   <div>
     <div className="help-page-header">
       <h1 className="help-page-title">Space Situation Room</h1>
-      <Link to="/about/contact" className="button btn-primary help-page-button">
-        Contact Us
-      </Link>
     </div>
     <div className="help-page-inner">
       <div className="help-page-section">
--- app/pages/help/TermsAndConditions.js
+++ app/pages/help/TermsAndConditions.js
@@ -6,9 +6,6 @@ const TermsAndConditions = () => (
   <div>
     <div className="help-page-header">
       <h1 className="help-page-title">Terms and Conditions</h1>
-      <Link to="/about/contact" className="button btn-primary help-page-button">
-        Contact Us
-      </Link>
     </div>
     <div className="help-page-inner">
       <div className="help-page-section">
