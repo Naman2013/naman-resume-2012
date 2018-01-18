08b91e162e1849a19e21c2fbd8e399216b469375
jmasse860@gmail.com <jmasse860@gmail.com>
2018-01-18 08:29:48 -0500

    chore(linting card front)

---
+++ app/components/TelescopeThumbnailView/TelescopeThumbnailView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+/**
+  TODO: paint top container and bottom container
+  TODO: takes top image and bottom image and applies them as background images
+  TODO: creates an SSE connection to source
+  TODO: disconnect from EventSource on dismount of component
+*/
