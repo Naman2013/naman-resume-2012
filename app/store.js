ce8f88297df6ca7294c38e2ada28a7ac8889dd3c
jmasse <jmasse860@gmail.com>
2018-02-11 22:43:54 -0500

    fix(router middleware function for API validation)

--- app/store.js
+++ app/store.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 import reducers from './modules/Reducers';
 import callAPIMiddleware from './modules/middleware/callAPIMiddleware';
 
@@ -11,6 +13,7 @@ function configureStore(initialState) {
       applyMiddleware(
         thunkMiddleware,
         callAPIMiddleware,
+        routerMiddleware(browserHistory),
       ),
       window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
     ),
