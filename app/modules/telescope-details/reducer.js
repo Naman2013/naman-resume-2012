dc1498e6af65ed114d8efbd5f896251f70c19941
jmasse <jmasse860@gmail.com>
2018-02-12 20:51:07 -0500

    feat(implementing reducer logic for keeping track of missions played)

--- app/modules/telescope-details/reducer.js
+++ app/modules/telescope-details/reducer.js
@@ -26,9 +26,10 @@ import {
   RESET_ACTIVE_SSE,
   REMOVE_IMAGE_VIEWER_CLIP_STATE,
   APPLY_IMAGE_VIEWER_CLIP_STATE,
-  START_FETCH_UPCOMING_MISSIONS,
-  SUCCESS_FETCH_UPCOMING_MISSIONS,
-  FAIL_FETCH_UPCOMING_MISSIONS,
+  INCREMENT_MISSION_COUNTER,
+  RESET_MISSION_COUNTER,
+  UPDATE_RECENTLY_VIEWED_MISSION_ID,
+  RESET_VIEWED_MISSION_STATE,
 } from './actions';
 
 
@@ -95,9 +96,37 @@ const initialState = {
   activeSSE: {
     astroObjectID: 0,
   },
+
+  viewedMissionsCounter: 0,
+  recentlyViewedMissionID: 0,
 };
 
 export default createReducer(initialState, {
+  [INCREMENT_MISSION_COUNTER](state) {
+    return {
+      ...state,
+      viewedMissionsCounter: state.viewedMissionsCounter + 1,
+    };
+  },
+  [RESET_MISSION_COUNTER](state) {
+    return {
+      ...state,
+      viewedMissionsCounter: 0,
+    };
+  },
+  [UPDATE_RECENTLY_VIEWED_MISSION_ID](state, { missionID }) {
+    return {
+      ...state,
+      recentlyViewedMissionID: missionID,
+    };
+  },
+  [RESET_VIEWED_MISSION_STATE](state) {
+    return {
+      ...state,
+      viewedMissionsCounter: 0,
+      recentlyViewedMissionID: 0,
+    };
+  },
   [SET_DISPLAY_COMMUNITY_CONTENT](state, { payload }) {
     return {
       ...state,
