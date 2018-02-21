900e47744f858bd90169e55004e88ced1d3a7048
jmasse860@gmail.com <jmasse860@gmail.com>
2018-02-20 20:26:08 -0500

    chore(refactoring to provide internal state)

--- app/components/telescope-details/MissionAudio/MissionAudio.js
+++ app/components/telescope-details/MissionAudio/MissionAudio.js
@@ -1,61 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SectionHeader from '../../common/headers/SectionHeader';
 
 const SECTION_TITLE = 'Mission Audio';
 
-const propTypes = {
-  missionAudioURL: PropTypes.string.isRequired,
-  audioEnabled: PropTypes.bool,
-};
+class MissionAudio extends Component {
+  static propTypes = {
+    missionAudioURL: PropTypes.string.isRequired,
+    audioEnabled: PropTypes.bool,
+  };
 
-const defaultProps = {
-  audioEnabled: true,
-};
+  static defaultProps = {
+    audioEnabled: true,
+  };
 
-const MissionAudio = ({ missionAudioURL, audioEnabled }) => (
-  <div className="root">
-    <SectionHeader title={SECTION_TITLE} />
+  state = {
+    displayPlayer: false,
+  };
 
-    <div className="player-container">
-      <form className="player" method="POST">
-        <input id="enable-player" type="checkbox" />
-        <label htmlFor="enable-player">Play Mission Audio</label>
-      </form>
+  render() {
+    const { missionAudioURL, audioEnabled } = this.props;
 
-      {
-        audioEnabled &&
-          <audio src={missionAudioURL} controls />
-      }
+    return (
+      <div className="root">
+        <SectionHeader title={SECTION_TITLE} />
 
-      {
-        !audioEnabled &&
-          <h4 className="disabled-text">Audio is not enabled for this mission.</h4>
-      }
-    </div>
+        <div className="player-container">
+          <form className="player" method="POST">
+            <input id="enable-player" type="checkbox" />
+            <label htmlFor="enable-player">Play Mission Audio</label>
+          </form>
 
-    <style jsx>{`
-      .player {
-        margin-bottom: 10px;
-      }
+          {
+            audioEnabled &&
+              <audio src={missionAudioURL} controls />
+          }
 
-      .disabled-text {
-        margin: 0;
-        padding: 0;
-      }
+          {
+            !audioEnabled &&
+              <h4 className="disabled-text">Audio is not enabled for this mission.</h4>
+          }
+        </div>
 
-      .player-container {
-        background-color: rgba(137, 137, 137, .5);
-        margin-bottom: 20px;
-        text-align: center;
-        padding: 20px 0;
-        margin-bottom: 10px;
-      }
-    `}</style>
-  </div>
-);
+        <style jsx>{`
+          .player {
+            margin-bottom: 10px;
+          }
 
-MissionAudio.propTypes = propTypes;
-MissionAudio.defaultProps = defaultProps;
+          .disabled-text {
+            margin: 0;
+            padding: 0;
+          }
+
+          .player-container {
+            background-color: rgba(137, 137, 137, .5);
+            margin-bottom: 20px;
+            text-align: center;
+            padding: 20px 0;
+            margin-bottom: 10px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
 
 export default MissionAudio;
