fe57113c1c407267177451e83d174ed0ac71eaac
jmasse860@gmail.com <jmasse860@gmail.com>
2018-02-20 19:53:37 -0500

    feat(adding state for when audio is not supported)

--- app/components/telescope-details/MissionAudio/MissionAudio.js
+++ app/components/telescope-details/MissionAudio/MissionAudio.js
@@ -6,30 +6,56 @@ const SECTION_TITLE = 'Mission Audio';
 
 const propTypes = {
   missionAudioURL: PropTypes.string.isRequired,
+  audioEnabled: PropTypes.bool,
 };
 
-const MissionAudio = ({ missionAudioURL }) => (
+const defaultProps = {
+  audioEnabled: true,
+};
+
+const MissionAudio = ({ missionAudioURL, audioEnabled }) => (
   <div className="root">
     <SectionHeader title={SECTION_TITLE} />
 
     <div className="player-container">
-      <form method="POST">
+      <form className="player" method="POST">
         <input id="enable-player" type="checkbox" />
         <label htmlFor="enable-player">Play Mission Audio</label>
       </form>
 
-      <audio src={missionAudioURL} controls />
+      {
+        audioEnabled &&
+          <audio src={missionAudioURL} controls />
+      }
+
+      {
+        !audioEnabled &&
+          <h4 className="disabled-text">Audio is not enabled for this mission.</h4>
+      }
     </div>
 
     <style jsx>{`
+      .player {
+        margin-bottom: 10px;
+      }
+
+      .disabled-text {
+        margin: 0;
+        padding: 0;
+      }
+
       .player-container {
         background-color: rgba(137, 137, 137, .5);
         margin-bottom: 20px;
+        text-align: center;
+        padding: 20px 0;
+        margin-bottom: 10px;
       }
     `}</style>
   </div>
 );
 
 MissionAudio.propTypes = propTypes;
+MissionAudio.defaultProps = defaultProps;
 
 export default MissionAudio;
