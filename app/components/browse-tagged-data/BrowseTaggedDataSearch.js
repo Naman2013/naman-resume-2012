4e2d12802c8278b9ead11ad01478d145ba2e3cf3
Todd Reisel <treisel@coastalbt.com>
2018-02-28 00:29:52 -0500

    top nav: browse/search: no search term browse complete.

--- app/components/browse-tagged-data/BrowseTaggedDataSearch.js
+++ app/components/browse-tagged-data/BrowseTaggedDataSearch.js
@@ -125,42 +125,67 @@ class BrowseTaggedDataSearch extends Component {
     //document.getElementById('BrowseTaggedDataSearchInputField').value = '';
   }
 
-  renderTaggedDataDisplay() {
-      const { topNavSearchTerm, topNavSearchEnabled, renderTaggedData } = this.state;
-      const { browseTaggedData } = this.props;
+    changeGrandParentNodeID(grandParentKey) {
+      const { grandParentNodeID } = this.state;
 
-      return(
-        <div className="search-results-set">
-          {renderTaggedData.taggedData &&
-            <div>
-              {Object.keys(renderTaggedData.taggedData).map(function (grandParentKey) {
-                  return (
-                    <div>
-                      <h1 className="search-results-grandparent">{renderTaggedData.taggedData[grandParentKey].title}</h1>
-                      {Object.keys(renderTaggedData.taggedData[grandParentKey].subnodes).map(function (parentKey) {
-                          return (
-                            <div>
-                              <h2 className="search-results-parent">{renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].title}</h2>
-                              {Object.keys(renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].subnodes).map(function (itemKey) {
-                                  return (
-                                    <div className="search-results-item">
-                                      <Link onClick={(event) => { this.endSearch(); }} to={`/${renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].subnodes[itemKey].dataType}-details/${renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].subnodes[itemKey].dataId}`}>{renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].subnodes[itemKey].title}</Link>
-                                    </div>
-                                  )
-                                })
-                              }
-                            </div>
-                          )
-                        })
-                      }
-                    </div>
-                    )
-                  })
-                }
+      /* detect changes in the grand parent key */
+      if ( (grandParentNodeID === null) || (grandParentNodeID != grandParentKey) ) {
+        /* select the grand parent node */
+        this.setState({ grandParentNodeID: grandParentKey, parentNodeID: null });
+      }
+      else {
+        /* un-select the grand parent node */
+        this.setState({ grandParentNodeID: null, parentNodeID: null });
+      }
+    }
+
+    changeParentNodeID(parentKey) {
+      const { parentNodeID } = this.state;
+
+      if (parentNodeID === null) {
+        /* select the parent node */
+        this.setState({ parentNodeID: parentKey });
+      }
+      else {
+        /* un-select the parent node */
+        this.setState({ parentNodeID: null });
+      }
+    }
+
+    renderTaggedDataDisplay() {
+        const { topNavSearchTerm, topNavSearchEnabled, renderTaggedData } = this.state;
+        const { browseTaggedData } = this.props;
+
+        /**************************************************************************************
+        Use cases:
+          noSearchTerm Use Cases:
+              grandParent=null and parent=null: show top level nodes only
+              grandParent=x and parent=null: show grandParent expanded one level (show parents)
+              grandParent=x and parent=y: show child nodes and links
+
+          searchTerm Use Cases:
+            Scan through all child nodes at the grandparent/parent level to see if there are any child objects that
+              .... begin with noSearchTerm (use browseTaggedData to build renderTaggedData with just the necessary grandparents/parents)
+
+                Display all grandparents/parents/child nodes (renderTaggedData), no ability to expand / collapse grandparent/parent nodes.
+
+        **************************************************************************************/
+
+        return(
+          <div className="search-results-set">
+            {Object.keys(browseTaggedData.taggedData).length === 0 && Object.keys(renderTaggedData.taggedData).length === 0 && <p>Loading....</p>}
+            {Object.keys(browseTaggedData.taggedData).length > 0 && Object.keys(renderTaggedData.taggedData).length === 0 && <p>No results found.</p>}
+
+            {Object.keys(renderTaggedData.taggedData).length > 0 && topNavSearchTerm === '' &&
+              <div>
+                {this.renderTaggedDataDisplay_NoSearchTerm()}
               </div>
             }
-            {Object.keys(browseTaggedData.taggedData).length > 0 && Object.keys(renderTaggedData.taggedData).length === 0 && <p>No results found.</p>}
-            {Object.keys(browseTaggedData.taggedData).length === 0 && Object.keys(renderTaggedData.taggedData).length === 0 && <p>Loading....</p>}
+            {Object.keys(renderTaggedData.taggedData).length > 0 && topNavSearchTerm !== '' &&
+              <div>
+                {this.renderTaggedDataDisplay_SearchTerm()}
+              </div>
+            }
 
             <style jsx>{`
               .search-results-set {
@@ -168,6 +193,8 @@ class BrowseTaggedDataSearch extends Component {
                   margin-top: 25px;
                   overflow-y: scroll;
                   min-height: 300px;
+                  height: 300px;
+                  max-height: 300px;
               }
 
               .search-results-grandparent {
@@ -184,79 +211,164 @@ class BrowseTaggedDataSearch extends Component {
                 margin-left: 150px;
               }
               `}</style>
+          </div>
+        )
+    }
+
+    renderTaggedDataDisplay_SearchTerm() {
+      console.log('here');
+      return(
+        <p>with search term...</p>
+      )
+    }
+
+    renderTaggedDataDisplay_NoSearchTerm() {
+      const { grandParentNodeID, parentNodeID, renderTaggedData } = this.state;
+
+      /***************************************************************************************
+      noSearchTerm Use Cases:
+          grandParent=null and parent=null: show top level nodes only
+          grandParent=x and parent=null: show grandParent expanded one level (show parents)
+          grandParent=x and parent=y: show child nodes and links
+      ***************************************************************************************/
+      return (
+        <div>
+          {grandParentNodeID === null && parentNodeID === null && <div>
+            {Object.keys(renderTaggedData.taggedData).map(function (grandParentKey) {
+                return (
+                  <div>
+                    <h1 onClick={(event) => { this.changeGrandParentNodeID(grandParentKey) }} className="search-results-grandparent">{renderTaggedData.taggedData[grandParentKey].title}</h1>
+                  </div>
+                )
+              }, this)
+            }
+            </div>
+          }
+
+          {grandParentNodeID !== null && parentNodeID === null && <div>
+            {Object.keys(renderTaggedData.taggedData).map(function (grandParentKey) {
+                return (
+                  <div>
+                    <h1 onClick={(event) => { this.changeGrandParentNodeID(grandParentKey) }} className="search-results-grandparent">{renderTaggedData.taggedData[grandParentKey].title}</h1>
+                    {Object.keys(renderTaggedData.taggedData[grandParentKey].subnodes).map(function (parentKey) {
+                        return (
+                          <div>
+                            {grandParentKey === grandParentNodeID && <h2 onClick={(event) => { this.changeParentNodeID(parentKey) }} className="search-results-parent">{renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].title}</h2>}
+                          </div>
+                        )
+                      }, this)
+                    }
+                  </div>
+                )
+              }, this)
+            }
+            </div>
+          }
+
+          {grandParentNodeID !== null && parentNodeID !== null && <div>
+            {Object.keys(renderTaggedData.taggedData).map(function (grandParentKey) {
+                return (
+                  <div>
+                    <h1 onClick={(event) => { this.changeGrandParentNodeID(grandParentKey) }} className="search-results-grandparent">{renderTaggedData.taggedData[grandParentKey].title}</h1>
+                    {Object.keys(renderTaggedData.taggedData[grandParentKey].subnodes).map(function (parentKey) {
+                        return (
+                          <div>
+                            {grandParentKey === grandParentNodeID && <h2 onClick={(event) => { this.changeParentNodeID(parentKey) }} className="search-results-parent">{renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].title}</h2>}
+                            {Object.keys(renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].subnodes).map(function (itemKey) {
+                                return (
+                                  <div className="search-results-item">
+                                    {grandParentKey === grandParentNodeID && parentKey === parentNodeID && <Link onClick={(event) => { this.endSearch(); }} to={`/${renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].subnodes[itemKey].dataType}-details/${renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].subnodes[itemKey].dataId}`}>{renderTaggedData.taggedData[grandParentKey].subnodes[parentKey].subnodes[itemKey].title}</Link>}
+                                  </div>
+                                )
+                              })
+                            }
+                          </div>
+                        )
+                      }, this)
+                    }
+                  </div>
+                )
+              }, this)
+            }
+            </div>
+          }
+
+
         </div>
       )
-  }
+    }
 
-  render() {
-    const { browseTaggedData } = this.props;
-    const { topNavSearchTerm, topNavSearchEnabled, renderTaggedData } = this.state;
+    render() {
+      const { browseTaggedData } = this.props;
+      const { topNavSearchTerm, topNavSearchEnabled, renderTaggedData } = this.state;
 
-    //console.log('Rendering...');
-    //console.log(renderTaggedData.taggedData);
+      //console.log('Rendering...');
+      //console.log(renderTaggedData.taggedData);
 
-    return (
-      <div className="search-site-bytags">
-        <p className="search-text">Search:</p>
-        <input
-          id="BrowseTaggedDataSearchInputField"
-          onClick={(event) => { this.handleClick({ value: event.target.value}); }}
-          onChange={(event) => { this.handleFieldChange({ value: event.target.value }); }}
-          type="text"
-          className="search-input-field" />
+      return (
+        <div className="search-site-bytags">
+          <p className="search-text">Search:</p>
+          <input
+            id="BrowseTaggedDataSearchInputField"
+            onClick={(event) => { this.handleClick({ value: event.target.value}); }}
+            onChange={(event) => { this.handleFieldChange({ value: event.target.value }); }}
+            type="text"
+            className="search-input-field" />
 
-        {topNavSearchEnabled == true && <div className="search-results-container">
-            <Button style={{'float': 'right', 'marginLeft': '20px', 'marginBottom': '0px'}} className="btn-primary"
-              onClick={(event) => { this.endSearch(); }}>
-              Close/Cancel Search
-            </Button>
-            <h1 className="search-results-headertext">Browse / Search Results:</h1>
-            <hr/>
-            {this.renderTaggedDataDisplay()}
-          </div>
-        }
-        <style jsx>{`
-          .search-site-bytags {
-            position: relative;
-            max-width: 400px;
-            min-width: 400px;
-            display: inline-block;
-            margin-top: -10px;
+          {topNavSearchEnabled == true && <div className="search-results-container">
+              <Button style={{'float': 'right', 'marginLeft': '20px', 'marginBottom': '0px'}} className="btn-primary"
+                onClick={(event) => { this.endSearch(); }}>
+                Close/Cancel Search
+              </Button>
+              <h1 className="search-results-headertext">Browse / Search Results:</h1>
+              <hr/>
+              {this.renderTaggedDataDisplay()}
+            </div>
           }
+          <style jsx>{`
+            .search-site-bytags {
+              position: relative;
+              max-width: 400px;
+              min-width: 400px;
+              display: inline-block;
+              margin-top: -10px;
+            }
 
-          .search-results-container {
-            display: inline-block;
-            min-height: 300px;
-            border: 2px solid ${black};
-            background-color: ${sloohBlue};
-            color: ${white};
-            z-index: 1;
-            max-width: 500px;
-            min-width: 500px;
-            margin-left: -160px;
-            position: absolute;
-            margin-top: 30px;
-          }
-
-          .search-results-headertext {
-              margin-left: 20px;
-              font-size: 1.5em;
-          }
-
-          .search-text {
-              margin-top: 20px;
+            .search-results-container {
+              display: inline-block;
+              min-height: 400px;
+              max-height: 400px;
+              height: 400px;
+              border: 2px solid ${black};
+              background-color: ${sloohBlue};
               color: ${white};
-          }
+              z-index: 1;
+              max-width: 500px;
+              min-width: 500px;
+              margin-left: -160px;
+              position: absolute;
+              margin-top: 30px;
+            }
 
-          .search-input-field {
-            background-color: gray;
-            color: ${white}
-            font-weight: bold;
-          }
-          `}</style>
-      </div>
-    );
-  }
+            .search-results-headertext {
+                margin-left: 20px;
+                font-size: 1.5em;
+            }
+
+            .search-text {
+                margin-top: 20px;
+                color: ${white};
+            }
+
+            .search-input-field {
+              background-color: gray;
+              color: ${white}
+              font-weight: bold;
+            }
+            `}</style>
+        </div>
+      );
+    }
 }
 
 export default BrowseTaggedDataSearch;
