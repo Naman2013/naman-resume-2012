d61904aee4b3cc6c05155f98015aafe73befdcf0
Todd Reisel <treisel@coastalbt.com>
2018-03-05 08:54:13 -0500

    telescope details: allsky tabs, initial draft.

---
+++ app/components/telescope-details/AllSkyTabs/generate-tab-configuration.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import CenterContent from '../../../design-system/CenterContent';
+import AllSkyCamWidget from '../allsky-widget';
+import AllSkyTimelapseWidget from '../allsky-timelapse-widget';
+import DomeCamWidget from '../domecam-widget';
+import DomeCamTimelapseWidget from '../domecam-timelapse-widget';
+
+const inlineTitleStyle = {
+  color: 'white',
+}
+
+const inlineTimelapseStyle = {
+  minWidth: '100%',
+  width: '100%',
+}
+
+export default function generateAllSkyTabConfiguration({
+  obsId,
+  AllskyWidgetId,
+  AllskyTimelapseWidgetId,
+}) {
+  return (
+  [
+    {
+      tabText: 'All Sky Camera',
+      tabContent: (
+        <CenterContent>
+          <AllSkyCamWidget obsId={obsId} AllskyWidgetId={AllskyWidgetId}/>
+        </CenterContent>
+      ),
+    },
+    {
+      tabText: 'All Sky Lapse',
+      tabContent: (
+          <AllSkyTimelapseWidget obsId={obsId} AllskyTimelapseWidgetId={AllskyTimelapseWidgetId}/>
+      ),
+    },
+  ]
+  );
+}
---
+++ app/components/telescope-details/allsky-timelapse-widget/AllSkyTimelapseWidget.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import RefreshedImage from '../../common/refreshed-static-image/RefreshedImage';
+import Offline from '../condition-snapshot/Offline';
+import GenericLoadingBox from '../../common/loading-screens/generic-loading-box';
+import { fetchAllSkyTimelapseAction } from '../../../modules/Telescope-Overview';
+import { white } from '../../../styles/variables/colors';
+import './allsky-timelapse-widget.scss';
+
+const mapStateToProps = ({
+  telescopeOverview,
+  telescopeOverview: { domeCamTimelapseWidgetResult },
+}) => ({
+  allSkyTimelapseTitle: allSkyTimelapseWidgetResult.title,
+  refreshIntervalSec: allSkyTimelapseWidgetResult.refreshIntervalSec,
+  domeCamTimelapseURL: allSkyTimelapseWidgetResult.domecamTimelapseURL,
+  offlineImageURL: allSkyTimelapseWidgetResult.offlineImageURL,
+  onlineStatus: allSkyTimelapseWidgetResult.onlineStatus,
+  widgetWidth: allSkyTimelapseWidgetResult.widgetWidth,
+  fetchingAllSkyTimelapseWidgetResult: telescopeOverview.fetchingAllSkyTimelapseWidgetResult,
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({
+    fetchAllSkyTimelapseAction,
+  }, dispatch),
+});
+
+@connect(mapStateToProps, mapDispatchToProps)
+class AllSkyTimelapseWidget extends Component {
+  static propTypes = {
+    obsId: PropTypes.string.isRequired,
+    AllskyTimelapseWidgetId: PropTypes.string.isRequired,
+    fetchingAllSkyTimelapseWidgetResult: PropTypes.bool.isRequired,
+    refreshIntervalSec: PropTypes.number.isRequired,
+    allSkyTimelapseURL: PropTypes.string.isRequired,
+    onlineStatus: PropTypes.string.isRequired,
+    offlineImageURL: PropTypes.string.isRequired,
+  };
+
+  componentDidMount() {
+    const { obsId, AllskyTimelapseWidgetId } = this.props;
+    this.props.actions.fetchAllSkyTimelapseAction({ obsId, AllskyTimelapseWidgetId });
+  }
+
+  componentWillUpdate(nextProps) {
+    if (this.props.AllskyTimelapseWidgetId !== nextProps.AllskyTimelapseWidgetId && this.props.obsId !== nextProps.obsId) {
+      this.props.actions.fetchAllSkyTimelapseAction({ obsId: nextProps.obsId, AllskyTimelapseWidgetId: nextProps.AllskyTimelapseWidgetId });
+    }
+  }
+
+  render() {
+    const {
+      fetchingAllSkyTimelapseWidgetResult,
+      allSkyTimelapseTitle,
+      refreshIntervalSec,
+      allSkyTimelapseURL,
+      onlineStatus,
+      offlineImageURL,
+      widgetWidth,
+    } = this.props;
+
+    const inlineTitleStyle = {
+      color: 'white',
+      textAlign: 'center',
+      position: 'relative',
+      minWidth: '100%',
+    }
+
+    return (
+      <div className="telescope-block live-allsky">
+        <h1 style={inlineTitleStyle}>{allSkyTimelapseTitle}</h1>
+        <div className="live-allskytimelapse">
+          {onlineStatus == 'offline' && <Offline offlineImageURL={offlineImageURL}/>}
+          {onlineStatus == 'online' && allSkyTimelapseURL ?
+            <video style={{width: widgetWidth + 'px'}} className="allskytimelapse-video" playsInline autoPlay muted loop nodownload controls controlsList="nodownload">
+              <source src={allSkyTimelapseURL} type="video/mp4" />
+            </video>
+            : <GenericLoadingBox />
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AllSkyTimelapseWidget;
