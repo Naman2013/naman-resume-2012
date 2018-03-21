17104e12c36a65a2316ea2f2e20cd6c768942bad
jmasse <jmasse860@gmail.com>
2018-03-20 21:19:22 -0400

    feat(stubbed social icons)

--- app/components/GlobalNavigation/Menus/Main.js
+++ app/components/GlobalNavigation/Menus/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uniqueId from 'lodash/uniqueId';
 import MenuList from './partials/MenuList';
+import SocialMenu from './partials/SocialMenu';
 import PrimaryButton from './partials/buttons/PrimaryButton';
 
 import MAIN_CONFIGURATION from './mainConfiguration';
@@ -8,6 +9,7 @@ import MAIN_CONFIGURATION from './mainConfiguration';
 const Main = () => (
   <div>
     <MenuList items={MAIN_CONFIGURATION.primary} />
+    <SocialMenu />
     <MenuList items={MAIN_CONFIGURATION.secondary} />
   </div>
 );
