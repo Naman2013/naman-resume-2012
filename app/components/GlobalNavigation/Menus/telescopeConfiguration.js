68bea77a06c31dbda82c4a08f1874c34acab72e7
jmasse <jmasse860@gmail.com>
2018-03-25 12:36:08 -0400

    feat(wired in the telescopes data)

--- app/components/GlobalNavigation/Menus/telescopeConfiguration.js
+++ app/components/GlobalNavigation/Menus/telescopeConfiguration.js
@@ -12,34 +12,14 @@ export const modelTelescopesFromObsList = {
   },
 };
 
-export default function buildTelescopeConfiguration(telescopes = []) {
-  console.log(telescopes);
-
-  return (
-    {
-      component: <PrimaryButton />,
-      content: [
-        {
-          _ID: uniqueId(),
-          text: 'Chile one',
-          anchor: '/telescope-details/3f332115-7908-11e6-a635-0eb2b1774883/4624887a-7909-11e6-a635-0eb2b1774883',
-        },
-        {
-          _ID: uniqueId(),
-          text: 'Canary one',
-          anchor: '/telescope-details/d7f673a5-7908-11e6-a635-0eb2b1774883/1ff72faa-7909-11e6-a635-0eb2b1774883',
-        },
-        {
-          _ID: uniqueId(),
-          text: 'Canary two',
-          anchor: '/telescope-details/d7f673a5-7908-11e6-a635-0eb2b1774883/2590c3fd-7909-11e6-a635-0eb2b1774883',
-        },
-        {
-          _ID: uniqueId(),
-          text: 'Canary three',
-          anchor: '/telescope-details/d7f673a5-7908-11e6-a635-0eb2b1774883/3686b322-7909-11e6-a635-0eb2b1774883',
-        },
-      ],
-    }
-  );
-}
+export default (telescopes = []) => ({
+  component: <PrimaryButton />,
+  content: telescopes.map(_telescope => ({
+    _ID: uniqueId(),
+    text: _telescope.teleName,
+    anchor: _telescope.teleDetailsURL,
+    isOnline: _telescope.teleOnlineStatus === 'online',
+    logo: _telescope.teleLogoURL,
+    telescopeID: _telescope.teleId,
+  })),
+});
