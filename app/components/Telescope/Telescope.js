c619dbbb04a37537b6d16aca1a0177f8a913b0e7
jmasse <jmasse860@gmail.com>
2018-03-26 21:10:04 -0400

    feat(drawing the first line)

--- app/components/Telescope/Telescope.js
+++ app/components/Telescope/Telescope.js
@@ -4,7 +4,7 @@ import Polyline from '../SVG/Polyline';
 const Telescope = () => (
   <div>
     <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
-      <Polyline points="0,0 0,25" />
+      <Polyline points="0,0 0,15" strokeWidth={5} />
     </svg>
   </div>
 );
