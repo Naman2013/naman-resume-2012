48157d9f404ae1642713e0240696a681d4a34714
jmasse <jmasse860@gmail.com>
2018-03-26 21:06:27 -0400

    feat(stubbing out some basic svg shapes)

--- app/components/Telescope/Telescope.js
+++ app/components/Telescope/Telescope.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Polyline from '../SVG/Polyline';
 
 const Telescope = () => (
   <div>
-    <h1>Hey...</h1>
+    <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <Polyline points="0,0 0,25" />
+    </svg>
   </div>
 );
 
