4ad36af38fcf7c3bb02b5272bc617d541b25acc9
jmasse <jmasse860@gmail.com>
2018-03-26 21:06:27 -0400

    feat(stubbing out some basic svg shapes)

---
+++ app/components/SVG/Group.js
@@ -0,0 +1,20 @@
+/**
+ * SVG group
+ * contains SVG elements
+ *
+ * id
+ * class
+ * pointer-events
+ * transform="translate(0 35)" - supports double point precision
+ * visibility="visible" || "hidden"
+ */
+
+import React from 'react';
+
+const Group = ({  }) => (
+  <g>
+
+  </g>
+);
+
+export default Group;
---
+++ app/components/SVG/Polyline.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * @param fill
+ * @param stroke - hexadecimal color value
+ * @param points
+ * @returns {*}
+ * @constructor
+ *
+ * example
+ * <polyline fill="none" stroke="black" points="0,0 0, 25" />
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  fill: PropTypes.string,
+  points: PropTypes.string.isRequired,
+  stroke: PropTypes.string,
+  strokeWidth: PropTypes.string,
+};
+
+const defaultProps = {
+  fill: '',
+  stroke: 'black',
+  strokeWidth: '1',
+};
+
+const Polyline = ({ fill, stroke, strokeWidth, points }) => (
+  <polyline fill={fill} strokeWidth={strokeWidth} stroke={stroke} points={points} />
+);
+
+Polyline.propTypes = propTypes;
+Polyline.defaultProps = defaultProps;
+
+export default Polyline;
