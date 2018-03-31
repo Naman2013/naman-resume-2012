194225e90c853d0a0f0cac20bbe48cb21ce91acc
jmasse <jmasse860@gmail.com>
2018-03-31 15:18:07 -0400

    feat(drawing all of the borders to scale)

--- app/components/Telescope/generateRow.js
+++ app/components/Telescope/generateRow.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ROW_CONFIG from './rowConfigurationEnum';
 import Polyline from '../SVG/Polyline';
 
-function draw() {}
+function warnClient() {
+  console.warn('Invalid row configuration provided');
+}
 
 export function generateRow(dimension = 0, count = 0, rowConfiguration = ROW_CONFIG.TOP) {
   if (!(rowConfiguration instanceof ROW_CONFIG)) {
-    console.warn('Invalid row configuration provided');
+    warnClient();
     return [];
   }
   if (!dimension) { return []; }
@@ -20,37 +22,46 @@ export function generateRow(dimension = 0, count = 0, rowConfiguration = ROW_CON
   const SHORT_TICK_THICKNESS = (LARGE_TICK_THICKNESS / 2);
 
   let ACCUMULATOR = 0;
-  let Y_MODIFIER;
-
-  switch (rowConfiguration) {
-    case ROW_CONFIG.TOP:
-      Y_MODIFIER = 0;
-      break;
-    case ROW_CONFIG.BOTTOM:
-      Y_MODIFIER = dimension;
-      break;
-    case ROW_CONFIG.LEFT:
-      Y_MODIFIER = 0;
-      break;
-    case ROW_CONFIG.RIGHT:
-      Y_MODIFIER = 0;
-      break;
-    default:
-      Y_MODIFIER = 0;
-      break;
-  }
 
   for (let i = 0; i <= COUNT; i += 1) {
     const isLongTick = (i % 5) === 0;
 
+    let x1;
+    let y1;
+    let x2;
+    let y2;
+
     ACCUMULATOR += SPACING;
 
-    let x1 = ACCUMULATOR;
-    let y1 = Y_MODIFIER;
-    let x2 = ACCUMULATOR;
-    let y2 = (isLongTick) ? Math.abs(Y_MODIFIER - LARGE_TICK_LENGTH) : Math.abs(Y_MODIFIER - SHORT_TICK_LENGTH);
-
-
+    switch (rowConfiguration) {
+      case ROW_CONFIG.TOP:
+        x1 = ACCUMULATOR;
+        y1 = 0;
+        x2 = ACCUMULATOR;
+        y2 = (isLongTick) ? LARGE_TICK_LENGTH : SHORT_TICK_LENGTH;
+        break;
+      case ROW_CONFIG.BOTTOM:
+        x1 = ACCUMULATOR;
+        y1 = dimension;
+        x2 = ACCUMULATOR;
+        y2 = (isLongTick) ? (dimension - LARGE_TICK_LENGTH) : (dimension - SHORT_TICK_LENGTH);
+        break;
+      case ROW_CONFIG.LEFT:
+        x1 = 0;
+        y1 = ACCUMULATOR;
+        x2 = (isLongTick) ? LARGE_TICK_LENGTH : SHORT_TICK_LENGTH;
+        y2 = ACCUMULATOR;
+        break;
+      case ROW_CONFIG.RIGHT:
+        x1 = dimension;
+        y1 = ACCUMULATOR;
+        x2 = (isLongTick) ? (dimension - LARGE_TICK_LENGTH) : (dimension - SHORT_TICK_LENGTH);
+        y2 = ACCUMULATOR;
+        break;
+      default:
+        warnClient();
+        break;
+    }
 
     const polylineAttributes = {
       points: `${x1},${y1} ${x2},${y2}`,
