05366b554387d63aa1b097742ba7e36fe90a6da5
jmasse <jmasse860@gmail.com>
2018-04-15 14:10:41 -0400

    chore(cleaning up gridling, preparing to add to the rest of the top draw method)

--- app/components/Telescope/TelescopeFrame/GridLine.js
+++ app/components/Telescope/TelescopeFrame/GridLine.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import uniqueId from 'lodash/uniqueId';
 import Circle from './Circle';
 
 function generateLine(dimension, resolution, spacing, currentX, increment, style) {
-  // start from the center and draw out from there...
-  // using every
-
   const ROW = [];
   const MID_POINT = (dimension / 2);
 
   let TOP_STARTING_POINT = MID_POINT;
   let BOTTOM_STARTING_POINT = MID_POINT;
 
-  console.log('--------');
   for (let i = 0; i <= resolution; i += 1) {
     if (i % increment === 0) {
       if (TOP_STARTING_POINT > 0) {
@@ -35,5 +32,18 @@ const GridLine = ({
     {generateLine(dimension, resolution, spacing, currentX, increment, style)}
   </g>
 );
+
+GridLine.propTypes = {
+  dimension: PropTypes.number.isRequired,
+  resolution: PropTypes.number.isRequired,
+  spacing: PropTypes.number.isRequired,
+  currentX: PropTypes.number.isRequired,
+  increment: PropTypes.number.isRequired,
+  style: PropTypes.object,
+};
+
+GridLine.defaultProps = {
+  style: {},
+};
 
 export default GridLine;
