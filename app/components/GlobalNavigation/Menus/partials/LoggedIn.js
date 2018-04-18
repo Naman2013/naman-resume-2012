b1b9f17343b91322e2964901086bd11a0b5241a9
tlerias <tlerias@gmail.com>
2018-04-18 18:08:59 +0200

    feature( my profile button in nav )

--- app/components/GlobalNavigation/Menus/partials/LoggedIn.js
+++ app/components/GlobalNavigation/Menus/partials/LoggedIn.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router';
 import Logout from 'redux/components/Logout';
 
 const LoggedIn = ({ userName }) => (
   <div>
     <h1>Welcome, {userName}</h1>
+    <div><Link to="/profile/private">My Profile</Link></div>
     <Logout />
   </div>
 );
