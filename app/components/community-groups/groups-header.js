a9141d8f262fd83982f0b71b3f6741a05dfad360
tlerias <tlerias@gmail.com>
2018-04-19 21:43:32 +0200

    feature(Groups main header )

--- app/components/community-groups/groups-header.js
+++ app/components/community-groups/groups-header.js
@@ -16,25 +16,40 @@ const {
 
 const GroupsHeader = () => (
   <div className="groups-header">
-  This is a header.
-  <div>
-    <Link to={'/community-groups/public'} activeClassName="active-group-header-item">
-      <span className="group-header-item-title">Public Groups</span>
-    </Link>
-  </div>
-  <div>
-    <Link to={'/community-groups/private'} activeClassName="active-group-header-item">
-      <span className="group-header-item-title">Private Groups</span>
-    </Link>
-  </div>
-  <div>
-    <Link to={'/community-groups/my-groups'} activeClassName="active-group-header-item">
-      <span className="group-header-item-title">My Groups</span>
-    </Link>
-  </div>
+    <h4>Slooh Community</h4>
+    <h3>Discussion Groups</h3>
+    <nav className="nav-container">
+      <Link to={'/community-groups/public'} activeClassName="active-group-header-item">
+        <span className="group-header-item-title">Public Groups</span>
+      </Link>
+      <Link to={'/community-groups/private'} activeClassName="active-group-header-item">
+        <span className="group-header-item-title">Private Groups</span>
+      </Link>
+      <Link to={'/community-groups/my-groups'} activeClassName="active-group-header-item">
+        <span className="group-header-item-title">My Groups</span>
+      </Link>
+    </nav>
     <style jsx>{`
+      .groups-header {
+        background-color: ${darkBlueGray};
+        text-align: center;
+        padding: 25px;
+      }
+
+      .group-header-item-title {
+        padding: 25px;
+        color: ${white};
+      }
       :global(.active-group-header-item) .group-header-item-title {
         font-weight: bold;
+      }
+
+      .nav-container {
+        display: flex;
+        padding: 50px;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
       }
 
     `}</style>
