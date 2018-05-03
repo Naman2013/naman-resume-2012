4354c48f6cb216b4a545caaac2856d605fcd3ae7
jmasse <jmasse860@gmail.com>
2018-05-03 19:51:06 -0400

    chore(refactor)

--- app/components/Telescope/Mask.js
+++ app/components/Telescope/Mask.js
@@ -15,7 +15,7 @@ const Mask = ({ isVisible }) => (
     <style jsx>
       {`
         .mask {
-          transition: opacity 0.5s ease-in-out;
+          transition: opacity 0.25s ease-in-out;
         }
       `}
     </style>
