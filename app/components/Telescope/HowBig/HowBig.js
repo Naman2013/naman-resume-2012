061c5851741b6b0c8a0ef2d0a2f56bbcc9cbc40b
jmasse860@gmail.com <jmasse860@gmail.com>
2018-06-07 19:28:21 -0400

    chore(adding initial props to the how big interface)

--- app/components/Telescope/HowBig/HowBig.js
+++ app/components/Telescope/HowBig/HowBig.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import domains from './domains';
+import { primaryFont } from 'styles/variables/fonts';
 
 class HowBig extends Component {
   static propTypes = {
-
+    targetDomain: PropTypes.string,
+    previousDomain: PropTypes.string,
   };
 
   render() {
     return (
-      <g></g>
+      <g>
+        <text x={0} y={0} className="text">How Big?</text>
+
+        <style jsx>
+          {`
+            .text {
+              font-family: ${primaryFont};
+              color: aqua;
+              font-size: 200px;
+            }
+          `}
+        </style>
+      </g>
     );
   }
 }
