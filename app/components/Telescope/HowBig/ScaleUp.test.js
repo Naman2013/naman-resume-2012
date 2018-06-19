08ed7752bc971f6aebb9a26795c9be7eaabc7006
jmasse <jmasse860@gmail.com>
2018-06-18 22:39:31 -0400

    chore(setting up unit tests for scaleup)

--- app/components/Telescope/HowBig/ScaleUp.test.js
+++ app/components/Telescope/HowBig/ScaleUp.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ScaleUp from './ScaleUp';
+import fauxMissions from 'content/fauxMissions';
+
+const setup = (customProps) => {
+  const props = Object.assign({}, fauxMissions.scaleUp, customProps);
+  const wrapper = shallow(<ScaleUp {...props} />);
+
+  return {
+    wrapper,
+  };
+};
 
 describe('ScaleUp', () => {
   it('should render correctly', () => {
-    const scaleUp = shallow(<ScaleUp />);
-    expect(scaleUp).toMatchSnapshot();
+    expect(setup().wrapper).toMatchSnapshot();
   });
 });
