12d53b5ef9c1cc9bddd4491e78f98ca055040a34
jmasse <jmasse860@gmail.com>
2018-06-20 20:24:04 -0400

    feat(setting up the beginning of scale up) - detecting onload of reference object - painting reference object when it is time to become visible

--- app/components/Telescope/HowBig/ScaleUp.js
+++ app/components/Telescope/HowBig/ScaleUp.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import noop from 'lodash/noop';
+import domains from './domains';
+import FadeSVG from '../../../components/common/Fade/FadeSVG';
+import { animateValues } from '../../../utils/easingFunctions';
 
 class ScaleUp extends Component {
-  static defaultProps = {
+  static BEFORE_START = 1000;
+
+  static propTypes = {
     dimension: PropTypes.number,
     targetObjectURL: PropTypes.string.isRequired,
     targetObjectScale: PropTypes.number.isRequired,
@@ -17,11 +22,62 @@ class ScaleUp extends Component {
     onComplete: noop,
   };
 
-  state = {};
+  state = {
+    referenceObjectLoaded: false,
+    showReference: false,
+    referenceScale: 1,
+    referencePosition: {
+      x: 0,
+      y: 0,
+    },
+  };
+
+  componentDidMount() {
+    this.beginDelayToShowReference();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timerDelayPresentReference);
+  }
+
+  handleReferenceObjectLoaded = () => {
+    this.setState({ referenceObjectLoaded: true });
+  }
+
+  beginDelayToShowReference() {
+    setTimeout(() => {
+      this.presentReference();
+    }, ScaleUp.BEFORE_START);
+  }
+
+  presentReference() {
+    this.setState({ showReference: true });
+  }
+
+  timerDelayPresentReference = undefined;
 
   render() {
+    const { referenceObject } = this.props;
+    const { showReference, referenceObjectLoaded } = this.state;
+
+    const displayReferenceObject = (showReference && referenceObjectLoaded);
+
     return (
-      <g></g>
+      <g>
+        <FadeSVG isHidden={!(displayReferenceObject)}>
+          {
+            domains
+              .enumValueOf(referenceObject)
+              .render({
+                width: 200,
+                height: 200,
+                x: 0,
+                y: 0,
+                onLoadCallback: this.handleReferenceObjectLoaded,
+              })
+          }
+        </FadeSVG>
+      </g>
     );
   }
 }
--- app/components/Telescope/HowBig/ScaleUp.test.js
+++ app/components/Telescope/HowBig/ScaleUp.test.js
@@ -1,19 +1,62 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import ScaleUp from './ScaleUp';
 import fauxMissions from 'content/fauxMissions';
 
 const setup = (customProps) => {
   const props = Object.assign({}, fauxMissions.scaleUp, customProps);
-  const wrapper = shallow(<ScaleUp {...props} />);
+
+  const shallowWrapper = shallow(<ScaleUp {...props} />);
+  const mountedWrapper = mount(<ScaleUp {...props} />);
 
   return {
-    wrapper,
+    mountWrapper: () => (mount(<ScaleUp {...props} />)),
+    shallowWrapper,
+    mountedWrapper,
+    FadeSVG: shallowWrapper.find('FadeSVG'),
   };
 };
 
 describe('ScaleUp', () => {
-  it('should render correctly', () => {
-    expect(setup().wrapper).toMatchSnapshot();
+  jest.useFakeTimers();
+
+  describe('rendering', () => {
+    const { shallowWrapper, FadeSVG } = setup();
+    it('should render correctly', () => {
+      expect(shallowWrapper).toMatchSnapshot();
+    });
+
+    it('should have a `FadeSVG` element', () => {
+      expect(FadeSVG.length).toEqual(1);
+    });
+  });
+
+  describe('when the component mounts', () => {
+    const spyBeginDelayToShowReference = jest.spyOn(ScaleUp.prototype, 'beginDelayToShowReference');
+    const spyPresentReference = jest.spyOn(ScaleUp.prototype, 'presentReference');
+    let mountedWrapper;
+
+    beforeEach(() => {
+      mountedWrapper = setup().mountWrapper();
+    });
+
+    it('should call `beginDelayToShowReference()`', () => {
+      expect(spyBeginDelayToShowReference).toHaveBeenCalled();
+    });
+
+    it('should call `presentReference()` when timers are done, then sets `showReference` to `true`', () => {
+      jest.runAllTimers();
+      expect(spyPresentReference).toHaveBeenCalled();
+      expect(mountedWrapper.state().showReference).toEqual(true);
+    });
+
+    describe('when reference asset is loaded', () => {
+      it('sets `referenceObjectLoaded` to `true`', () => {
+        mountedWrapper.instance().handleReferenceObjectLoaded();
+        expect(mountedWrapper.state().referenceObjectLoaded).toEqual(true);
+      });
+    });
+
+    jest.clearAllMocks();
   });
 });
