aae0db754d26ac856cd32723b9396c61c832fea6
jmasse860@gmail.com <jmasse860@gmail.com>
2018-07-03 16:58:58 -0400

    chore(adding css breakpoints for the team) - also added documentation on how to style in the application

---
+++ app/styles/variables/breakpoints.js
@@ -0,0 +1,3 @@
+export const screenMedium = 'screen and (min-width: 768px)';
+export const screenLarge = 'screen and (min-width: 1024px)';
+export const screenXLarge = 'screen and (min-width: 1366px)';
