9b30d766f8c9413dcda03d46a56dddb09c92f200
jmasse860@gmail.com <jmasse860@gmail.com>
2018-07-04 00:00:34 -0400

    chore(setting up the test to include default props)

--- app/components/common/TiaraTitleSection/TiaraTitleSection.test.js
+++ app/components/common/TiaraTitleSection/TiaraTitleSection.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import TiaraTitleSection from './TiaraTitleSection';
+import alienHeadIcon from '../../../../assets/icons/alien-head.png';
 
 const setup = (customProps = {}) => {
   const props = Object.assign({}, customProps);
@@ -12,7 +13,13 @@ const setup = (customProps = {}) => {
 };
 
 describe('TiaraTitleSection', () => {
+  const defaultProps = {
+    iconURL: alienHeadIcon,
+    preTitle: 'A guide to',
+    title: 'The solar system',
+  };
+
   it('should render correctly', () => {
-    expect(setup().shallowWrapper).toMatchSnapshot();
+    expect(setup(defaultProps).shallowWrapper).toMatchSnapshot();
   });
 });
