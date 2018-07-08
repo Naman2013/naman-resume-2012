830edb6a98e59a3c06a74e0e78c9656bca8aa4db
jmasse860@gmail.com <jmasse860@gmail.com>
2018-07-08 18:17:22 -0400

    chore(fixing up the wiring for the unit test on the content positioning component)

--- app/components/guides/GuideSection/GuideSection.js
+++ app/components/guides/GuideSection/GuideSection.js
@@ -2,14 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import noop from 'lodash/noop';
 
-const GuideSection = ({ content, column, contentAlign }) => (
-  <div />
+// responsible for positioning content at various breakpoints
+const LEFT = 'left';
+const RIGHT = 'right';
+
+const GuideSection = ({ content, column, alignContent }) => (
+  <div>
+    {
+      (alignContent === LEFT)
+        ? ([content, column()].join(''))
+        : ([column(), content].join(''))
+    }
+  </div>
 );
 
 GuideSection.propTypes = {
-  content: PropTypes.string.isRequired,
+  content: PropTypes.func.isRequired,
   column: PropTypes.func,
-  alignContent: PropTypes.oneOf(['left', 'right']),
+  alignContent: PropTypes.oneOf([LEFT, RIGHT]),
 };
 
 GuideSection.defaultProps = {
--- app/components/guides/GuideSection/GuideSection.test.js
+++ app/components/guides/GuideSection/GuideSection.test.js
@@ -5,12 +5,12 @@ import GuideSection from './GuideSection';
 
 describe('GuideSection', () => {
   const props = {
-    content: `Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero lorem posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing evitae est. Sed nec felis loren posuere posuere, rutrum eu ipsum. Aliquam eget odio sed ligula dolae iaculis consequat at eget orci. Mauris moleistie sit amet metus loi mass imattis varius Donec sit amet ligula eget nisi sodales lorem a molestie bibendum. Etiam nisi anteni consectetur eget placerat a, tempus a neque. Donec ut elit urna. Etiam venenatis leni eleifend urna eget scelerisqueliquam in nunc.
+    content: () => `Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero lorem posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing evitae est. Sed nec felis loren posuere posuere, rutrum eu ipsum. Aliquam eget odio sed ligula dolae iaculis consequat at eget orci. Mauris moleistie sit amet metus loi mass imattis varius Donec sit amet ligula eget nisi sodales lorem a molestie bibendum. Etiam nisi anteni consectetur eget placerat a, tempus a neque. Donec ut elit urna. Etiam venenatis leni eleifend urna eget scelerisqueliquam in nunc.
 
 Donec sit amet ligula eget nisi sodales egestas. Aliquam interdum dolor aliquet dolor  iaculis consequat at eget orci. Mauris moleistie sit amet metus loi mass imattis varius Donec sit amet ligula eget nisi sodales lorem a molestie bibendum. Etiam nisi anteni
 posuere vulputate. Etiam elit elit, elementum sed varius at.`,
     column: () => (<AbelList list={['contains 10 objects', 'object type guide', 'beginners and up']} />),
-    contentAlign: 'left',
+    alignContent: 'left',
   };
 
   const shallowWrapper = shallow(<GuideSection {...props} />);
