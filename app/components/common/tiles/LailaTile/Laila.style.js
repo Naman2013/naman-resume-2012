32d4bedd9d584516ce71fab8b048238c98af479a
jmasse860@gmail.com <jmasse860@gmail.com>
2018-07-12 20:02:26 -0400

    feat(a bit of styling of the root container)

--- app/components/common/tiles/LailaTile/Laila.style.js
+++ app/components/common/tiles/LailaTile/Laila.style.js
@@ -1,5 +1,24 @@
 import css from 'styled-jsx/css';
+import { primaryFont, secondaryFont } from '../../../../styles/variables/fonts';
+import { white_tile, astronaut } from '../../../../styles/variables/colors_tiles_v4';
 
 export default css`
+  .root {
+    width: 300px;
+    box-shadow: 0px 0px 15px 1px rgba(65,86,113,.2);
+    color: ${astronaut};
+    list-style-type: none;
+    background: url(${white_tile});
+    background-size: 30px;
+  }
 
+  .title {
+    font-family: ${secondaryFont};
+  }
+
+  .button {
+    color: ${astronaut};
+    text-decoration: none;
+    font-family: ${primaryFont};
+  }
 `;
