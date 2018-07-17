3bbee4ef66a448b37b5616534a2aa99fca8c762f
jmasse <jmasse860@gmail.com>
2018-07-17 14:56:18 -0400

    feat(adding title text into the InAppNavigation)

--- app/components/common/InAppNavigation/InAppNavigation.js
+++ app/components/common/InAppNavigation/InAppNavigation.js
@@ -2,15 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './InAppNavigation.style';
 
-const InAppNavigation = ({ previousText }) => (
+const InAppNavigation = ({ previousText, title }) => (
   <div>
     <p className="back-text">back{previousText && ` to ${previousText}`}</p>
+    <h5 className="title">{title}</h5>
     <style jsx>{style}</style>
   </div>
 );
 
 InAppNavigation.propTypes = {
   previousText: PropTypes.string,
+  title: PropTypes.string.isRequired,
 };
 
 InAppNavigation.defaultProps = {
--- app/components/common/InAppNavigation/InAppNavigation.test.js
+++ app/components/common/InAppNavigation/InAppNavigation.test.js
@@ -3,18 +3,23 @@ import { shallow } from 'enzyme';
 import InAppNavigation from './InAppNavigation';
 
 describe('InAppNavigation', () => {
-  const shallowWrapper = shallow(<InAppNavigation />);
+  const props = { previousText: 'guide', title: 'topic 1: stuff...' };
+  const shallowWrapper = shallow(<InAppNavigation {...props} />);
+
   it('should render correctly', () => {
     expect(shallowWrapper).toMatchSnapshot();
   });
 
-  it('should render previous page name when provided', () => {
-    shallowWrapper.setProps({ previousText: 'guide' });
+  it('should render "back to pageName" when `previousText` is provided', () => {
     expect(shallowWrapper.find('.back-text').text()).toBe('back to guide');
   });
 
-  it('should render back as the text in the back button when no back page is provided', () => {
+  it('should render "back" as the text in the back button when no back page is provided', () => {
     shallowWrapper.setProps({ previousText: '' });
     expect(shallowWrapper.find('.back-text').text()).toBe('back');
+  });
+
+  it('displays the page title', () => {
+    expect(shallowWrapper.find('.title').text()).toBe(props.title);
   });
 });
