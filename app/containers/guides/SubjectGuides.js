aedbc4e3299520d96e80935cd0ee48ac746a9e0b
jmasse <jmasse860@gmail.com>
2018-07-17 11:48:52 -0400

    feat(adding a Sterling title to the page)

--- app/containers/guides/SubjectGuides.js
+++ app/containers/guides/SubjectGuides.js
@@ -5,6 +5,7 @@ import GuideSection from 'components/guides/GuideSection';
 import GuideBodyContent from 'components/guides/GuideBodyContent';
 import GuideContentList from 'components/guides/GuideContentList';
 import SubjectGuideList from 'components/guides/SubjectGuideList';
+import SterlingTitle from 'components/common/titles/SterlingTitle';
 
 const bodyContent = `Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero lorem posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing evitae est. Sed nec felis loren posuere posuere, rutrum eu ipsum. Aliquam eget odio sed ligula dolae iaculis consequat at eget orci. Mauris moleistie sit amet metus loi mass imattis varius Donec sit amet ligula eget nisi sodales lorem a molestie bibendum. Etiam nisi anteni consectetur eget placerat a, tempus a neque. Donec ut elit urna. Etiam venenatis leni eleifend urna eget scelerisqueliquam in nunc.
 
@@ -30,6 +31,8 @@ const SubjectGuides = () => (
     <CenterColumn theme={{ boxShadow: 'rgba(65, 86, 113, 0.2) 0px 3px 8px 1px', marginBottom: '60px' }}>
       <GuideSection {...stubGuideSectionContent} />
     </CenterColumn>
+
+    <SterlingTitle title="Topics within this guide" subTitle="Select a Topic for more information" />
 
     <CenterColumn>
       <SubjectGuideList list={mockList} />
