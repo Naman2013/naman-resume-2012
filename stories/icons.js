5f0a3f72e7af52ac278dd9597a6b4489510e3f15
jmasse <jmasse860@gmail.com>
2018-07-19 18:53:06 -0400

    feat(creating back arrow icon)

--- stories/icons.js
+++ stories/icons.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import BackArrow from '../app/components/common/icons/BackArrow';
 import Close from '../app/components/common/icons/Close';
 import Dots from '../app/components/common/icons/Dots';
 
 storiesOf('Icons', module)
+  .add('BackArrow', () => (<BackArrow theme={{ arrowColor: 'aqua' }} />))
   .add('Close', () => (<Close />))
   .add('Dots', () => (<Dots theme={{ circleColor: 'aqua' }} />));
