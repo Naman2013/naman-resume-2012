51cbbcd33968a395c7d10506b93fd62d94a4db16
jmasse <jmasse860@gmail.com>
2018-07-27 08:29:15 -0400

    feat(adding SterlingTitle to the page)

--- app/containers/guides/TopicGuides.js
+++ app/containers/guides/TopicGuides.js
@@ -2,6 +2,7 @@ import React from 'react';
 import InAppNavigation from 'components/common/InAppNavigation';
 import CenterColumn from 'components/common/CenterColumn';
 import TopicContent from 'components/guides/TopicContent';
+import SterlingTitle from 'components/common/titles/SterlingTitle';
 
 const TopicGuides = () => (
   <div>
@@ -9,10 +10,12 @@ const TopicGuides = () => (
       title="Topic 1: Astronomical Time"
       menuTopAdjustment={162}
     />
-
     <div style={{ backgroundColor: 'aqua', textAlign: 'center', color: 'white', padding: '20px 0' }}>TODO - ADD HEADER FROM MATT</div>
-
     <TopicContent />
+    <SterlingTitle
+      title="Things to know"
+      subTitle="Noteworthy facts from your friends at Slooh"
+    />
   </div>
 );
 
