21d4d1ae2909087c4d7e1cd150d3c69de58b3970
jmasse <jmasse860@gmail.com>
2018-07-28 12:52:33 -0400

    feat(tidying the content region for topics on medium screens)

--- app/components/guides/TopicBodyContent.style.js
+++ app/components/guides/TopicBodyContent.style.js
@@ -1,5 +1,28 @@
 import css from 'styled-jsx/css';
+import { secondaryFont } from 'styles/variables/fonts';
+import { screenMedium } from 'styles/variables/breakpoints';
 
 export default css`
+  .root {
+    background-color: white;
+    padding: 20px 35px;
+  }
 
+  .title-wrapper {
+    padding: 15px 0;
+  }
+
+  p {
+    font-family: ${secondaryFont};
+    line-height: 1.5;
+    font-size: 18px;
+  }
+
+  @media ${screenMedium} {
+    .root {
+      padding: 60px 40px;
+    }
+
+    .title-wrapper {}
+  }
 `;
