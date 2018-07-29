a77db3e676a4f7889fbc849068460535e05fbc03
jmasse <jmasse860@gmail.com>
2018-07-29 10:59:23 -0400

    chore(setting up the test content scenarios in storybook)

--- app/components/common/tiles/BobbieTile/BobbieTile.js
+++ app/components/common/tiles/BobbieTile/BobbieTile.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import style from './BobbieTile.style';
+
+/**
+  @BobbieTile
+  This tile is hard coded to an expected HTML blob provided
+  by the `getGuidePanels` response.
+  40|60
+  60|40
+  50|50
+  image and video placement support
+  https://docs.google.com/document/d/1PK8X1clsV618gHQWtBnwRcFCVsTbpMhSbIYvnz8MW6Y/edit#
+*/
 
 const BobbieTile = ({ HTMLBlob }) => (
-  <div dangerouslySetInnerHTML={{ __html: HTMLBlob }} />
+  <Fragment>
+    <div dangerouslySetInnerHTML={{ __html: HTMLBlob }} />
+    <style jsx>{style}</style>
+  </Fragment>
 );
 
 BobbieTile.propTypes = {
--- stories/Tiles.js
+++ stories/Tiles.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import LailaTile from '../app/components/common/tiles/LailaTile';
+import BobbieTile from 'components/common/tiles/BobbieTile';
+import LailaTile from 'components/common/tiles/LailaTile';
 import LeighTile from 'components/common/tiles/LeighTile';
+import { SAMPLE_IMAGE_HTML_BLOG, SAMPLE_VIDEO_HTML_BLOB } from './content/getGuidesPanels';
 
 storiesOf('Tiles', module)
+  .add('BobbieTile - 40_60_SPLIT with image', () => (
+    <BobbieTile HTMLBlob={SAMPLE_IMAGE_HTML_BLOG} />
+  ))
+  .add('BobbieTile - 60_40_SPLIT with video', () => (
+    <BobbieTile HTMLBlob={SAMPLE_VIDEO_HTML_BLOB} />
+  ))
   .add('LailaTile', () => (
     <LailaTile
       title="Venus"
