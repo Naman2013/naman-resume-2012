4ec2986b2280c921cc43ca5582bc65f115493a5f
jmasse <jmasse860@gmail.com>
2018-07-29 11:33:32 -0400

    feat(static mobile tile design ready, moving onto large screens)

--- app/components/common/tiles/BobbieTile/BobbieTile.style.js
+++ app/components/common/tiles/BobbieTile/BobbieTile.style.js
@@ -1,5 +1,50 @@
 import css from 'styled-jsx/css';
+import { primaryFont, secondaryFont } from 'styles/variables/fonts';
+import { faintShadow } from 'styles/variables/shadows';
+import { astronaut, geyser } from 'styles/variables/colors_tiles_v4';
+import { resetMarginPadding } from 'styles/variables/utils';
 
 export default css`
+  div, h3, ul, li {
+    ${resetMarginPadding}
+  }
 
+  .root {
+    ${faintShadow}
+    background: white;
+  }
+
+  .tile-content-container {
+    padding: 40px;
+  }
+
+  .title {
+    font-family: ${secondaryFont};
+    font-size: 20px;
+    color: ${astronaut};
+    border-bottom: 1px solid ${geyser};
+    font-weight: 300;
+    padding-bottom: 20px;
+  }
+
+  ul {
+    list-style-type: none;
+    font-family: ${primaryFont};
+    color: ${astronaut};
+    text-transform: uppercase;
+    font-size: 10px;
+    font-weight: bold;
+    padding-top: 20px;
+  }
+
+  .author-name {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .html-blob-content-container,
+  .read-duration {
+    display: none;
+  }
 `;
