3e11f914c870199f871c28bd3df74932dfccbe2f
jmasse <jmasse860@gmail.com>
2018-08-25 11:38:17 -0400

    feat(shared buttons are now flexible)

--- app/components/common/style/buttons/Button.style.js
+++ app/components/common/style/buttons/Button.style.js
@@ -3,25 +3,19 @@ import { astronaut, romance } from 'styles/variables/colors_tiles_v4';
 import { primaryFont } from 'styles/variables/fonts';
 
 export default css`
-  .text {
-    vertical-align: middle;
-    font-size: 11px;
-    margin: 0 auto;
-  }
   .button-container {
     display: flex;
-    font-family: ${primaryFont};
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    border-radius: 100px;
+    padding: 10px 20px;
     background-color: transparent;
     border: 1px dashed ${astronaut};
-    border-radius: 100px;
-    width: 110px;
-    margin: 15px 0;
+    text-transform: uppercase;
     font-size: 11px;
     font-weight: bold;
-    padding: 5px 0;
-    text-transform: uppercase;
-    height: 40px;
-    width: 140px;
+    font-family: ${primaryFont};
   }
 
   .circular {
@@ -29,9 +23,17 @@ export default css`
     height: 40px;
   }
 
+  .text {
+    display: inline-block;
+    padding-right: 40px;
+    font-size: 11px;
+    margin: 0 auto;
+  }
+
   .active.button-container {
     background-color: ${astronaut};
   }
+
   .active .button-icon,
   .active .text {
     color: ${romance};
