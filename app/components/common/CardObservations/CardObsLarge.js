a98f4a3102c23eb3b9bfcc1b224a54b2d171d52e
jmasse860@gmail.com <jmasse860@gmail.com>
2018-08-25 22:41:18 -0400

    feat(completed first round of featured object API content)

--- app/components/common/CardObservations/CardObsLarge.js
+++ app/components/common/CardObservations/CardObsLarge.js
@@ -2,22 +2,36 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import style from './CardObservationsLarge.style';
 
-const CardObsLarge = ({ title, author, descContent, imageSrcUrl, capturedDate, likesCount, commentsCount, detailsLinkUrl }) => (
+const CardObsLarge = ({
+  title,
+  subTitle,
+  description,
+  imageUrl,
+  hasLink,
+  linkLabel,
+  linkUrl,
+}) => (
   <Fragment>
     <div className="card-obs">
-      <div className="media-card-img-right"><img src={imageSrcUrl} alt="Observation" /></div>
+      <div className="media-card-img-right">
+        <img src={imageUrl} alt="Observation" />
+      </div>
       <div className="obs-left">
         <div className="card-obs-title">{title}</div>
-        <div className="card-obs-author">BY {author}</div>
-        <div className="card-obs-desc">{descContent}</div>
+        <div className="card-obs-author">{subTitle}</div>
+        <div className="card-obs-desc">{description}</div>
       </div>
       <div className="card-bottom">
-        <ul>
-          <li><img src="https://vega.slooh.com/assets/v4/common/heart.svg" alt="" /> {likesCount}</li>
-          <li><img src="https://vega.slooh.com/assets/v4/common/comment.svg" alt="" /> {commentsCount}</li>
-          <a href={detailsLinkUrl} ><li>DETAILS <img src="https://vega.slooh.com/assets/v4/common/arrow_horz.svg" alt="" /></li></a>
-          <li>Captured: {capturedDate}</li>
-        </ul>
+        {
+          hasLink &&
+            <ul>
+              <li>
+                <a href={linkUrl}>
+                  {linkLabel} <img src="https://vega.slooh.com/assets/v4/common/arrow_horz.svg" alt="" />
+                </a>
+              </li>
+            </ul>
+        }
       </div>
     </div>
     <style jsx>{style}</style>
@@ -26,13 +40,12 @@ const CardObsLarge = ({ title, author, descContent, imageSrcUrl, capturedDate, l
 
 CardObsLarge.propTypes = {
   title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-  descContent: PropTypes.string.isRequired,
-  imageSrcUrl: PropTypes.string.isRequired,
-  capturedDate: PropTypes.string.isRequired,
-  likesCount: PropTypes.string.isRequired,
-  commentsCount: PropTypes.string.isRequired,
-  detailsLinkUrl: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  hasLink: PropTypes.bool.isRequired,
+  linkLabel: PropTypes.string.isRequired,
+  linkUrl: PropTypes.string.isRequired,
 };
 
 export default CardObsLarge;
