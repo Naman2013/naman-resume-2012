f6f9a55ae85bc58c35dd9179ceb1d5aa390cda6e
tlerias <tlerias@gmail.com>
2018-09-13 17:02:21 -0400

    urls merge

--- app/styles/variables/iconURLs.js
+++ app/styles/variables/iconURLs.js
@@ -1,3 +1,10 @@
 export const horizontalArrow = 'https://vega.slooh.com/assets/v4/common/arrow_horz.svg';
 
-export const goldCompass = 'https://vega.slooh.com/assets/v4/icons/gold_compass.svg';
+export const threeLinesAstronaut = 'https://vega.slooh.com/assets/v4/icons/three_lines_astronaut.svg';
+export const searchAstronaut = 'https://vega.slooh.com/assets/v4/icons/search_astronaut.svg';
+export const sloohLogoAstronaut = 'https://vega.slooh.com/assets/v4/icons/slooh_logo_astronaut.svg';
+export const telescopeAstronaut = 'https://vega.slooh.com/assets/v4/icons/telescope_astronaut.svg';
+
+export const checkAstronaut = 'https://vega.slooh.com/assets/v4/icons/check_astronaut.svg';
+export const closeAstronaut = 'https://vega.slooh.com/assets/v4/icons/close_astronaut.svg';
+export const userAstronaut = 'https://vega.slooh.com/assets/v4/icons/user_astronaut.svg';
