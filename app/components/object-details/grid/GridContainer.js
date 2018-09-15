8df60043e52334f3b18287aaf1d623f48f07c379
jmasse <jmasse860@gmail.com>
2018-09-15 16:21:42 -0400

    chore(adding spacing and themeing to the grid container)

--- app/components/object-details/grid/GridContainer.js
+++ app/components/object-details/grid/GridContainer.js
@@ -2,8 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import style from './GridContainer.style';
 
-const GridContainer = ({ children }) => (
-  <section className="object-details-grid-container">
+const GridContainer = ({ children, theme }) => (
+  <section
+    style={theme}
+    className="object-details-grid-container"
+  >
     {children}
     <style jsx>{style}</style>
   </section>
@@ -11,6 +14,11 @@ const GridContainer = ({ children }) => (
 
 GridContainer.propTypes = {
   children: PropTypes.node.isRequired,
+  theme: PropTypes.shape({ }),
+};
+
+GridContainer.defaultProps = {
+  theme: {},
 };
 
 export default GridContainer;
