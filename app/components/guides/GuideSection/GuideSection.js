357c44c41d3d8b95504da9faad9c4743246e9f71
jmasse <jmasse860@gmail.com>
2018-09-15 13:13:50 -0400

    fix(updating snapshot)

--- app/components/guides/GuideSection/GuideSection.js
+++ app/components/guides/GuideSection/GuideSection.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import noop from 'lodash/noop';
-import { screenLarge } from '../../../styles/variables/breakpoints';
 import style from './GuideSection.style';
-
-const LEFT = 'left';
-const RIGHT = 'right';
-
-function getFloat(contentAlignment = LEFT) {
-  return (contentAlignment === LEFT) ? LEFT : RIGHT;
-}
-
-function getMargin(contentAlignment = LEFT) {
-  return (contentAlignment === LEFT) ? 'margin-right: 100px;' : 'margin-left: 100px;';
-}
 
 const GuideSection = ({
   content,
   column,
-  alignContent,
   guideId,
 }) => (
   <div className="root">
@@ -26,31 +13,17 @@ const GuideSection = ({
     <div className="content-container">{content({ guideId })}</div>
 
     <style jsx>{style}</style>
-    <style jsx>
-      {`
-        @media ${screenLarge} {
-          .column-container {
-            float: ${getFloat(alignContent)};
-            ${getMargin(alignContent)}
-            margin-bottom: 40px;
-            width: 30%;
-          }
-        }
-      `}
-    </style>
   </div>
 );
 
 GuideSection.propTypes = {
   content: PropTypes.func.isRequired,
   column: PropTypes.func,
-  alignContent: PropTypes.oneOf([LEFT, RIGHT]),
   guideId: PropTypes.string.isRequired,
 };
 
 GuideSection.defaultProps = {
   column: noop,
-  alignContent: 'left',
 };
 
 export default GuideSection;
--- app/components/guides/GuideSection/GuideSection.style.js
+++ app/components/guides/GuideSection/GuideSection.style.js
@@ -1,8 +1,24 @@
 import css from 'styled-jsx/css';
+import { screenLarge } from 'styles/variables/breakpoints';
 
 export default css`
   .content-container, .column-container {
     margin: 0;
     padding: 0;
+  }
+
+  @media ${screenLarge} {
+    .root {
+      display: flex;
+      flex-direction: row-reverse;
+    }
+
+    .content-container {
+      flex: 40%;
+    }
+
+    .column-container {
+      flex: 10%;
+    }
   }
 `;
