3a81a5bfd8f9548fd225b1537356fd1b208cc1bd
jmasse <jmasse860@gmail.com>
2018-09-16 15:12:34 -0400

    feat(new util that will generate the days in a month by year - including leap year)

---
+++ app/components/object-details/ObjectVisibilityProfile/constants.js
@@ -0,0 +1,14 @@
+export const months = [
+  { name: 'Jan', value: 1 },
+  { name: 'Feb', value: 2 },
+  { name: 'March', value: 3 },
+  { name: 'April', value: 4 },
+  { name: 'May', value: 5 },
+  { name: 'June', value: 6 },
+  { name: 'July', value: 7 },
+  { name: 'Aug', value: 8 },
+  { name: 'Sept', value: 9 },
+  { name: 'Oct', value: 10 },
+  { name: 'Nov', value: 11 },
+  { name: 'Dec', value: 12 },
+];
