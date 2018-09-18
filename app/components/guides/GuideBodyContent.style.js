e2e2e3385c13ff949419491df0e8f1582f2a9054
jmasse <jmasse860@gmail.com>
2018-09-17 21:12:03 -0400

    feat(adding read more / read less to the about content for subject guides)

--- app/components/guides/GuideBodyContent.style.js
+++ app/components/guides/GuideBodyContent.style.js
@@ -23,11 +23,21 @@ export default css`
     color: ${lynch};
     margin: 0;
     padding: 0;
-    margin-bottom: 32px;
+    margin-bottom: 10px;
     font-family: ${secondaryFont};
     font-size: 18px;
     line-height: 1.75;
   }
+
+  .action-read-more {
+    background: none;
+    border: none;
+    margin: 0 0 32px;
+    padding: 0;
+  }
+
+  .action-read-more:focus,
+  .action-read-more:active { outline: none; }
 
   @media ${screenMedium} {
     .root {
@@ -50,7 +60,6 @@ export default css`
     }
 
     .__html-content__ {
-      min-height: 140px;
       display: inline-block;
     }
   }
