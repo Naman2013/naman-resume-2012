0d1901c6197937c22b0315de8f2316c28b795f8a
Todd Reisel <todd@slooh.com>
2018-09-24 19:50:15 -0400

    login: create account now links to /join/step1

--- app/redux/components/Login.js
+++ app/redux/components/Login.js
@@ -79,7 +79,7 @@ class Login extends Component {
 
           <div className="register-container">
             <span className="title-link">{`Don't have an account?`}</span>
-            <Link to="/join">
+            <Link to="/join/step1">
               <LargeButtonWithRightIcon
                 icon={horizontalArrowRightWhite}
                 theme={{
