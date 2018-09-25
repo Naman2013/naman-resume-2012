e012e78e1987f2c971bca7c4f7908bba70bcb0a3
jmasse860@gmail.com <jmasse860@gmail.com>
2018-09-24 20:08:33 -0400

    feat(styling border details)

--- app/components/common/tiles/guide-tile/guide-tile.js
+++ app/components/common/tiles/guide-tile/guide-tile.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { guideCorner } from 'styles/variables/iconURLs';
 import style from './guide-tile.style';
 
 const GuideTile = ({ title, subTitle }) => (
   <div className="guide-tile-root">
     <div className="guide-tile-frame">
-      <h4>{title}</h4>
-      <h5>{subTitle}</h5>
+      <img alt="" className="top-left" src={guideCorner} />
+      <img alt="" className="top-right" src={guideCorner} />
     </div>
+
+    <div className="guide-tile-frame bottom">
+      <img alt="" className="top-left" src={guideCorner} />
+      <img alt="" className="top-right" src={guideCorner} />
+    </div>
+
+    <h4 className="title">{title}</h4>
+    <h5 className="subTitle">{subTitle}</h5>
     <style jsx>{style}</style>
   </div>
 );
--- app/components/common/tiles/guide-tile/guide-tile.style.js
+++ app/components/common/tiles/guide-tile/guide-tile.style.js
@@ -1,12 +1,47 @@
 import css from 'styled-jsx/css';
+import { screenMedium } from 'styles/variables/breakpoints';
 import { primaryFont, secondaryFont } from 'styles/variables/fonts';
+import { blue_tile_feat, golden } from 'styles/variables/colors_tiles_v4';
 
 export default css`
+  .guide-tile-root {
+    position: relative;
+    background: url(${blue_tile_feat});
+    width: 100%;
+    height: 200px;
+    text-align: center;
+  }
+
+  .guide-tile-frame {
+    position: absolute;
+    width: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .bottom {
+    transform: rotate(180deg);
+    bottom: 0;
+    left:0;
+  }
+
+  .top-left {}
+  .top-right { transform: rotate(90deg) }
+
+
+
   .title {
     font-family: ${primaryFont};
+    color: ${golden};
+    text-transform: uppercase;
   }
 
   .subTitle {
     font-family: ${secondaryFont};
+    color: white;
   }
+
+  @media ${screenMedium} {}
 `;
