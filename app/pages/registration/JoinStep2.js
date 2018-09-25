d33851c09f32b8e07214952fc19d45d276607c74
Todd Reisel <todd@slooh.com>
2018-09-25 10:18:01 -0400

    join: form registration.

--- app/pages/registration/JoinStep2.js
+++ app/pages/registration/JoinStep2.js
@@ -6,10 +6,18 @@ import React, { Component, cloneElement } from 'react';
 import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import InputField from 'components/form/InputField';
+import { createValidator, required } from 'modules/utils/validation';
+import Button from 'components/common/style/buttons/Button';
 
 class JoinStep2 extends Component {
   constructor(props) {
     super(props);
+  }
+
+  handleSubmit = (formValues) => {
+    formValues.preventDefault();
   }
 
   render() {
@@ -23,6 +31,17 @@ class JoinStep2 extends Component {
         <h3>Step 2: ACCOUNT DETAILS</h3>
         <br/>
         <br/>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <Field
+            name="username"
+            type="email"
+            label="Email Address"
+            component={InputField}
+          />
+          <Button theme={{ margin: '0 auto'}} type="submit" text="Step 2" onClickEvent={null} />
+        </form>
+        <br/>
+        <br/>
         <Link to="/join/step1">Go Back</Link><br/>
         <br/>
         <Link to="/join/step3">Goto Payment</Link>
@@ -31,4 +50,13 @@ class JoinStep2 extends Component {
   }
 }
 
-export default JoinStep2;
+
+const mapStateToProps = ({ appConfig }) => ({
+  appConfig,
+});
+
+const joinStep2Validation = createValidator({
+  username: [required],
+});
+
+export default connect(mapStateToProps, null)(reduxForm({ form: 'joinForm', validate: joinStep2Validation })(JoinStep2));
