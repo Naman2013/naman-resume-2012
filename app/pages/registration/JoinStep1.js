772e99616685e2c8e1f03c1dfcdb5f63a4ebbb01
Todd Reisel <todd@slooh.com>
2018-09-26 01:23:51 -0400

    join flow: added join page-level API request to Step 1 and Step 3 of the flow.

--- app/pages/registration/JoinStep1.js
+++ app/pages/registration/JoinStep1.js
@@ -1,5 +1,5 @@
 /** *********************************
-* V4 Join
+* V4 Join - Step 1 - Select a Plan
 ********************************** */
 
 import React, { Component, cloneElement, Fragment } from 'react';
@@ -15,7 +15,17 @@ class JoinStep1 extends Component {
   }
 
   render() {
+    const JOIN_PAGE_ENDPOINT_URL = '/api/page/join';
     const SUBSCRIPTION_PLANS_ENDPOINT_URL = '/api/registration/getSubscriptionPlans';
+
+    const joinPageModel = {
+      name: 'JOIN_PAGE_MODEL',
+      model: resp => ({
+        pageHeading1: resp.pageHeading1,
+        pageHeading2: resp.pageHeading2,
+        sectionHeading: resp.sectionHeading,
+      }),
+    };
 
     const subscriptionPlansModel = {
       name: 'SUBSCRIPTION_PLANS_MODEL',
@@ -26,54 +36,71 @@ class JoinStep1 extends Component {
 
     return (
       <div style={{'paddingTop': '55px', 'marginLeft': 'auto', 'marginRight': 'auto', 'width': '600px'}}>
-        <h1>Joining Slooh is Easy</h1>
-        <h2>Join Slooh in three easy steps!  Simply select a plan, enter your details, make your payment, and youre in!</h2>
-        <h3>Step 1: Select your Membership</h3>
-        <br/>
-        <br/>
         <Request
-          serviceURL={SUBSCRIPTION_PLANS_ENDPOINT_URL}
-          model={subscriptionPlansModel}
-          requestBody={{ 'callSource': 'join' }}
+          serviceURL={JOIN_PAGE_ENDPOINT_URL}
+          model={joinPageModel}
+          requestBody={{ 'callSource': 'selectSubscriptionPlan' }}
           render={({
             fetchingContent,
-            modeledResponses: { SUBSCRIPTION_PLANS_MODEL },
+            modeledResponses: { JOIN_PAGE_MODEL },
           }) => (
             <Fragment>
               {
                 !fetchingContent &&
                   <Fragment>
-                    <ul style={{'listStyle': 'none', 'marginLeft': 'auto', 'marginRight': 'auto', 'width': '400px'}}>
-                      {SUBSCRIPTION_PLANS_MODEL.subscriptionPlans.map(subscriptionPlan => <li style={{'paddingTop': '10px', 'paddingBottom': '10px'}} key={`subscriptionplan-tile-${subscriptionPlan.planID}`}>
-                        <div style={{'border': '1px solid'}}>
-                          <div style={{'marginLeft': '30px', 'marginRight': '10px'}}>
-                            <br/>
-                            <b>{subscriptionPlan.planName}</b><br/>
-                            <hr/>
-                            <br/>
-                            <i>{subscriptionPlan.planDescription}</i><br/>
-                            <br/>
-                            <hr/>
-                            <br/>
-                            {subscriptionPlan.planCostPrefix}{subscriptionPlan.planCost}<br/>
-                            {subscriptionPlan.planCostPostfix}<br/>
-                            <br/>
-                            <hr/>
-                            <div id={'subscriptionPlanDetails_' + subscriptionPlan.planID} dangerouslySetInnerHTML={{ __html: subscriptionPlan.aboutThisPlan }}/><br/>
-                            <br/>
-                            <br/>
-                            <Link to={'/join/step2/' + subscriptionPlan.planID}><Button theme={{ margin: '0 auto'}} type="button" text={subscriptionPlan.selectButtonText}/></Link><br/>
-                          </div>
-                        </div>
-                      </li>)}
-                    </ul>
+                      <h1>{JOIN_PAGE_MODEL.pageHeading1}</h1>
+                      <h2>{JOIN_PAGE_MODEL.pageHeading2}</h2>
+                      <h3>Step 1: {JOIN_PAGE_MODEL.sectionHeading}</h3>
+                      <br/>
+                      <br/>
+                      <Request
+                        serviceURL={SUBSCRIPTION_PLANS_ENDPOINT_URL}
+                        model={subscriptionPlansModel}
+                        requestBody={{ 'callSource': 'join' }}
+                        render={({
+                          fetchingContent,
+                          modeledResponses: { SUBSCRIPTION_PLANS_MODEL },
+                        }) => (
+                          <Fragment>
+                            {
+                              !fetchingContent &&
+                                <Fragment>
+                                  <ul style={{'listStyle': 'none', 'marginLeft': 'auto', 'marginRight': 'auto', 'width': '400px'}}>
+                                    {SUBSCRIPTION_PLANS_MODEL.subscriptionPlans.map(subscriptionPlan => <li style={{'paddingTop': '10px', 'paddingBottom': '10px'}} key={`subscriptionplan-tile-${subscriptionPlan.planID}`}>
+                                      <div style={{'border': '1px solid'}}>
+                                        <div style={{'marginLeft': '30px', 'marginRight': '10px'}}>
+                                          <br/>
+                                          <b>{subscriptionPlan.planName}</b><br/>
+                                          <hr/>
+                                          <br/>
+                                          <i>{subscriptionPlan.planDescription}</i><br/>
+                                          <br/>
+                                          <hr/>
+                                          <br/>
+                                          {subscriptionPlan.planCostPrefix}{subscriptionPlan.planCost}<br/>
+                                          {subscriptionPlan.planCostPostfix}<br/>
+                                          <br/>
+                                          <hr/>
+                                          <div id={'subscriptionPlanDetails_' + subscriptionPlan.planID} dangerouslySetInnerHTML={{ __html: subscriptionPlan.aboutThisPlan }}/><br/>
+                                          <br/>
+                                          <br/>
+                                          <Link to={'/join/step2/' + subscriptionPlan.planID}><Button theme={{ margin: '0 auto'}} type="button" text={subscriptionPlan.selectButtonText}/></Link><br/>
+                                        </div>
+                                       </div>
+                                      </li>)}
+                                    </ul>
+                                  </Fragment>
+                                }
+                              </Fragment>
+                            )}
+                          />
+                      </Fragment>
+                    }
                   </Fragment>
-              }
-            </Fragment>
-          )}
-        />
-        <br/>
-        <br/>
+                )}
+          />
+          <br/>
+          <br/>
       </div>
     )
   }
--- app/pages/registration/JoinStep3.js
+++ app/pages/registration/JoinStep3.js
@@ -2,12 +2,12 @@
 * V4 Join
 ********************************** */
 
-import React, { Component , cloneElement } from 'react';
+import React, { Component , cloneElement, Fragment } from 'react';
 import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { Braintree, HostedField } from 'react-braintree-fields';
+import Request from 'components/common/network/Request';
 
 const mapStateToProps = ({ appConfig, isBraintreeReady }) => ({
   appConfig,
@@ -35,20 +35,49 @@ class JoinStep3 extends Component  {
   }
 
   render() {
+    const JOIN_PAGE_ENDPOINT_URL = '/api/page/join';
+
+    const joinPageModel = {
+      name: 'JOIN_PAGE_MODEL',
+      model: resp => ({
+        pageHeading1: resp.pageHeading1,
+        pageHeading2: resp.pageHeading2,
+        sectionHeading: resp.sectionHeading,
+        hostedPaymentFormURL: resp.hostedPaymentFormURL,
+      }),
+    };
 
     return (
       <div style={{'paddingTop': '55px', 'marginLeft': 'auto', 'marginRight': 'auto', 'width': '600px'}}>
-        <header className="header">
-          <div className="icon"></div>
-        </header>
-        <h1>Joining Slooh is Easy</h1>
-        <h2>Join Slooh in three easy steps!  Simply select a plan, enter your details, make your payment, and youre in!</h2>
-        <h3>Step 3: PAYMENT INFO</h3>
-        <br/>
-        <br/>
-        <iframe style={{'width': '100%', 'minHeight': '400px'}} src="https://eris.slooh.com/getHostedPaymentForm.php"></iframe>
-        <Link to="/join/complete">Submit to Join!</Link>
+      <Request
+        serviceURL={JOIN_PAGE_ENDPOINT_URL}
+        model={joinPageModel}
+        requestBody={{ 'callSource': 'selectSubscriptionPlan' }}
+        render={({
+          fetchingContent,
+          modeledResponses: { JOIN_PAGE_MODEL },
+        }) => (
+          <Fragment>
+            {
+              !fetchingContent &&
+                <Fragment>
 
+
+                  <header className="header">
+                    <div className="icon"></div>
+                  </header>
+                  <h1>{JOIN_PAGE_MODEL.pageHeading1}</h1>
+                  <h2>{JOIN_PAGE_MODEL.pageHeading2}</h2>
+                  <h3>Step 3: {JOIN_PAGE_MODEL.sectionHeading}</h3>
+                  <br/>
+                  <br/>
+                  <iframe style={{'width': '100%', 'minHeight': '400px'}} src="{JOIN_PAGE_MODEL.hostedPaymentFormURL}"></iframe>
+                  <Link to="/join/complete">Submit to Join!</Link>
+                </Fragment>
+              }
+              </Fragment>
+            )}
+          />
       </div>
     )
   }
