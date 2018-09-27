8cde6a5d12a8a7d9824b31619e1c3d290f621896
jmasse <jmasse860@gmail.com>
2018-09-26 20:11:58 -0400

    feat(converting the tiles into links)

--- app/components/common/tiles/guide-tile/guide-tile.js
+++ app/components/common/tiles/guide-tile/guide-tile.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router';
 import { guideCorner } from 'styles/variables/iconURLs';
 import style from './guide-tile.style';
 
-const GuideTile = ({ title, subTitle }) => (
+const GuideTile = ({ title, subTitle, linkURL }) => (
   <div className="guide-tile-root">
-    <div className="guide-tile-frame">
-      <img alt="" className="top-left" src={guideCorner} />
-      <img alt="" className="top-right" src={guideCorner} />
-    </div>
+    <Link to={linkURL}>
+      <div className="guide-tile-frame">
+        <img alt="" className="top-left" src={guideCorner} />
+        <img alt="" className="top-right" src={guideCorner} />
+      </div>
 
-    <div className="guide-tile-frame bottom">
-      <img alt="" className="top-left" src={guideCorner} />
-      <img alt="" className="top-right" src={guideCorner} />
-    </div>
+      <div className="guide-tile-frame bottom">
+        <img alt="" className="top-left" src={guideCorner} />
+        <img alt="" className="top-right" src={guideCorner} />
+      </div>
 
-    <h4 className="title">{title}</h4>
-    <h5 className="subTitle">{subTitle}</h5>
+      <h4 className="title">{title}</h4>
+      <h5 className="subTitle">{subTitle}</h5>
+    </Link>
     <style jsx>{style}</style>
   </div>
 );
@@ -24,6 +27,7 @@ const GuideTile = ({ title, subTitle }) => (
 GuideTile.propTypes = {
   title: PropTypes.string.isRequired,
   subTitle: PropTypes.string.isRequired,
+  linkURL: PropTypes.string.isRequired,
 };
 
 export default GuideTile;
