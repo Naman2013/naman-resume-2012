62328b1a49283e513fcaba6f26fbe5c13485eed5
jmasse860@gmail.com <jmasse860@gmail.com>
2018-09-28 22:46:13 -0400

    feat(styling row of page icons)

--- app/components/common/pagination/v4-pagination/pagination.js
+++ app/components/common/pagination/v4-pagination/pagination.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
 import style from './pagination.style';
 
-const Pagination = () => (
+const Pagination = ({ pages }) => (
   <div>
-    <h1>Pagination</h1>
+    <ul className="page-select-root">
+      {pages
+        .map(page => <li className="page-select" key={`pagination-page-${page}`}><Link href="/home">{page}</Link></li>)
+      }
+    </ul>
     <style jsx>{style}</style>
   </div>
 );
+
+Pagination.propTypes = {
+  pages: PropTypes.arrayOf(PropTypes.number),
+};
+
+Pagination.defaultProps = {
+  pages: [1, 2, 3, 4],
+};
 
 export default Pagination;
