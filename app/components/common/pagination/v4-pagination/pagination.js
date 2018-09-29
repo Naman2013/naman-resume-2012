c32efadf7ef03aa7bc6b8833f3d6a141e546d906
jmasse860@gmail.com <jmasse860@gmail.com>
2018-09-28 21:55:28 -0400

    feat(stubbing out files for pagination)

---
+++ app/components/common/pagination/v4-pagination/pagination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './pagination.style';
+
+const Pagination = () => (
+  <div>
+    <h1>Pagination</h1>
+    <style jsx>{style}</style>
+  </div>
+);
+
+export default Pagination;
