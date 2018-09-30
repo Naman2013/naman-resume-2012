3ef1fe19abc616e189e71b1cd2a2b3da0d42ad99
jmasse <jmasse860@gmail.com>
2018-09-30 12:21:12 -0400

    feat(tracking and setting active page) - stubbing state for keeping track of attributes of the pagination)

--- app/components/common/pagination/v4-pagination/pagination.js
+++ app/components/common/pagination/v4-pagination/pagination.js
@@ -1,49 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import GenericButton from 'components/common/style/buttons/Button';
 import { horizontalArrow } from 'styles/variables/iconURLs';
 import Pages from './pages';
 import style from './pagination.style';
 
-const Pagination = ({ pages, activePage }) => (
-  <div className="pagination-root">
-    <ul className="buttons">
-      <li className="button">
-        <GenericButton text="First" />
-      </li>
-      <li className="button">
-        <GenericButton theme={{ transform: 'rotate(180deg)' }} icon={horizontalArrow} />
-      </li>
-    </ul>
+class Pagination extends Component {
+  static propTypes = {
+    activePage: PropTypes.number,
+  }
 
-    <div>
-      <Pages
-        pages={pages}
-        activePage={activePage}
-        onPageSelect={(selectedPage) => { console.log(selectedPage); }}
-      />
-    </div>
+  static defaultProps = {
+    activePage: 1,
+  }
 
-    <ul className="buttons">
-      <li className="button">
-        <GenericButton icon={horizontalArrow} />
-      </li>
-      <li className="button">
-        <GenericButton text="Last" />
-      </li>
-    </ul>
-    <style jsx>{style}</style>
-  </div>
-);
+  state = {
+    pages: [1, 2, 3, 4],
+    activePage: this.props.activePage,
+  }
 
-Pagination.propTypes = {
-  pages: PropTypes.arrayOf(PropTypes.number),
-  activePage: PropTypes.number.isRequired,
-};
+  handlePageSelect = ({ pageNumber }) => {
+    this.setState({ activePage: pageNumber });
+  }
 
-Pagination.defaultProps = {
-  pages: [1, 2, 3, 4],
-};
+  render() {
+    const { pages, activePage } = this.state;
 
+    return (
+      <div className="pagination-root">
+        <ul className="buttons">
+          <li className="button">
+            <GenericButton text="First" />
+          </li>
+          <li className="button">
+            <GenericButton theme={{ transform: 'rotate(180deg)' }} icon={horizontalArrow} />
+          </li>
+        </ul>
+
+        <div>
+          <Pages
+            pages={pages}
+            activePage={activePage}
+            onPageSelect={this.handlePageSelect}
+          />
+        </div>
+
+        <ul className="buttons">
+          <li className="button">
+            <GenericButton icon={horizontalArrow} />
+          </li>
+          <li className="button">
+            <GenericButton text="Last" />
+          </li>
+        </ul>
+        <style jsx>{style}</style>
+      </div>
+    );
+  }
+}
 
 export default Pagination;
