83b704c7462ce76763e075f919192e3d807021aa
jmasse <jmasse860@gmail.com>
2018-09-30 11:02:40 -0400

    chore(stubbing new wrapper component for dealing with network requests)

--- stories/pagination.js
+++ stories/pagination.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Pagination from '../app/components/common/pagination';
+import PaginateStories from '../app/components/common/paginate-stories';
 
 storiesOf('Pagination', module)
-  .add('Default', () => (
+  .add('Pagination component', () => (
     <Pagination />
+  ))
+  .add('Paginate stories API', () => (
+    <PaginateStories />
   ));
