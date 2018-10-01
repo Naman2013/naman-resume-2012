db920d827e35e935d77662955ba2327c4055dc84
jmasse <jmasse860@gmail.com>
2018-10-01 18:34:03 -0400

    feat(next and previous pages will increment or decrement the active page)

--- app/components/common/pagination/v4-pagination/pagination.js
+++ app/components/common/pagination/v4-pagination/pagination.js
@@ -8,19 +8,39 @@ import style from './pagination.style';
 class Pagination extends Component {
   static propTypes = {
     activePage: PropTypes.number,
+    pages: PropTypes.arrayOf(PropTypes.number),
   }
 
   static defaultProps = {
     activePage: 1,
+    pages: [1, 2, 3, 4],
   }
 
   state = {
-    pages: [1, 2, 3, 4],
+    pages: this.props.pages,
     activePage: this.props.activePage,
   }
 
   handlePageSelect = ({ pageNumber }) => {
     this.setState({ activePage: pageNumber });
+  }
+
+  handleNextPage = () => {
+    const { activePage, pages } = this.state;
+    const maxPage = pages[pages.length - 1];
+
+    if (activePage < maxPage) {
+      this.setState(prevState => ({ activePage: (prevState.activePage + 1) }));
+    }
+  }
+
+  handlePrevPage = () => {
+    const { activePage, pages } = this.state;
+    const [minPage] = pages;
+
+    if (activePage > minPage) {
+      this.setState(prevState => ({ activePage: (prevState.activePage - 1) }));
+    }
   }
 
   render() {
@@ -33,7 +53,11 @@ class Pagination extends Component {
             <GenericButton text="First" />
           </li>
           <li className="button">
-            <GenericButton theme={{ transform: 'rotate(180deg)' }} icon={horizontalArrow} />
+            <GenericButton
+              theme={{ transform: 'rotate(180deg)' }}
+              icon={horizontalArrow}
+              onClickEvent={this.handlePrevPage}
+            />
           </li>
         </ul>
 
@@ -47,7 +71,7 @@ class Pagination extends Component {
 
         <ul className="buttons">
           <li className="button">
-            <GenericButton icon={horizontalArrow} />
+            <GenericButton onClickEvent={this.handleNextPage} icon={horizontalArrow} />
           </li>
           <li className="button">
             <GenericButton text="Last" />
