988f80ab3f1ad5a89a1e92e931182adb80e36024
Todd Reisel <todd@slooh.com>
2018-10-01 19:41:38 -0400

    login flow: google sign-in to account, validation/site signin.

--- app/modules/login/actions.js
+++ app/modules/login/actions.js
@@ -3,6 +3,10 @@ import { store as storeUser } from 'modules/User';
 
 export const LOGIN_USER_START = 'LOGIN_USER_START';
 export const LOGIN_USER_FAIL = 'LOGIN_USER_FAIL';
+
+export const LOGIN_GOOGLE_USER_START = 'LOGIN_GOOGLE_USER_START';
+export const LOGIN_GOOGLE_USER_FAIL = 'LOGIN_GOOGLE_USER_FAIL';
+
 export const RESET_LOGIN_USER = 'RESET_LOGIN_USER';
 
 const logUserInStart = payload => ({
@@ -12,6 +16,16 @@ const logUserInStart = payload => ({
 
 const logUserInFail = payload => ({
   type: LOGIN_USER_FAIL,
+  payload,
+});
+
+const logGoogleUserInStart = payload => ({
+  type: LOGIN_GOOGLE_USER_START,
+  payload,
+});
+
+const logGoogleUserInFail = payload => ({
+  type: LOGIN_GOOGLE_USER_FAIL,
   payload,
 });
 
@@ -38,4 +52,27 @@ export const logUserIn = loginForm => (dispatch) => {
     }
   })
     .catch(error => dispatch(logUserInFail(error)));
+};
+
+/* Log the user in via Google SSO */
+export const logGoogleUserIn = googleProfileResult => (dispatch) => {
+  const { googleProfileId, googleProfileEmail } = googleProfileResult;
+
+  dispatch(logGoogleUserInStart());
+
+  return axios.post('/api/users/login', {
+    username: googleProfileEmail,
+    passwd: 'notrequiredforthiscall',
+    googleProfileId: googleProfileId,
+  }).then((result) => {
+    const { apiError } = result.data;
+
+    if (apiError) {
+      dispatch(logGoogleUserInFail(result.data));
+    } else {
+      dispatch(resetLogIn());
+      dispatch(storeUser(result.data));
+    }
+  })
+    .catch(error => dispatch(logGoogleUserInFail(error)));
 };
