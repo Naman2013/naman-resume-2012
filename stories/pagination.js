6fbfeb6604c17986bf566391695e2b0a40a77bc0
jmasse <jmasse860@gmail.com>
2018-10-01 20:49:54 -0400

    feat(installing network powered pagination into the guides hub)

--- stories/pagination.js
+++ stories/pagination.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, number } from '@storybook/addon-knobs';
+import store from '../app/store';
 import Pagination from '../app/components/common/pagination';
 import PaginateWithNetwork from '../app/components/common/paginate-with-network';
 
-const stories = storiesOf('Pagination', module).addDecorator(withKnobs);
-const pagesPerPage = number('pagesPerPage', 4);
+import { GET_STORIES } from '../app/services/content';
+
+
+const PAGE_TYPES = ['all', 'artCulture', 'diy', 'humanSpirit', 'scienceLog'];
+const SORT_OPTIONS = ['recent', 'likes', 'atoz', 'ztoa'];
+
+const stories = storiesOf('Pagination', module);
 stories.add('Pagination component', () => (
   <Pagination activePage={1} />
 )).add('Paginate stories API', () => (
-  <PaginateWithNetwork />
+  <Provider store={store}>
+    <PaginateWithNetwork
+      apiURL={GET_STORIES}
+      filterOptions={{
+        sortBy: SORT_OPTIONS[0],
+        page: 1,
+      }}
+    />
+  </Provider>
 ));
