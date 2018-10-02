7ed52bbe450b2eb7104f159cd392120ff915ee6a
jmasse <jmasse860@gmail.com>
2018-10-01 20:03:20 -0400

    chore

--- stories/pagination.js
+++ stories/pagination.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, number } from '@storybook/addon-knobs';
 import Pagination from '../app/components/common/pagination';
 import PaginateWithNetwork from '../app/components/common/paginate-with-network';
 
-storiesOf('Pagination', module)
-  .add('Pagination component', () => (
-    <Pagination activePage={1} />
-  ))
-  .add('Paginate stories API', () => (
-    <PaginateWithNetwork />
-  ));
+const stories = storiesOf('Pagination', module).addDecorator(withKnobs);
+const pagesPerPage = number('pagesPerPage', 4);
+stories.add('Pagination component', () => (
+  <Pagination activePage={1} />
+)).add('Paginate stories API', () => (
+  <PaginateWithNetwork />
+));
