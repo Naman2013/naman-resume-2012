f8e65a80971ca2599555761f074aa1eeb921abd7
jmasse <jmasse860@gmail.com>
2018-10-06 15:18:05 -0400

    chore(stubbed containers for each tab set)

---
+++ app/components/telescope-details/v4-modules/tab-live.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TabLive = () => (
+  <div>
+    LIVE
+  </div>
+);
+
+export { TabLive };
--- app/pages/telescope-details/v4-telescope-details.js
+++ app/pages/telescope-details/v4-telescope-details.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ColumnTabs } from 'components/Tabs';
+import { TabConditions, TabLive, TabQueue, TabTelescope } from 'components/telescope-details/v4-modules';
 
 const TelescopeDetails = () => (
   <div>
