74582e7aa61758da18c28240cee464a53146f61b
Todd Reisel <todd@slooh.com>
2018-10-06 11:31:50 -0400

    join flow: managing account signup form fields depending on normal signup or Google Account signup - attempt 5.

--- app/pages/registration/JoinStep2.js
+++ app/pages/registration/JoinStep2.js
@@ -247,7 +247,7 @@ class JoinStep2 extends Component {
                       <br/>
                       <p>{JOIN_PAGE_MODEL.formFieldLabels.loginemailaddress.label}:
                         <Field
-                          input={{'disabled': this.state.accountFormDetails.loginEmailAddress.editable}}
+                          input={{'disabled': ! this.state.accountFormDetails.loginEmailAddress.editable}}
                           name="loginEmailAddress"
                           type="email"
                           label={JOIN_PAGE_MODEL.formFieldLabels.loginemailaddress.hintText}
