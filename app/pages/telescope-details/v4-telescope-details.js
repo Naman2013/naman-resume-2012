82cd41026c4bbb0d9480d2d608f4c9f0f398c5f6
jmasse <jmasse860@gmail.com>
2018-10-06 16:10:35 -0400

    feat(rendering each tab level component)

--- app/pages/telescope-details/v4-telescope-details.js
+++ app/pages/telescope-details/v4-telescope-details.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { ColumnTabs } from 'components/Tabs';
-import { TabConditions, TabLive, TabQueue, TabTelescope } from 'components/telescope-details/v4-modules';
+import {
+  TabConditions,
+  TabLive,
+  TabQueue,
+  TabTelescope,
+} from 'components/telescope-details/v4-modules';
 
 const TelescopeDetails = () => (
   <div>
-    <ColumnTabs />
+    <ColumnTabs
+      tabConfiguration={[
+        { tabTitle: 'Live', content: () => (<TabLive />) },
+        { tabTitle: 'Queue', content: () => (<TabQueue />) },
+        { tabTitle: 'Cond.', content: () => (<TabConditions />) },
+        { tabTitle: 'Scope', content: () => (<TabTelescope />) },
+      ]}
+    />
   </div>
 );
 
