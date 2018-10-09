a0104a3a9a0bb6a81a7cd4fa932e0a9f70745fc0
Todd Reisel <treisel@coastalbt.com>
2018-10-09 14:57:44 -0400

    join flow: step 2: form field verification - SVD-213 - attempt 9 - google sso email field issue - attempt 6.

--- app/pages/registration/JoinStep2.js
+++ app/pages/registration/JoinStep2.js
@@ -405,7 +405,7 @@ class JoinStep2 extends Component {
                         />
                       </p>
                       <br/>
-                      <p>{this.state.accountFormDetails.loginEmailAddress.label}: <span style={{'color': 'red', 'fontStyle': 'italic'}}>{this.state.accountFormDetails.loginEmailAddress.errorText}</span>
+                      {this.state.accountCreationType === 'userpass' && <p>{this.state.accountFormDetails.loginEmailAddress.label}: <span style={{'color': 'red', 'fontStyle': 'italic'}}>{this.state.accountFormDetails.loginEmailAddress.errorText}</span>
                         <Field
                           name="loginEmailAddress"
                           type="email"
@@ -415,6 +415,19 @@ class JoinStep2 extends Component {
                           value={this.state.accountFormDetails.loginEmailAddress.value}
                         />
                       </p>
+                      }
+                      {this.state.accountCreationType === 'googleaccount' && <p>{this.state.accountFormDetails.loginEmailAddress.label}: <span style={{'color': 'red', 'fontStyle': 'italic'}}>{this.state.accountFormDetails.loginEmailAddress.errorText}</span>
+                        <Field
+                          name="loginEmailAddress"
+                          type="email"
+                          input={{'disabled': true}}
+                          label={this.state.accountFormDetails.loginEmailAddress.hintText}
+                          component={InputField}
+                          onChange={(event) => { this.handleFieldChange({ field: 'loginEmailAddress', value: event.target.value }); }}
+                          value={this.state.accountFormDetails.loginEmailAddress.value}
+                        />
+                      </p>
+                      }
                       <br/>
                       {this.state.accountFormDetails.loginEmailAddressVerification.visible == true && <p>{this.state.accountFormDetails.loginEmailAddressVerification.label}: <span style={{'color': 'red', 'fontStyle': 'italic'}}>{this.state.accountFormDetails.loginEmailAddressVerification.errorText}</span>
                         <Field
