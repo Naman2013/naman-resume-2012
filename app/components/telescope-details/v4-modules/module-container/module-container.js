1a2ffad4265a0f3594705eb194284a812936d641
jmasse <jmasse860@gmail.com>
2018-10-10 18:03:35 -0400

    feat(module container paints children)

--- app/components/telescope-details/v4-modules/module-container/module-container.js
+++ app/components/telescope-details/v4-modules/module-container/module-container.js
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import { ModuleHeader } from './module-header';
 import style from './module-container.style';
 
-const ModuleContainer = () => (
+const ModuleContainer = ({ children }) => (
   <div className="container">
     <ModuleHeader title="Where in the sky" />
+    {children}
     <style jsx>{style}</style>
   </div>
 );
+
+ModuleContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export { ModuleContainer };
