031ff81956de8664826e4ccf1371e85c1161690d
jmasse <jmasse860@gmail.com>
2018-10-10 18:00:04 -0400

    chore(common container for building other modules)

--- app/components/telescope-details/v4-modules/index.js
+++ app/components/telescope-details/v4-modules/index.js
@@ -2,3 +2,5 @@ export * from './tab-conditions';
 export * from './tab-live';
 export * from './tab-queue';
 export * from './tab-telescope';
+export * from './module-header';
+export * from './module-container';
