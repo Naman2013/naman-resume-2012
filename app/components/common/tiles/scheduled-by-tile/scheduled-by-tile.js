345207da2413e67431999283d51d89a9639ff027
jmasse <jmasse860@gmail.com>
2018-10-10 13:59:56 -0400

    chore(stubbing files to build out another tile design)

---
+++ app/components/common/tiles/scheduled-by-tile/scheduled-by-tile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ScheduledByTile = () => (
+  <div>
+    <h1>Scheduled By...</h1>
+  </div>
+);
+
+export { ScheduledByTile };
--- stories/Tiles.js
+++ stories/Tiles.js
@@ -15,7 +15,7 @@ import AveryTile from 'components/common/tiles/AveryTile';
 import Host from 'components/Host';
 import HostLongTile from 'components/HostLongTile';
 import GuideTile from 'components/common/tiles/guide-tile';
-import { ObjectSummaryTile } from 'components/common/tiles';
+import { ObjectSummaryTile, ScheduledByTile } from 'components/common/tiles';
 
 import { SAMPLE_IMAGE_HTML_BLOB, SAMPLE_VIDEO_HTML_BLOB } from './content/getGuidesPanels';
 
@@ -123,4 +123,7 @@ storiesOf('Tiles', module)
   ))
   .add('ObjectSummaryTile', () => (
     <ObjectSummaryTile />
+  ))
+  .add('ScheduledByTile', () => (
+    <ScheduledByTile />
   ));
