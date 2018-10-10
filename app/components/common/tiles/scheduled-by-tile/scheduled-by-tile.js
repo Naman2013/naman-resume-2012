dc99892a9418b04286cbf8ecde7cb0702b837357
jmasse <jmasse860@gmail.com>
2018-10-10 14:09:51 -0400

    chore(getting the markup down)

--- app/components/common/tiles/scheduled-by-tile/scheduled-by-tile.js
+++ app/components/common/tiles/scheduled-by-tile/scheduled-by-tile.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import starShape from 'atoms/icons/star-shape.svg';
+import style from './scheduled-by-tile.style';
 
 const ScheduledByTile = () => (
   <div>
-    <h1>Scheduled By...</h1>
+    <h3 className="title">Mission scheduled by:</h3>
+    <img className="profile-photo" alt="scheduled mission member" src="" />
+    <h4 className="profile-name">Thomas Johnson</h4>
+    <ul>
+      <li>White dwarf</li>
+      <li><img alt="" src={starShape} />129k</li>
+    </ul>
+    <style jsx>{style}</style>
   </div>
 );
 
