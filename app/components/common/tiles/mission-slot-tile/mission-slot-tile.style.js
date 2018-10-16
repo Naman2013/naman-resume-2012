cd01b9e7a7b39c605ac2c8d6f9c7fa0c88bc31ce
jmasse <jmasse860@gmail.com>
2018-10-16 19:59:48 -0400

    ifeat(first cut ui of the queue mission tile)

--- app/components/common/tiles/mission-slot-tile/mission-slot-tile.style.js
+++ app/components/common/tiles/mission-slot-tile/mission-slot-tile.style.js
@@ -1,5 +1,56 @@
 import css from 'styled-jsx/css';
+import { faintShadow } from 'styles/variables/shadows';
+import { primaryFont, secondaryFont } from 'styles/variables/fonts';
+import { astronaut, hawkesBlue } from 'styles/variables/colors_tiles_v4';
 
 export default css`
+  .mission-slot-root {
+    ${faintShadow}
+    padding: 40px;
+    color: ${astronaut};
+  }
 
+  .object-title {
+    margin: 0;
+    padding-bottom: 20px;
+    font-size: 20px;
+    font-weight: normal;
+    font-family: ${secondaryFont};
+    border-bottom: 1px solid ${hawkesBlue};
+  }
+
+  .time {
+    display: flex;
+    align-items: baseline;
+    border-bottom: 1px solid ${hawkesBlue};
+    padding: 15px 0;
+  }
+
+  .time p {
+    font-family: ${primaryFont};
+    margin: 0;
+  }
+
+  .time p:first-child {
+    margin: 0;
+    font-size: 48px;
+    font-weight: 300;
+  }
+
+  .time p:last-child {
+    font-size: 10px;
+    transform: rotate(90deg) translateX(-5px);
+  }
+
+  .attendee-info {
+    list-style-type: none;
+    font-family: ${primaryFont};
+    font-size: 12px;
+    padding: 0;
+    display: flex;
+    justify-content: space-between;
+    font-weight: 600;
+    text-transform: uppercase;
+    padding-top: 10px;
+  }
 `;
