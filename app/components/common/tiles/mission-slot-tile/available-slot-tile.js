9ecb20dd8ea79126dc71306efe7179eabce75b3e
jmasse <jmasse860@gmail.com>
2018-10-16 20:22:43 -0400

    chore(adding the ad-hoc elements from the pdf attached to the ticket)

--- app/components/common/tiles/mission-slot-tile/available-slot-tile.js
+++ app/components/common/tiles/mission-slot-tile/available-slot-tile.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DefaultButton from 'components/common/style/buttons/Button';
 import style from './available-slot-tile.style';
 
 const AvailableSlotTile = ({
   missionTitle,
   time,
   date,
-  telescopeName,
   handleScheduleClick,
 }) => (
   <div className="available-slot-root">
     <h4 className="object-title">{missionTitle}</h4>
 
     <div className="time">
-      <p>{time}</p>
-      <p>UTC</p>
+      <div className="utc-time">
+        <p>{time}</p>
+        <p>UTC</p>
+      </div>
+
+      <div>
+        <ul className="times">
+          <li>16:30 EDT</li>
+          <li>13:30 PDT</li>
+        </ul>
+      </div>
     </div>
 
     <ul className="attendee-info">
-      <li>{date}</li>
-      <li>{telescopeName}</li>
+      <li>
+        {date}
+        <DefaultButton
+          onClick={handleScheduleClick}
+          theme={{ color: 'white', border: 'none', padding: '10px 0 0 0' }}
+          text="Reserve now!"
+        />
+      </li>
     </ul>
     <style jsx>{style}</style>
   </div>
