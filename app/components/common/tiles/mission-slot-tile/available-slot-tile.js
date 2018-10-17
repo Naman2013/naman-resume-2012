b333a2f3439c31a1e52f7b143e0cc2383e680ae2
jmasse <jmasse860@gmail.com>
2018-10-16 20:14:09 -0400

    feat(first draft of the available slot tile)

--- app/components/common/tiles/mission-slot-tile/available-slot-tile.js
+++ app/components/common/tiles/mission-slot-tile/available-slot-tile.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import style from './available-slot-tile.style';
 
-const AvailableSlotTile = () => (
-  <div>
+const AvailableSlotTile = ({
+  missionTitle,
+  time,
+  date,
+  telescopeName,
+  handleScheduleClick,
+}) => (
+  <div className="available-slot-root">
+    <h4 className="object-title">{missionTitle}</h4>
 
+    <div className="time">
+      <p>{time}</p>
+      <p>UTC</p>
+    </div>
+
+    <ul className="attendee-info">
+      <li>{date}</li>
+      <li>{telescopeName}</li>
+    </ul>
     <style jsx>{style}</style>
   </div>
 );
+
+AvailableSlotTile.propTypes = {
+  missionTitle: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  telescopeName: PropTypes.string.isRequired,
+  handleScheduleClick: PropTypes.func.isRequired,
+};
 
 export { AvailableSlotTile };
--- stories/Tiles.js
+++ stories/Tiles.js
@@ -160,7 +160,7 @@ storiesOf('Tiles', module)
     <ScheduledByTile />
   ))
   .add('AvailableSlotTile', () => (
-    <AvailableSlotTile />
+    <AvailableSlotTile missionTitle="The moon" time="20:30" date="Mon. Jan. 06" telescopeName="Canary three" />
   ))
   .add('MissionSlotTile', () => (
     <MissionSlotTile missionTitle="The moon" time="20:30" date="Mon. Jan. 06" attendeeCount="17" />
