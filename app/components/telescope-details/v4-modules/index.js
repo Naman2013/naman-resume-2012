9e3e0d7f875163f6a7aa6a64f98b396eb56b459f
jmasse <jmasse860@gmail.com>
2018-10-26 11:31:59 -0400

    chore(stubbing out telescope detail)

--- app/components/telescope-details/v4-modules/index.js
+++ app/components/telescope-details/v4-modules/index.js
@@ -15,6 +15,7 @@ export * from './moonlight-conditions';
 export * from './day-night-bar';
 export * from './day-night-map';
 export * from './weekly-forecast';
+export * from './telescope-detail';
 
 
 export * from './common/simple-list';
---
+++ app/components/telescope-details/v4-modules/telescope-detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './telescope-detail.style';
+
+const TelescopeDetail = () => (
+  <div>
+    <h1>Telescope detail</h1>
+    <style jsx>{style}</style>
+  </div>
+);
+
+export { TelescopeDetail };
