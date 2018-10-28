5a74ad73441711786aea6074da04fad386396b3a
Todd Reisel <todd@slooh.com>
2018-10-28 19:17:56 -0400

    display district/school dropdowns based on preceding form fields (zipcode and district selection).

--- app/pages/registration/JoinStep1SchoolSelection.js
+++ app/pages/registration/JoinStep1SchoolSelection.js
@@ -12,7 +12,7 @@ import { browserHistory } from 'react-router';
 import { Field, reduxForm } from 'redux-form';
 import InputField from 'components/form/InputField';
 import styles from './JoinStep1SchoolSelection.style';
-
+import { CLASSROOM_GET_US_DISTRICTLIST_ENDPOINT_URL, CLASSROOM_GET_US_SCHOOLLIST_ENDPOINT_URL } from 'services/classroom/classroom';
 import axios from 'axios';
 
 class JoinStep1SchoolSelection extends Component {
@@ -21,6 +21,8 @@ class JoinStep1SchoolSelection extends Component {
   }
 
   state = {
+    schoolDistrictOptions: { },
+    schoolOptions: { },
     schoolDistrictFormDetails: {
       zipcode: {
         label: '',
@@ -29,27 +31,98 @@ class JoinStep1SchoolSelection extends Component {
         errorText: ''
       },
       school: {
-        label: '', value: '', hintText: '', errorText: ''
+        label: '', value: '', hintText: '', errorText: '',
       },
       district: {
-        label: '', value: '', hintText: '', errorText: ''
+        label: '', value: '', hintText: '', errorText: '',
       },
     },
   };
 
-  /* This function handles a field change in the form and sets the state accordingly */
-  handleFieldChange({ field, value }) {
+  /* This function handles a zipcode change in the form and sets the state accordingly */
+  handleZipCodeChange({ value }) {
     /* Get the existing state of the signup form, modify it and re-set the state */
+    const field = "zipcode";
     const schoolDistrictFormDetailsData = cloneDeep(this.state.schoolDistrictFormDetails);
     schoolDistrictFormDetailsData[field].value = value;
-
-    //console.log(field);
-    //console.log(value);
-    //console.log(accountFormDetailsData);
 
     this.setState({
       schoolDistrictFormDetails: schoolDistrictFormDetailsData,
     });
+
+    if (value.length >=5) {
+      //get a list of school districts for this zipcode.
+      //console.log("Get a list of districts for this zipcode." + value);
+
+      const schoolDistrictsResult = axios.post(CLASSROOM_GET_US_DISTRICTLIST_ENDPOINT_URL, {
+        zipcode: value,
+        includePleaseSelectOption: "yes",
+      })
+        .then((response) => {
+          const res = response.data;
+          if (res.apiError == false) {
+            const districtResult = {
+              districtList: res.districtList,
+            }
+            this.setState({
+              schoolDistrictOptions: districtResult.districtList,
+              schoolOptions: { },
+            });
+          }
+        })
+        .catch((err) => {
+          throw ('Error: ', err);
+        });
+    }
+    else {
+      //reset the form
+      this.setState({
+        schoolDistrictOptions: { },
+        schoolOptions: { },
+      });
+    }
+  }
+
+  /* This function handles a zipcode change in the form and sets the state accordingly */
+  handleSchoolDistrictChange({ value }) {
+    /* Get the existing state of the signup form, modify it and re-set the state */
+    const field = "district";
+    const schoolDistrictFormDetailsData = cloneDeep(this.state.schoolDistrictFormDetails);
+    schoolDistrictFormDetailsData[field].value = value;
+
+    this.setState({
+      schoolDistrictFormDetails: schoolDistrictFormDetailsData,
+    });
+
+    if (value !== "") {
+      //get a list of schools for this school district.
+      //console.log("Get a list of schools for this district." + value);
+
+      const schoolResult = axios.post(CLASSROOM_GET_US_SCHOOLLIST_ENDPOINT_URL, {
+        ncesDistrictId: value,
+        includePleaseSelectOption: "yes",
+      })
+        .then((response) => {
+          const res = response.data;
+          if (res.apiError == false) {
+            const schoolResult = {
+              schoolList: res.schoolList,
+            }
+            this.setState({
+              schoolOptions: schoolResult.schoolList,
+            });
+          }
+        })
+        .catch((err) => {
+          throw ('Error: ', err);
+        });
+    }
+    else {
+      //reset the form
+      this.setState({
+        schoolOptions: { },
+      });
+    }
   }
 
   handleSubmit = (formValues) => {
@@ -61,16 +134,6 @@ class JoinStep1SchoolSelection extends Component {
   }
 
   render() {
-    const districtList = [
-      {key: "-1", name: "Please Select a District"},
-      {key: "1.0", name: "Region 14"},
-    ];
-
-    const schoolList = [
-      {key: "-1", name: "Please Select a School"},
-      {key: "1.0", name: "Bethlehem Elementary School"},
-    ];
-
     return (
       <div className="step-root">
         <div className="inner-container">
@@ -84,31 +147,40 @@ class JoinStep1SchoolSelection extends Component {
                   type="name"
                   label="Enter your zipcode"
                   component={InputField}
-                  onChange={(event) => { this.handleFieldChange({ field: 'zipcode', value: event.target.value }); }}
+                  onChange={(event) => { this.handleZipCodeChange({ value: event.target.value }); }}
                 />
               </div>
 
+              {this.state.schoolDistrictOptions.length > 0 &&
               <div className="form-field-container">
                 <span className="form-label">District:</span>
                 <Field
                   name="district"
                   className="input-row form-group form-control"
                   component="select"
+                  onChange={(event) => { this.handleSchoolDistrictChange({ value: event.target.value }); }}
                 >
-                  {districtList.map(ranking =>
-                    <option value={ranking.key} key={ranking.key}>{ranking.name}</option>)
+                  {this.state.schoolDistrictOptions.map(schoolDistrict =>
+                    <option value={schoolDistrict.NCESDistrictId} key={schoolDistrict.NCESDistrictId}>{schoolDistrict.DistrictName}</option>)
                   }
                 </Field>
               </div>
+              }
 
+              {this.state.schoolOptions.length &&
               <div className="form-field-container">
                 <span className="form-label">School:</span>
-                <Field name="school" className="input-row form-group form-control" component="select">
-                  {schoolList.map(ranking =>
-                    <option value={ranking.key} key={ranking.key}>{ranking.name}</option>
-                  )}
+                <Field
+                  name="school"
+                  className="input-row form-group form-control"
+                  component="select"
+                  >
+                  {this.state.schoolOptions.map(school =>
+                    <option value={school.NCESSchoolId} key={school.NCESSchoolId}>{school.SchoolName}</option>)
+                  }
                 </Field>
               </div>
+              }
             </div>
 
             <div className="button-container">
@@ -132,8 +204,8 @@ class JoinStep1SchoolSelection extends Component {
   }
 }
 
-const mapStateToProps = ({ joinAccountForm }) => ({
-  joinAccountForm: joinAccountForm,
+const mapStateToProps = ({ schoolDistrictForm }) => ({
+  schoolDistrictForm: schoolDistrictForm,
 });
 
 export default connect(mapStateToProps, null)(reduxForm({ form: 'schoolDistrictForm', enableReinitialize: true, })(JoinStep1SchoolSelection));
--- app/services/classroom/classroom.js
+++ app/services/classroom/classroom.js
@@ -1,3 +1,5 @@
 /* Centralized repository of classroom API endpoints */
 export const CLASSROOM_CREATENEWGROUP_PAGE_ENDPOINT_URL = '/api/page/createNewGroup';
 export const CLASSROOM_CREATENEWGROUP_ENDPOINT_URL = '/api/classroom/createNewGroup';
+export const CLASSROOM_GET_US_DISTRICTLIST_ENDPOINT_URL = '/api/school/getDistrictList';
+export const CLASSROOM_GET_US_SCHOOLLIST_ENDPOINT_URL = '/api/school/getSchoolList';
