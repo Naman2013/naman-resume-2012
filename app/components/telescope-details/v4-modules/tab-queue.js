4549b03c6ceeea17acfa5ce33de6c5391ab0e393
Todd Reisel <treisel@coastalbt.com>
2018-11-14 17:36:27 -0500

    telescope-details: Observatory Bot stubbed functionality.

--- app/components/telescope-details/v4-modules/tab-queue.js
+++ app/components/telescope-details/v4-modules/tab-queue.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { AvailableSlotTile, MissionSlotTile } from 'components/common/tiles';
 import { QueueNavigation } from './';
+import ObservatoryBot from 'components/telescope-details/ObservatoryBot/ObservatoryBot';
 import style from './tab-queue.style';
 
 const testStuff = [0, 0, 1, 1, 0, 1, 0, 1];
 
 const TabQueue = () => (
   <div className="tab-queue-root">
+    <div className="tile-container">
+      <ObservatoryBot
+        viewGroup={"queue"}
+        teleSystem={"teide1highmag"}
+      />
+    </div>
+    <br/>
     <QueueNavigation
       handlePrevClick={() => { console.log('click-prev'); }}
       handleNextClick={() => { console.log('click-next'); }}
