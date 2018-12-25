c5ccd32cf5aa3c2faec125f538d63e16ffcd8d5f
Sviatoslav Nazar <sviatoslav.nazar@inoxoft.com>
2018-12-25 14:52:12 +0200

    [SV4FED-2] Quests: Migrate static English content ot resource file

--- app/components/quest-details/modals/resources-modal.js
+++ app/components/quest-details/modals/resources-modal.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import noop from 'lodash/noop';
+import { intlShape, injectIntl } from 'react-intl';
 import SterlingTitle from 'components/common/titles/SterlingTitle';
 import IntroText from 'components/common/form-sections/intro-text';
 import GenericButton from 'components/common/style/buttons/Button';
 import style from './resources-modal.style';
+import messages from './resources-modal.messages';
 
 const ResourcesModal = ({
   appendixSubheader,
   appendixHeader,
   appendixText,
   closeModal,
+  intl,
 }) => (
   <div className="root">
     <SterlingTitle
@@ -20,7 +23,7 @@ const ResourcesModal = ({
     <IntroText desc={appendixText} />
     <GenericButton
       onClickEvent={closeModal}
-      text="Close"
+      text={intl.formatMessage(messages.Close)}
     />
     <style jsx>{style}</style>
   </div>
@@ -31,10 +34,11 @@ ResourcesModal.propTypes = {
   appendixHeader: PropTypes.string.isRequired,
   appendixText: PropTypes.string.isRequired,
   closeModal: PropTypes.func,
+  intl: intlShape.isRequired,
 };
 
 ResourcesModal.defaultProps = {
   closeModal: noop,
 }
 
-export default ResourcesModal;
+export default injectIntl(ResourcesModal);
