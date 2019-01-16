a13592fa1aa35ade49acdf0e4b56096f4efe6505
Sviatoslav Nazar <sviatoslav.nazar@inoxoft.com>
2019-01-16 15:48:51 +0200

    [SV4FED-133] Object miscellaneous part2

--- app/components/ask-astronomer/Modals/Modals.style.js
+++ app/components/ask-astronomer/Modals/Modals.style.js
@@ -1,6 +1,12 @@
 import css from 'styled-jsx/css';
 import { faintShadow } from 'styles/variables/shadows';
-import { astronaut, romance, seashell, golden_yellow, shadows } from 'styles/variables/colors_tiles_v4';
+import {
+  astronaut,
+  romance,
+  seashell,
+  golden_yellow,
+  shadows,
+} from 'styles/variables/colors_tiles_v4';
 import { primaryFont, secondaryFont } from 'styles/variables/fonts';
 import { screenMedium } from 'styles/variables/breakpoints';
 import { backgroundImageCover, dropShadowContainer } from 'styles/mixins/utilities';
@@ -78,6 +84,7 @@ export default css`
 
   .button-container {
     display: flex;
+    flex-flow: row wrap;
     justify-content: space-between;
     margin-top: 15px;
     align-items: center;
