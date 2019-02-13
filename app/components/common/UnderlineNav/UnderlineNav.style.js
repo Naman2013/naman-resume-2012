45c71778a5a05276084f60559d7dc3313074510e
denmelnyk <denys.melnyk@inoxoft.com>
2019-02-13 12:51:50 +0200

    [SV4FED-165] Change unselected navitem color

--- app/components/common/UnderlineNav/UnderlineNav.style.js
+++ app/components/common/UnderlineNav/UnderlineNav.style.js
@@ -1,9 +1,7 @@
 import css from 'styled-jsx/css';
 import { faintShadow } from 'styles/variables/shadows';
-import { astronaut, romance, golden_yellow, shadows } from 'styles/variables/colors_tiles_v4';
+import { astronaut, moodyBleu } from 'styles/variables/colors_tiles_v4';
 import { primaryFont, secondaryFont } from 'styles/variables/fonts';
-import { screenLarge } from 'styles/variables/breakpoints';
-import { backgroundImageCover, dropShadowContainer } from 'styles/mixins/utilities';
 
 export default css`
   .root {
@@ -31,7 +29,7 @@ export default css`
     text-transform: uppercase;
     font-family: ${primaryFont};
     font-size: 11px;
-    color: ${shadows};
+    color: ${moodyBleu};
     cursor: pointer;
   }
 
