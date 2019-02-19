62716f5b4627fbe3acd2e2b01f735149a5a051f5
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-02-19 23:09:38 +0200

    SV4FED-198 [Profiles]: Profiles Activity Tab: Add "Your Top Picks" active-groups component

--- app/components/profiles/private-profile/active-groups.js
+++ app/components/profiles/private-profile/active-groups.js
@@ -1,24 +1,29 @@
-/* eslint-disable */
-import React, {Fragment} from 'react';
-import {FormattedMessage} from 'react-intl';
+import React, { Fragment } from 'react';
+import { FormattedMessage } from 'react-intl';
 import GroupTiles from '../../groups-hub/group-tiles';
+import { HeaderWithCounter } from '../../header-with-counter';
 
 export const ActiveGroups = (props) => {
-  const {count, list} = props;
-  return <Fragment>
-    <div className="header">
-      <FormattedMessage id="Profile.Groups"/>
-      <span>({count})</span>
-    </div>
-    <div className="groups">
-
-      <GroupTiles
-        closeModal={() => {}}
-        updateGroupItemInfo={() => {}}
-        updatePrompt={() => {}}
-        groups={list}
+  const { count, list } = props; //eslint-disable-line
+  return (
+    <Fragment>
+      <HeaderWithCounter
+        txt={<FormattedMessage id="Profile.Groups" className="heading" />}
+        count={count}
       />
+      <div className="groups">
 
-    </div>
-  </Fragment>;
+        <GroupTiles
+          closeModal={() => {
+          }}
+          updateGroupItemInfo={() => {
+          }}
+          updatePrompt={() => {
+          }}
+          groups={list}
+        />
+
+      </div>
+    </Fragment>
+  );
 };
