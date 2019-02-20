036ae51b5de334ebc22a033bdfe58630752246ac
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-02-20 11:10:30 +0200

    SV4FED-198 [Profiles]: Profiles Activity Tab: Add "Your Top Picks" ActiveObjects styling

--- app/components/profiles/private-profile/active-objects.js
+++ app/components/profiles/private-profile/active-objects.js
@@ -1,26 +1,32 @@
+import LailaTile from 'app/components/common/tiles/LailaTile';
 import { HeaderWithCounter } from 'app/components/header-with-counter';
+import PropTypes from 'prop-types';
 import React, { Fragment } from 'react';
 import { FormattedMessage } from 'react-intl';
-import GuideTiles from '../../guides-hub/guide-tiles';
+import styles from './active-objects.module.scss';
 
 export const ActiveObjects = (props) => {
-  const { count, list } = props;  //eslint-disable-line
+  const { count, list = [] } = props;
   return (
     <Fragment>
       <HeaderWithCounter
         txt={<FormattedMessage id="Profile.Objects" />}
         count={count}
       />
-      <div className="groups">
-
-        <GuideTiles
-          updateReadingListInfo={() => {
-          }}
-          guides={list}
-          // isMobile={context.isMobile}
-        />
+      <div className={styles.objectsWrapper}>
+        {list.map(obj => (<LailaTile
+          key={obj.objectId}
+          iconURL={obj.iconUrl}
+          title={obj.imageTitle}
+          linkURL={obj.linkUrl}
+        />))}
 
       </div>
     </Fragment>
   );
+};
+
+ActiveObjects.propTypes = {
+  count: PropTypes.number.isRequired,
+  list: PropTypes.arrayOf().isRequired,
 };
