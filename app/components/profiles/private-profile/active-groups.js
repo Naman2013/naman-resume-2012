8130dafa749faf2ec5f9380673e67af189dc2abb
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-02-21 19:16:14 +0200

    Updated Top Picks to support topPicksForYouGroupsHeading and topPicksForYouObjectsHeading

--- app/components/profiles/private-profile/active-groups.js
+++ app/components/profiles/private-profile/active-groups.js
@@ -1,30 +1,28 @@
 import { HeaderWithCounter } from 'app/components/header-with-counter';
+import PropTypes from 'prop-types';
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import GroupTiles from '../../groups-hub/group-tiles';
 import styles from './active-groups.module.scss';
 
-export const ActiveGroups = (props) => {
-  const { count, list } = props; //eslint-disable-line
+export const ActiveGroups = props => {
+  const { count, list, header } = props;
   return (
     <div className={styles.activeGroups}>
-      <HeaderWithCounter
-        txt={<FormattedMessage id="Profile.Groups" />}
-        count={count}
-      />
+      <HeaderWithCounter txt={header} count={count} />
       <div className={styles.groupsWrapper}>
-
         <GroupTiles
-          closeModal={() => {
-          }}
-          updateGroupItemInfo={() => {
-          }}
-          updatePrompt={() => {
-          }}
+          closeModal={() => {}}
+          updateGroupItemInfo={() => {}}
+          updatePrompt={() => {}}
           groups={list}
         />
-
       </div>
     </div>
   );
+};
+
+ActiveGroups.propTypes = {
+  count: PropTypes.number.isRequired,
+  list: PropTypes.arrayOf().isRequired,
+  header: PropTypes.string.isRequired,
 };
