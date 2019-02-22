cb0664520643d8759ae0f3beb32feee8510e1470
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-02-22 14:38:07 +0200

    hotfix for urls

--- app/services/stories/index.js
+++ app/services/stories/index.js
@@ -1,1 +1,3 @@
 export const STORY_DETAILS = '/api/content/getPost';
+export const STORIES_PAGE_ENDPOINT_URL = '/api/page/storiesHub';
+export const STORIES_ENDPOINT_URL = '/api/content/getStories';
