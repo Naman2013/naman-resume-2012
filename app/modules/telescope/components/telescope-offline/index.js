f8ac6e48f8c5a2d1a166d9092b8ae26e26b1edf5
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-02-28 16:24:12 +0200

    SV4FED-326 [Telescopes]: Offline State Implementation tabs

--- app/modules/telescope/components/telescope-offline/index.js
+++ app/modules/telescope/components/telescope-offline/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Tab, Tabs } from 'react-bootstrap';
 
 export class TelescopeOffline extends Component {
   render() {
@@ -7,7 +7,22 @@ export class TelescopeOffline extends Component {
     return (
       <Container>
         <Row>
-          <Col>Offline</Col>
+          <Col>
+            <h1>Canary One: Offline</h1>
+            <hr />
+            <Tabs defaultActiveKey="STATUS" id="uncontrolled-tab-example">
+              <Tab eventKey="STATUS" title="STATUS">
+                STATUS
+              </Tab>
+              <Tab eventKey="QUEUE" title="QUEUE" disabled>
+                QUEUE
+              </Tab>
+              <Tab eventKey="ABOUT_THIS_SCOPE" title="ABOUT THIS SCOPE">
+                ABOUT THIS SCOPE
+              </Tab>
+            </Tabs>
+            ;
+          </Col>
         </Row>
       </Container>
     );
