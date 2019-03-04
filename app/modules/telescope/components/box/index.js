a58fec9ad07dcd21b96f2105fd9d8bafca83c222
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-03-04 10:05:04 +0200

    SV4FED-326 [Telescopes]: Offline State Implementation box

--- app/modules/telescope/components/box/index.js
+++ app/modules/telescope/components/box/index.js
@@ -5,7 +5,9 @@ export const Box = props => {
   const { header, children } = props;
   return (
     <div className="box">
-      <div className="box-header">{header}</div>
+      <div className="box-header">
+        <h3 className="h3-custom">{header}</h3>
+      </div>
       <div className="box-body">{children}</div>
     </div>
   );
--- app/modules/telescope/components/status-tab/index.js
+++ app/modules/telescope/components/status-tab/index.js
@@ -12,10 +12,62 @@ export const StatusTab = props => {
           <Box header="OBSERVATORY INFORMATION">
             <div>test</div>
           </Box>
+          <Box header="THIS JUST IN!">
+            <h4 className="h4-custom">MOON RISING</h4>
+            <p className="p19">
+              The Moon is rising at the Canary Islands Observatory!
+            </p>
+            <hr />
+            <h4 className="h4-custom">TELESCOPE ALERT</h4>
+            <h2 className="h2-custom">Canary One is down to Weather</h2>
+            <p className="p19">
+              Sometimes these things happen. If you had a Mission scheduled for
+              this time, you’re account will be credited.
+            </p>
+            <hr />
+          </Box>
+          <Box header="NEEDS A TITLE">
+            <div>test</div>
+          </Box>
         </Col>
         <Col md={4}>
           <Box header="SKY CONDITIONS">
-            <div>test</div>
+            {/*<div className="sky-cond">test</div>*/}
+            <h4 className="h4-custom">SEEING CONDITIONS:</h4>
+            <h2 className="h2-custom">Level 3</h2>
+            <p className="p-19">
+              Almost continuous distortion with occasional brief good moments.
+            </p>
+            <hr />
+
+            <h4 className="h4-custom">Measured FWHM Telemetry:</h4>
+            <h2 className="h2-custom">N.N Arcseconds</h2>
+            <hr />
+
+            <h4 className="h4-custom">Transparency:</h4>
+            <h2 className="h2-custom">Moderate Haze</h2>
+          </Box>
+
+          <Box header="WEATHER CONDITIONS">
+            <h4 className="h4-custom">Temperature</h4>
+            <h2 className="h2-custom float-right">67° F</h2>
+            <hr />
+
+            <h4 className="h4-custom">Dew Point</h4>
+            <h2 className="h2-custom float-right">57° F</h2>
+            <hr />
+
+            <h4 className="h4-custom">Dew Point</h4>
+            <h2 className="h2-custom float-right">67° F</h2>
+            <hr />
+
+            <h4 className="h4-custom">Temperature</h4>
+            <h2 className="h2-custom float-right">67° F</h2>
+            <hr />
+
+            <h4 className="h4-custom">Temperature</h4>
+            <h2 className="h2-custom float-right">67° F</h2>
+            <hr />
           </Box>
         </Col>
       </Row>
