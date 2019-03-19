bfb4b1ee017e62a8c541a978c745acea373f2c05
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-03-19 16:46:27 +0200

    [Telescopes] Online, Cond-B: Implement Day/Night Map Widget In Conditions Tab

--- app/modules/telescope/components/old/satellite.js
+++ app/modules/telescope/components/old/satellite.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import { ModalImg } from 'app/modules/telescope/components/modal-img';
+import React, { Component } from 'react';
 import { ModuleContainer } from './index';
 
-const Satellite = ({ satelliteImageURL }) => (
-  <ModuleContainer title="Satellite">
-    <img alt="satellite" width="100%" src={satelliteImageURL} />
-  </ModuleContainer>
-);
+export class Satellite extends Component {
+  state = {
+    isModalOpen: false,
+  };
 
-export { Satellite };
+  openModal = () => this.setState({ isModalOpen: true });
+
+  closeModal = () => this.setState({ isModalOpen: false });
+
+  render() {
+    const { satelliteImageURL } = this.props;
+    const { isModalOpen } = this.state;
+
+    return (
+      <ModuleContainer title="Satellite">
+        <img
+          alt="satellite"
+          style={{ width: '100%', cursor: 'pointer' }}
+          src={satelliteImageURL}
+          onClick={this.openModal}
+          role="presentation"
+        />
+        <ModalImg
+          isOpen={isModalOpen}
+          imageURL={satelliteImageURL}
+          onHide={this.closeModal}
+        />
+      </ModuleContainer>
+    );
+  }
+}
