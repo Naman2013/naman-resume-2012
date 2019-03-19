b41e94057ab8f290f02a9dd4da468c36287ef721
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-03-19 16:21:05 +0200

    [Telescopes] Online, Cond-B: Implement Day/Night Map Widget In Conditions Tab

--- app/modules/telescope/components/old/day-night-map.js
+++ app/modules/telescope/components/old/day-night-map.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import { ModalImg } from 'app/modules/telescope/components/modal-img';
+import React, { Component } from 'react';
 import { ModuleContainer } from './index';
 
-const DayNightMap = ({ dayNightMapURL }) => (
-  <ModuleContainer title="Day/night map">
-    <img alt="Day night map" width="100%" height="300" src={dayNightMapURL} />
-  </ModuleContainer>
-);
+export class DayNightMap extends Component {
+  state = {
+    isModalOpen: false,
+  };
 
-export { DayNightMap };
+  openModal = () => this.setState({ isModalOpen: true });
+
+  closeModal = () => this.setState({ isModalOpen: false });
+
+  render() {
+    const { dayNightMapURL } = this.props;
+    const { isModalOpen } = this.state;
+    return (
+      <ModuleContainer title="Day/night map">
+        <img
+          alt="Day night map"
+          style={{ width: '100%', cursor: 'pointer' }}
+          onClick={this.openModal}
+          role="presentation"
+          src={dayNightMapURL}
+        />
+
+        <ModalImg
+          isOpen={isModalOpen}
+          imageURL={dayNightMapURL}
+          onHide={this.closeModal}
+        />
+      </ModuleContainer>
+    );
+  }
+}
