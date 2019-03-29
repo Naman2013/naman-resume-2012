422445a97ce467a444673df1bad2e79bdb22f766
Sviatoslav Nazar <sviatoslav.nazar@inoxoft.com>
2019-03-29 17:37:39 +0200

    SV4FED-474 All links should not have inderlines on hover

--- app/index.js
+++ app/index.js
@@ -12,11 +12,12 @@ import I18nProvider from './providers/I18nProvider';
 // redux store
 import store from './store';
 import 'animate.css/animate.css';
+// Bootstrap 4
+import './styles/bootstrap/index.scss';
+
 import './styles/animations.scss';
 import './styles/app.scss';
 import './styles/interface.css';
-// Bootstrap 4
-import './styles/bootstrap/index.scss';
 
 import './styles/static.scss';
 // utilities
