b9ffdf96df980f068b9100d1846de01622e11c6e
Anastasiia Bortniichuk <anastasiia.bortniichuk@inoxoft.com>
2019-04-04 18:38:26 +0300

    Selectors added

--- app/modules/mission-details/containers/mission-details.js
+++ app/modules/mission-details/containers/mission-details.js
@@ -1,21 +1,27 @@
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { getMissionDetails } from '../thunks';
 import MissionDetails from '../components/mission-details';
+import {
+  makeMissionDetailsLoadingSelector,
+  makeMissionDetailsTitleSelector,
+  makeMissionDetailsIconURLSelector,
+  makeMissionDetailsDateCreatedSelector,
+  makeMissionDetailsImageCountSelector,
+  makeMissionDetailsImageListSelector,
+  makeMissionDetailsApiURLSelector,
+} from '../selectors';
 
-// TODO: selectors
-
-const mapStateToProps = ({ missionDetails }) => {
-  return {
-    isFetching: missionDetails.isFetching,
-    missionTitle: missionDetails.missionTitle,
-    missionIconURL: missionDetails.missionIconURL,
-    missionDateCreated: missionDetails.missionDateCreated,
-    imageCount: missionDetails.imageCount,
-    imageList: missionDetails.imageList,
-    apiURL: missionDetails.apiURL,
-  };
-};
+const mapStateToProps = createStructuredSelector({
+  isFetching: makeMissionDetailsLoadingSelector(),
+  missionTitle: makeMissionDetailsTitleSelector(),
+  missionIconURL: makeMissionDetailsIconURLSelector(),
+  missionDateCreated: makeMissionDetailsDateCreatedSelector(),
+  imageCount: makeMissionDetailsImageCountSelector(),
+  imageList: makeMissionDetailsImageListSelector(),
+  apiURL: makeMissionDetailsApiURLSelector(),
+});
 
 const mapDispatchToProps = {
   getMissionDetails,
