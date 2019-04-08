bbbe7b16995b59a9fd8c841bc572549cd97260fd
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-04-08 18:01:44 +0300

    refactored available-mission-tile

--- app/modules/missions/components/constellation/index.js
+++ app/modules/missions/components/constellation/index.js
@@ -1,11 +1,10 @@
 import { Box } from 'app/modules/missions/components/box';
 import React, { Component } from 'react';
-import moment from 'moment';
 import { AvailbleMissionTile } from '../available-mission-tile';
-import { MissionSuccessModal } from '../mission-success-modal';
 import { ConstellationSetup } from '../constellation-setup';
-import { ExplanationModal } from '../explanation-modal';
 import { ExpireCountdown } from '../expire-countdown';
+import { ExplanationModal } from '../explanation-modal';
+import { MissionSuccessModal } from '../mission-success-modal';
 import './styles.scss';
 
 export class Constellation extends Component {
@@ -67,10 +66,6 @@ export class Constellation extends Component {
     }).then(() => this.setState({ successModalShow: true }));
   };
 
-  getMissionDate = timestamp => moment.unix(timestamp).format('ddd. MMM. DD');
-
-  getMissionTime = timestamp => moment.unix(timestamp).format('HH:mm');
-
   modalClose = () => {
     const { resetMissionsData } = this.props;
     this.setState({ successModalShow: false }, () => resetMissionsData());
@@ -128,13 +123,9 @@ export class Constellation extends Component {
               <Box inside>
                 {missionSlot && missionSlot.missionAvailable ? (
                   <AvailbleMissionTile
-                    title={missionSlot.title}
-                    telescope={missionSlot.telescopeName}
-                    description={missionSlot.explanation}
-                    date={this.getMissionDate(missionSlot.missionStart)}
-                    time={this.getMissionTime(missionSlot.missionStart)}
-                    cancel={this.cancelMissionSlot}
-                    scheduleMission={this.reserveMissionSlot}
+                    missionSlot={missionSlot}
+                    onCancel={this.cancelMissionSlot}
+                    onSubmit={this.reserveMissionSlot}
                   />
                 ) : (
                   <div className="reserved-mission-gag">
