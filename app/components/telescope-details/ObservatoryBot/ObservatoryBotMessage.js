b1edc2ec0f5323d95ae23c9ed5c48c196fd4b87f
Anastasiia Bortniichuk <anastasiia.bortniichuk@inoxoft.com>
2019-04-17 15:59:36 +0300

    uniqueId removed

--- app/components/telescope-details/ObservatoryBot/ObservatoryBotMessage.js
+++ app/components/telescope-details/ObservatoryBot/ObservatoryBotMessage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import uniqueId from 'lodash/fp/uniqueId';
 import isEmpty from 'lodash/fp/isEmpty';
 
 const ObservatoryBotMessage = props => {
@@ -10,7 +9,7 @@ const ObservatoryBotMessage = props => {
     message: { Message, DTG },
   } = props;
   return (
-    <div className="observatorybot-message" key={uniqueId()}>
+    <div className="observatorybot-message" key={`${Message}-${DTG}`}>
       <p>{moment.utc(DTG).fromNow()}</p>
       <p dangerouslySetInnerHTML={{ __html: Message }} />
     </div>
