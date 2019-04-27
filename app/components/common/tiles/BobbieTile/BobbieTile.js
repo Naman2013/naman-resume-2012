40b522619fd5ea0f917fe10aa105daf565b8ad77
Todd Reisel <todd@slooh.com>
2019-04-27 10:19:42 -0400

    updated BobbieTile for readmore/less functionality.

--- app/components/common/tiles/BobbieTile/BobbieTile.js
+++ app/components/common/tiles/BobbieTile/BobbieTile.js
@@ -1,57 +1,90 @@
-import React from 'react';
+import bobbietilestyle from 'app/components/common/tiles/BobbieTile/BobbieTile.style.js';
+import cmsstyle from 'app/components/common/tiles/BobbieTile/CMS.style.js';
+import truncate from 'lodash/truncate';
 import PropTypes from 'prop-types';
-import ImageClickHandler from '../../ImageClickHandler';
-import { horizontalArrow } from 'app/styles/variables/iconURLs';
+import React, { Component, Fragment } from 'react';
 import style from './BobbieTile.style';
 import CMSStyle from './CMS.style';
+import ImageClickHandler from '../../ImageClickHandler';
 
-/**
-  @BobbieTile
-  This tile is hard coded to an expected HTML blob provided
-  by the `getGuidePanels` response.
-  40|60
-  60|40
-  50|50
-  image and video placement support
-  https://docs.google.com/document/d/1PK8X1clsV618gHQWtBnwRcFCVsTbpMhSbIYvnz8MW6Y/edit#
-*/
+const TRUNCATED_CONTENT_LENGTH = 800;
+const TRUNCATED_BUTTON_TEXT = 'read more';
+const DISPLAYED_BUTTON_TEXT = 'read less';
 
-const BobbieTile = ({
-  showTitle,
-  title,
-  showSubtitle,
-  subtitle,
-  readDuration,
-  authorName,
-  HTMLBlob,
-}) => (
-  <div className="root">
-    <div className="tile-content-container">
-      {showTitle === true ? <h3>{title}</h3> : null}
-      {showSubtitle === true ? <div className="subtitle">{subtitle}</div> : null}
-      {/*
-      <div className="post-meta-data">
-        <ul>
-          <li className="read-duration">{readDuration} mins</li>
-          <li className="author-name">
-            <div>
-              <span>By</span> {authorName}
+class BobbieTile extends Component {
+  static propTypes = {
+    content: PropTypes.string.isRequired,
+  };
+
+  state = {
+    buttonText: TRUNCATED_BUTTON_TEXT,
+    contentLength: TRUNCATED_CONTENT_LENGTH,
+  };
+
+  handleReadMoreClick = () => {
+    if (this.state.contentLength === TRUNCATED_CONTENT_LENGTH) {
+      this.setState({
+        contentLength: this.props.HTMLBlob.length,
+        buttonText: DISPLAYED_BUTTON_TEXT,
+      });
+    } else {
+      this.setState({
+        contentLength: TRUNCATED_CONTENT_LENGTH,
+        buttonText: TRUNCATED_BUTTON_TEXT,
+      });
+    }
+  };
+
+  prepareContent(bodyContent = '', length) {
+    return truncate(bodyContent, {
+      length,
+      separator: ' ',
+    });
+  }
+
+  render() {
+    const { contentLength, buttonText } = this.state;
+
+    const {
+      showTitle,
+      title,
+      showSubtitle,
+      subtitle,
+      readDuration,
+      authorName,
+      HTMLBlob,
+    } = this.props;
+
+    return (
+      <Fragment>
+        <div className="root">
+          <div className="tile-content-container">
+            {showTitle === true ? <h3>{title}</h3> : null}
+            {showSubtitle === true ? <div className="subtitle">{subtitle}</div> : null}
+
+            <ImageClickHandler>
+              <span
+                className="__html-blob-content-container__"
+                dangerouslySetInnerHTML={{
+                  __html: this.prepareContent(HTMLBlob, contentLength),
+                }}
+              />
+              {HTMLBlob.length > TRUNCATED_CONTENT_LENGTH && (
+                <div>
+                  <button onClick={this.handleReadMoreClick} className="action-read-more">
+                  {buttonText}
+                </button></div>
+              )}
+              </ImageClickHandler>
             </div>
-            <img alt="" src={horizontalArrow} /></li>
-        </ul>
-      </div>
-      */}
-      <ImageClickHandler>
-        <div
-          className="__html-blob-content-container__"
-          dangerouslySetInnerHTML={{ __html: HTMLBlob }}
-        />
-      </ImageClickHandler>
-    </div>
-    <style jsx>{style}</style>
-    <style jsx>{CMSStyle}</style>
-  </div>
-);
+          </div>
+        <style jsx>{style}</style>
+        <style jsx>{bobbietilestyle}</style>
+        <style jsx>{cmsstyle}</style>
+      </Fragment>
+    );
+  }
+}
 
 BobbieTile.propTypes = {
   title: PropTypes.string.isRequired,
@@ -63,6 +96,7 @@ BobbieTile.propTypes = {
 BobbieTile.defaultProps = {
   readDuration: '',
   authorName: '',
+  HTMLBlob: '',
 };
 
 export default BobbieTile;
