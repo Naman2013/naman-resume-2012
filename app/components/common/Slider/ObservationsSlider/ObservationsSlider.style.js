d101b3b72bd40c77ee55285282923bdf34896ce4
silentdust <silentdust@gmail.com>
2019-05-14 22:43:54 -0400

    update to dashboard slider arrows

--- app/components/common/Slider/ObservationsSlider/ObservationsSlider.style.js
+++ app/components/common/Slider/ObservationsSlider/ObservationsSlider.style.js
@@ -1,5 +1,5 @@
 import css from 'styled-jsx/css';
-import { romance, golda } from '../../../../styles/variables/colors_tiles_v4';
+import { romance, golda, lightHeadedAstronaut } from '../../../../styles/variables/colors_tiles_v4';
 
 export default css.global`
   .slick-prev, .slick-next {
@@ -9,10 +9,18 @@ export default css.global`
     transform: translate(0, -50%);
     cursor: pointer;
     color: ${romance};
-    border: solid 2px ${golda};
+    border: solid 2px ${lightHeadedAstronaut};
     border-radius: 50%;
     z-index: 9;
     background-color: transparent;
+  }
+
+  .dash-item-first .outer-well .dashboard-recomended-objects .root .root .slider-container .slick-slider .slick-prev {
+    border: solid 2px ${golda};
+  }
+
+  .dash-item-first .outer-well .dashboard-recomended-objects .root .root .slider-container .slick-slider .slick-next {
+    border: solid 2px ${golda};
   }
 
   .slick-prev:before {
@@ -37,9 +45,9 @@ export default css.global`
     transform: rotate(180deg);
   }
 
-  .dash-item-first .slick-prev, .dash-item-first .slick-next {
+  /* .dash-item-first .slick-prev, .dash-item-first .slick-next {
     border: solid 2px #FAD59A;
-  }
+  } */
   .dash-item-first .slick-prev:before {
     background: url('https://vega.slooh.com/assets/v4/common/slider_arrow_white.svg') 0 0 no-repeat;
   }
