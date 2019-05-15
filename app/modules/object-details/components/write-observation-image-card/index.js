1f68892c5f8173aace56cb4fc91de253161a78b0
Sviatoslav Nazar <sviatoslav.nazar@inoxoft.com>
2019-05-15 23:35:46 +0300

    Write observation default image

--- app/modules/object-details/components/write-observation-image-card/index.js
+++ app/modules/object-details/components/write-observation-image-card/index.js
@@ -9,19 +9,19 @@ export class WriteObservationImageCard extends PureComponent {
 
     return (
       <div className="write-observation-image-card" onClick={onClick}>
+        <div className="image-background">
+          <img className="object-image" src={imageURL} alt="" />
+          <div className="object-icon-container">
+            <img src={objectIconURL} alt="" />
+          </div>
+        </div>
         {defaultCard ? (
           <Fragment>
-            <div className="image-background">
-              <div className="object-icon-container">
-                <img src={objectIconURL} alt="" />
-              </div>
-            </div>
             <h5>{objectTitle}</h5>
             <p>Use default image</p>
           </Fragment>
         ) : (
           <Fragment>
-            <img className="object-image" src={imageURL} alt="" />
             <h5>{imageTitle}</h5>
             <p>{`${displayDate}  ${displayTime}`}</p>
           </Fragment>
