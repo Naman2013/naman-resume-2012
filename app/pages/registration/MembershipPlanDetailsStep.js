136ca0bfa7d618a8ab6af52f96ef65fa394944e5
Vitaliy Makoveichuk <vitaliymakoveichuk@Mac-mini-Andre.local>
2019-05-16 13:53:48 +0300

    make backgrounds load from api

--- app/pages/registration/MembershipPlanDetailsStep.js
+++ app/pages/registration/MembershipPlanDetailsStep.js
@@ -19,13 +19,15 @@ import {
 import { JOIN_PAGE_ENDPOINT_URL } from 'app/services/registration/registration.js';
 import axios from 'axios';
 import Request from 'app/components/common/network/Request';
-import JoinHeader from './partials/JoinHeader';
-import PlanDetailsCard from './partials/PlanDetailsCard';
 import BobbieTile from 'app/components/common/tiles/BobbieTile';
 import TabbedNav from 'app/components/TabbedNav';
+import { DeviceContext } from 'app/providers/DeviceProvider';
+import JoinHeader from './partials/JoinHeader';
+import PlanDetailsCard from './partials/PlanDetailsCard';
 import { PLAN_DETAILS_JOIN_TABS } from './StaticNavTabs';
 import styles from './JoinStep1SchoolSelection.style';
 import messages from './MembershipPlanDetailsStep.messages';
+import { makeIsFetchingSelector } from 'app/modules/account-settings/selectors';
 
 const { string, arrayOf, shape } = PropTypes;
 
@@ -54,10 +56,8 @@ class MembershipPlanDetailsStep extends Component {
 
   state = {
     selectedPlanId: window.localStorage.getItem('selectedPlanId'),
-    isAstronomyClub:
-      window.localStorage.getItem('isAstronomyClub') === 'true' ? true : false,
-    isClassroom:
-      window.localStorage.getItem('isClassroom') === 'true' ? true : false,
+    isAstronomyClub: window.localStorage.getItem('isAstronomyClub') === 'true',
+    isClassroom: window.localStorage.getItem('isClassroom') === 'true',
   };
 
   continueToJoinFlow = formValues => {
@@ -81,77 +81,101 @@ class MembershipPlanDetailsStep extends Component {
     const { pathname, tabs, activeTab, intl } = this.props;
 
     return (
-      <div className="join-root-alt">
-        <div className="step-root">
-          <Request
-            serviceURL={JOIN_PAGE_ENDPOINT_URL}
-            requestBody={{
-              callSource: 'membershipspagePlanDetails',
-              selectedPlanId: this.state.selectedPlanId,
-            }}
-            render={({ fetchingContent, serviceResponse }) => (
-              <Fragment>
-                {!fetchingContent && (
-                  <Fragment>
-                    <div className="join-root-alt-header">
-                      <h1>
-                        <FormattedMessage {...messages.JoinSlooh} />
-                      </h1>
-                      <h2>
-                        <FormattedMessage {...messages.JoinSloohTrial} />
-                      </h2>
-                    </div>
-                    <PlanDetailsCard
-                      {...serviceResponse.selectedSubscriptionPlan}
-                    />
-                    <TabbedNav
-                      tabs={PLAN_DETAILS_JOIN_TABS}
-                      activeTabValue={activeTab}
-                      onTabClick={this.changeActiveTab}
-                    />
-                    <div className="inner-container">
-                      <form
-                        style={{ paddingTop: '0px' }}
-                        className="form"
-                        onSubmit={this.continueToJoinFlow}
-                      >
-                        <BobbieTile
-                          className="form-section"
-                          showTitle={false}
-                          showSubtitle={false}
-                          title=""
-                          subtitle=""
-                          HTMLBlob={
-                            serviceResponse.selectedSubscriptionPlan
-                              .aboutThisPlan
-                          }
-                          disableReadMore={true}
-                        />
-                        <div
-                          style={{ paddingTop: '40px' }}
-                          className="button-container"
-                        >
-                          <Button
-                            type="button"
-                            text={intl.formatMessage(messages.GoBack)}
-                            onClickEvent={() => {
-                              browserHistory.goBack();
-                            }}
+      <DeviceContext.Consumer>
+        {({ isMobile, isTablet, isDesktop }) => (
+          <>
+            <Request
+              serviceURL={JOIN_PAGE_ENDPOINT_URL}
+              requestBody={{
+                callSource: 'membershipspagePlanDetails',
+                selectedPlanId: this.state.selectedPlanId,
+              }}
+              render={({ fetchingContent, serviceResponse }) => (
+                <Fragment>
+                  <div
+                    className="join-root-alt"
+                    style={{
+                      backgroundImage: `url(${
+                        fetchingContent
+                          ? '' :
+                        isMobile
+                          ? serviceResponse.selectedSubscriptionPlan
+                              .planSelectedBackgroundImageUrl_Mobile
+                          : isDesktop
+                          ? serviceResponse.selectedSubscriptionPlan
+                              .planSelectedBackgroundImageUrl_Desktop
+                          : isTablet
+                          ? serviceResponse.selectedSubscriptionPlan
+                              .planSelectedBackgroundImageUrl_Tablet
+                          : ''
+                      })`,
+                    }}
+                  >
+                    <div className="step-root">
+                      {!fetchingContent && (
+                        <Fragment>
+                          <div className="join-root-alt-header">
+                            <h1>
+                              <FormattedMessage {...messages.JoinSlooh} />
+                            </h1>
+                            <h2>
+                              <FormattedMessage {...messages.JoinSloohTrial} />
+                            </h2>
+                          </div>
+                          <PlanDetailsCard
+                            {...serviceResponse.selectedSubscriptionPlan}
                           />
-                          <button className="submit-button" type="submit">
-                            <FormattedMessage {...messages.JoinNow} />
-                          </button>
-                        </div>
-                      </form>
+                          <TabbedNav
+                            tabs={PLAN_DETAILS_JOIN_TABS}
+                            activeTabValue={activeTab}
+                            onTabClick={this.changeActiveTab}
+                          />
+                          <div className="inner-container">
+                            <form
+                              style={{ paddingTop: '0px' }}
+                              className="form"
+                              onSubmit={this.continueToJoinFlow}
+                            >
+                              <BobbieTile
+                                className="form-section"
+                                showTitle={false}
+                                showSubtitle={false}
+                                title=""
+                                subtitle=""
+                                HTMLBlob={
+                                  serviceResponse.selectedSubscriptionPlan
+                                    .aboutThisPlan
+                                }
+                                disableReadMore
+                              />
+                              <div
+                                style={{ paddingTop: '40px' }}
+                                className="button-container"
+                              >
+                                <Button
+                                  type="button"
+                                  text={intl.formatMessage(messages.GoBack)}
+                                  onClickEvent={() => {
+                                    browserHistory.goBack();
+                                  }}
+                                />
+                                <button className="submit-button" type="submit">
+                                  <FormattedMessage {...messages.JoinNow} />
+                                </button>
+                              </div>
+                            </form>
+                          </div>
+                        </Fragment>
+                      )}
                     </div>
-                  </Fragment>
-                )}
-              </Fragment>
-            )}
-          />
-        </div>
-        <style jsx>{styles}</style>
-      </div>
+                  </div>
+                </Fragment>
+              )}
+            />
+            <style jsx>{styles}</style>
+          </>
+        )}
+      </DeviceContext.Consumer>
     );
   }
 }
