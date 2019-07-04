3048c8942a9861d61f355f659eb78bf1ab44ed0f
Sviatoslav Nazar <sviatoslav.nazar@inoxoft.com>
2019-07-04 15:12:54 +0300

    Data collection slot list and slot card

--- app/modules/quests/components/quest-modules/data-collection/data-collection-slot-card/index.js
+++ app/modules/quests/components/quest-modules/data-collection/data-collection-slot-card/index.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import Dots from 'atoms/icons/Dots';
+import { astronaut } from 'app/styles/variables/colors_tiles_v4';
 import './styles.scss';
 
 export const DataCollectionSlotCard = props => {
   const { slot } = props;
-  const { slotSequence, thumbnailURL, slotButtonCaption } = slot;
+  const {
+    slotSequence,
+    thumbnailURL,
+    slotButtonCaption,
+    slotTitle,
+    showSlotTitle,
+    showSlotInfo,
+    showDotMenu,
+  } = slot;
   return (
     <div className="dc-slot-card">
       <div className="dc-slot-card-number">
         {slotSequence > 9 ? slotSequence : `0${slotSequence}`}
       </div>
-      <div className="dc-slot-card-title">Title</div>
+      {showSlotTitle && <div className="dc-slot-card-title">{slotTitle}</div>}
       <div className="dc-slot-card-thumbnail-box">
         <img src={thumbnailURL} />
       </div>
       <div className="dc-slot-card-actions">
-        <Button>{slotButtonCaption}</Button>
+        <Button className="dc-slot-card-find-btn">{slotButtonCaption}</Button>
+        {showSlotInfo && (
+          <Button className="dc-slot-card-info-btn">
+            <img
+              alt=""
+              src="https://vega.slooh.com/assets/v4/common/info_icon.svg"
+            />
+          </Button>
+        )}
+        {showDotMenu && (
+          <Button
+            onClick={() => toggleMenu(!isOpen)}
+            className="dc-slot-card-dots-menu"
+          >
+            <Dots theme={{ circleColor: astronaut }} />
+          </Button>
+        )}
       </div>
     </div>
   );
