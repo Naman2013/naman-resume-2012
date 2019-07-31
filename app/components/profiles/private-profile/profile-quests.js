9140457b700df7bbf52e4e9bf7ca17ceb70ea5c1
Todd Reisel <todd@slooh.com>
2019-07-31 15:00:03 -0400

    misc telescope fixes and font size fixes/quests coming soon wording.

--- app/components/profiles/private-profile/profile-quests.js
+++ app/components/profiles/private-profile/profile-quests.js
@@ -6,8 +6,8 @@ type TProfileQuests = {};
 
 export const ProfileQuests = (props: TProfileQuests) => {
   return (
-    <div className="container mt-5">
-      Quests will challenge you to explore space in fun and interesting ways in order to earn Badges and Gravity Points. There will be many different Quest types and they will be released serially starting in July.
+    <div style={{fontSize: "1.5em"}} className="container mt-5">
+      Quests will challenge you to explore space in fun and interesting ways in order to earn Badges and Gravity Points. There will be many different Quest types and they will be released soon.
     </div>
   );
 };
