0cd3b91e57ce11b36a7e6d811086ea29cd03bad7
Todd Reisel <treisel@coastalbt.com>
2019-08-20 15:14:15 -0400

    updated Profile: Badges: badgeIconURL

--- app/components/profiles/ProfileStats/Badges.js
+++ app/components/profiles/ProfileStats/Badges.js
@@ -25,8 +25,8 @@ class Badges extends Component {
       <div className="badges-list">
         {badgesList.map(item => (
           <div className="badges-list-item-div">
-            <div className="badges-list-item-img-span"><img className="badges-list-item-img" alt="" src={item.badgeThumbnailURL} /></div>
-            <div className="badges-list-item-description">{item.badgeTitle} {item.badgeTitle} {item.badgeTitle}</div>
+            <div className="badges-list-item-img-span"><img className="badges-list-item-img" alt="Badge Awarded: {item.badgeAwardedDate}" title="{Badge Awarded: {item.badgeAwardedDate}}" src={item.badgeIconURL} /></div>
+            <div className="badges-list-item-description">{item.badgeTitle}</div>
           </div>
         ))}
         <style jsx>{styles}</style>
