4ce942ea89f3223a8fae2a195cb2cf8c44986041
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-09-11 16:19:10 +0300

    SV4FED-1177 [Sitewide]: Live Activity Feed UI - footer, input

---
+++ app/modules/live-activity/components/feed-item/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './index.scss';
+import * as cx from 'classnames';
+
+type TFeedItem = {
+  item: any;
+};
+
+export const FeedItem = (props: TFeedItem) => (
+  <div
+    className={cx('feed-item', {
+      'feed-item-current-user': props.item.currentUser,
+    })}
+  >
+    <div className="feed-data">
+      <span className="feed-data-date">{props.item.date}</span>
+      <span className="feed-data-user">{props.item.user}</span>
+    </div>
+    <div className="feed-msg">{props.item.text}</div>
+  </div>
+);
--- app/modules/live-activity/components/live-activity/index.tsx
+++ app/modules/live-activity/components/live-activity/index.tsx
@@ -6,8 +6,44 @@ import './index.scss';
 import { Tooltip } from 'react-tippy';
 // @ts-ignore
 import { ResizableBox } from 'react-resizable';
+import { FeedItem } from '../feed-item/index';
 
 type TLiveActivity = {};
+
+const feed = [
+  {
+    id: 1,
+    user: 'Todd',
+    currentUser: false,
+    date: '09/11/2019 3:25PM',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit Aspernatur consectetur deserunt dignissimos, dolor est expedita id itaque labore, nostrum odit perferendis',
+  },
+  {
+    id: 2,
+    user: 'Ed',
+    currentUser: false,
+    date: '09/12/2019 5:25PM',
+    text:
+      'consectetur adipisicing elit Aspernatur consectetur deserunt dignissimos, dolor est expedita id itaque labore, nostrum odit perferendis',
+  },
+  {
+    id: 3,
+    user: 'Ivan',
+    currentUser: true,
+    date: '09/12/2019 10:25PM',
+    text:
+      'dignissimos, dolor est expedita id itaque labore, nostrum odit perferendis',
+  },
+  {
+    id: 4,
+    user: 'Sviatoslav',
+    currentUser: false,
+    date: '09/13/2019 1:25AM',
+    text:
+      'consectetur adipisicing elit Aspernatur consectetur deserunt dignissimos, dolor est expedita id itaque labore, nostrum odit perferendis',
+  },
+];
 
 export class LiveActivity extends React.Component<TLiveActivity, {}> {
   render() {
@@ -26,7 +62,7 @@ export class LiveActivity extends React.Component<TLiveActivity, {}> {
             <div>
               <ResizableBox
                 width={400}
-                height={400}
+                height={600}
                 minConstraints={[300, 300]}
               >
                 <div className="live-activity-window">
@@ -42,11 +78,9 @@ export class LiveActivity extends React.Component<TLiveActivity, {}> {
                   </div>
                   <div className="live-activity-window-body">
                     <div className="live-activity-window-body-feed">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Aspernatur consectetur deserunt dignissimos, dolor est
-                      expedita id itaque labore, nostrum odit perferendis
-                      placeat quod, voluptatum. Culpa ipsam maxime natus
-                      pariatur. Sed?
+                      {feed.map(feedItem => (
+                        <FeedItem item={feedItem} />
+                      ))}
                     </div>
                   </div>
 
