a360396055396cf9cc0d10ec572ac5f3628b680a
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-09-13 10:51:39 +0300

    TypeScript

--- app/modules/profile/containers/profile-activity.ts
+++ app/modules/profile/containers/profile-activity.ts
@@ -2,9 +2,12 @@ import ProfileActivity from 'app/modules/profile/components/profile-activity';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { getPrivateProfile } from 'app/modules/profile/thunks';
-import { cancelReservation, cancelPiggyback } from 'app/modules/missions/thunks';
+import {
+  cancelReservation,
+  cancelPiggyback,
+} from 'app/modules/missions/thunks';
 
-const mapStateToProps = state => {};
+const mapStateToProps = () => {};
 
 const mapDispatchToProps = {
   getPrivateProfile,
