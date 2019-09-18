8d2f55caa8ce4e534b833de4c8492867e05f7e8b
auroraeos <anastasiia.bortniichuk@inoxoft.com>
2019-09-18 12:43:34 +0300

    SV4FED-1206 [Object Details]: Link to Public Profile isn't worked.

--- app/modules/image-details/components/partials/MainContainer.js
+++ app/modules/image-details/components/partials/MainContainer.js
@@ -32,6 +32,7 @@ const MainContainer = ({
   validateResponseAccess,
   refetchData,
   shareMemberPhotoData,
+  iconFileData,
 }) => {
   const [isEditMode, setEditMode] = useState(!observationLog);
   const [title, setTitle] = useState('');
@@ -49,6 +50,7 @@ const MainContainer = ({
             canLikeFlag={canLikeFlag}
             customerImageId={customerImageId}
             fileData={fileData}
+            iconFileData={iconFileData}
             likesCount={likesCount}
             likePrompt={likePrompt}
             showLikePrompt={showLikePrompt}
--- app/modules/image-details/components/partials/ObservationInformation.js
+++ app/modules/image-details/components/partials/ObservationInformation.js
@@ -7,6 +7,7 @@
 
 import Btn from 'app/atoms/Btn';
 import Icon from 'app/atoms/Icon';
+import { Link } from 'react-router';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -21,7 +22,6 @@ import { primaryFont, secondaryFont } from 'app/styles/variables/fonts';
 import { customModalStyles } from 'app/styles/mixins/utilities';
 import LikeButton from 'app/components/common/style/buttons/LikeButton';
 import { WriteObservationStep3 } from 'app/modules/object-details/components/write-observation-step3';
-
 
 const {
   any,
@@ -90,7 +90,7 @@ class BootstrappedImageDetails extends Component {
   }
 
   closeModal = e => {
-    if(e) {
+    if (e) {
       e.preventDefault();
     }
     const { refetchData } = this.props;
@@ -145,7 +145,7 @@ class BootstrappedImageDetails extends Component {
 
     shareMemberPicture({ customerImageId }).then(() => {
       this.setState({
-        isOpen: true
+        isOpen: true,
       });
     });
   };
@@ -166,6 +166,7 @@ class BootstrappedImageDetails extends Component {
       canShareFlag,
       canEditFlag,
       shareMemberPhotoData,
+      iconFileData,
     } = this.props;
 
     const { isOpen, likePrompt, count, promptText } = this.state;
@@ -177,10 +178,12 @@ class BootstrappedImageDetails extends Component {
             dangerouslySetInnerHTML={{ __html: observationTitle || imageTitle }}
           />
           <div className="obs-name-and-time">
-            <div
-              className="obs-author"
-              dangerouslySetInnerHTML={{ __html: fileData['Photo by'] }}
-            />
+            <Link to={iconFileData?.Member?.linkUrl}>
+              <div
+                className="obs-author"
+                dangerouslySetInnerHTML={{ __html: fileData['Photo by'] }}
+              />
+            </Link>
             <div
               className="obs-time"
               dangerouslySetInnerHTML={{
@@ -196,16 +199,13 @@ class BootstrappedImageDetails extends Component {
             <LikeButton onClickEvent={this.likeObservation} count={count} />
           </div>
 
-          <Modal
-            show={isOpen}
-            onHide={this.closeModal}
-          >
-            <WriteObservationStep3 
+          <Modal show={isOpen} onHide={this.closeModal}>
+            <WriteObservationStep3
               onHide={this.closeModal}
               shareMemberPhotoData={shareMemberPhotoData}
             />
           </Modal>
-          
+
           {canEditFlag && (
             <div className="pull-right my-3 ml-3">
               <Btn mod="circle" onClick={this.onEdit}>
@@ -251,6 +251,10 @@ class BootstrappedImageDetails extends Component {
           .obs-author,
           .obs-time {
             flex: 1 1 0;
+          }
+
+          .obs-author {
+            color: #415671;
           }
 
           .obs-time {
