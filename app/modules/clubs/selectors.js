48a277b34041bdf4afade1320998b6ecc66f3a1b
auroraeos <anastasiia.bortniichuk@inoxoft.com>
2019-09-18 17:48:12 +0300

    add user link to popular discussions

--- app/modules/clubs/selectors.js
+++ app/modules/clubs/selectors.js
@@ -26,6 +26,7 @@ export const makeTopThreadsDataSelector = () =>
           threadId: x.threadId,
           title: x.title,
           totalLikes: x.totalLikes,
+          authorInfo: x.authorInfo,
         };
       })
   );
