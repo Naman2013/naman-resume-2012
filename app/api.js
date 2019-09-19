1d948a7b2c1911f758323beb9b6d96312e192a77
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-09-19 15:38:38 +0300

    iOS fix

--- app/api.js
+++ app/api.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 const commonData = {
   clientDeviceDetails: {
-    platform: window.navigator.platform,
-    language: window.navigator.language,
-    appCodeName: window.navigator.appCodeName,
-    appVersion: window.navigator.appVersion,
-    userAgent: window.navigator.userAgent, // browser, OS
-    orientationType: window.screen.orientation.type,
+    platform: window.navigator?.platform,
+    language: window.navigator?.language,
+    appCodeName: window.navigator?.appCodeName,
+    appVersion: window.navigator?.appVersion,
+    userAgent: window.navigator?.userAgent, // browser, OS
+    orientationType: window.screen?.orientation?.type,
     screenResolution: {
-      width: window.screen.width,
-      height: window.screen.height,
-      availHeight: window.screen.availHeight,
-      availWidth: window.screen.availWidth,
+      width: window.screen?.width,
+      height: window.screen?.height,
+      availHeight: window.screen?.availHeight,
+      availWidth: window.screen?.availWidth,
     },
   },
 };
