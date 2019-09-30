7ef6dd6c15125d81c5ed35f4dd667cedec97f78d
Todd Reisel <treisel@coastalbt.com>
2019-09-30 11:48:33 -0400

    updated activity feed to reverse display.

--- app/modules/live-activity/components/feed-item/index.tsx
+++ app/modules/live-activity/components/feed-item/index.tsx
@@ -14,14 +14,14 @@ export const FeedItem = (props: TFeedItem) => (
   >
     <div className="feed-data">
       {/* <span className="feed-data-date">{props.item.date}</span> */}
-      <span className="feed-data-user">{props.item.user}</span>
+      {/* <span className="feed-data-user">{props.item.user}</span> */}
     </div>
 
     <div className="feed-msg">
       {props.item.currentUser ? (
-        <div className="arrow-right" />
-      ) : (
         <div className="arrow-left" />
+      ) : (
+        <div className="arrow-right" />
       )}
 
       <span dangerouslySetInnerHTML={{ __html: props.item.text }}/>
