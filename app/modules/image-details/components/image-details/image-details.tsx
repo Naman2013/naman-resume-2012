021d4398e4c2d36ff92ec635cdbb9bd393df2a55
Sviatoslav Nazar <sviatoslav.nazar@inoxoft.com>
2019-10-03 18:41:12 +0300

    Refact image details

--- app/modules/image-details/components/image-details/image-details.tsx
+++ app/modules/image-details/components/image-details/image-details.tsx
@@ -105,6 +105,7 @@ export class ImageDetails extends Component<
     } = this.props;
     const { isOpenModal } = this.state;
     console.log('RENDER');
+    console.log('fffffff');
 
     const showMissionRelatedInfo = Number(scheduledMissionId) > 0;
     const rightPanelDisplayFlags = [showMissionRelatedInfo];
