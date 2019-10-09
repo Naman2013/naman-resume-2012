72283d774bf32352a19454c9c31b10e365df6b67
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-10-09 17:28:19 +0300

    fixed suspense

--- app/i18n.ts
+++ app/i18n.ts
@@ -7,12 +7,15 @@ const fallbackLng = ['en'];
 const availableLanguages = ['en', 'cn'];
 
 i18n
-  .use(Backend) // load translation using xhr -> see /public/locales. We will add locales in the next step
+  .use(Backend) // load translation using xhr -> see /public/locales.
   .use(initReactI18next) // pass the i18n instance to react-i18next.
 
   .init({
-    fallbackLng, // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
-    debug: false,
+    react: {
+      useSuspense: false,
+    },
+    fallbackLng,
+    debug: true,
     whitelist: availableLanguages,
     interpolation: {
       escapeValue: false,
