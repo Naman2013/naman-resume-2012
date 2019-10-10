17480b33c8c8fdf2f2cd4898bbec41eb4a0b51a2
auroraeos <anastasiia.bortniichuk@inoxoft.com>
2019-10-10 18:26:01 +0300

    Fixed SV4FED-1268 [About Slooh]: The navigation panel works incorrectly.

--- app/components/common/sub-page-navigation/SubPageNavigation.js
+++ app/components/common/sub-page-navigation/SubPageNavigation.js
@@ -4,20 +4,22 @@
  *  Currently used by V4 Object Details and V4 Guide Details
  ***********************************/
 
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, createRef } from 'react';
 import { Link } from 'react-router';
 import PropTypes from 'prop-types';
 import style from './SubPageNavigation.style';
 
 const SubPageNavigation = ({ items, locationPath }) => {
+  const ref = createRef();
+
   useEffect(() => {
-    const el = document.querySelector('#navigationId .subnav-active');
+    const el = ref.current.querySelector('.subnav-active');
     el.scrollIntoView({ inline: 'start' });
   }, [locationPath]);
 
   return (
     <Fragment>
-      <ul className="subnav" id="navigationId">
+      <ul className="subnav" id="navigationId" ref={ref}>
         {items.map(item => (
           <li className="item" key={item.link}>
             <Link
