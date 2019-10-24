abb01486b46eb90ea5b32e9fc2b3f93c96aca035
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-10-24 15:39:11 +0300

    js9

--- app/containers/about/About.js
+++ app/containers/about/About.js
@@ -19,11 +19,35 @@ class About extends Component {
     this.props.fetchAboutDataAction();
   }
 
+  loadJS9 = ind => {
+    const urls = [
+      'https://polaris.slooh.com/chile/1/highmag/2018/01/30/0435_m65/m65_20180130_043523_0_bhvtcx_l_cal.fit',
+      'https://polaris.slooh.com/chile/1/highmag/2018/01/30/0415_ngc3372/ngc3372_20180130_041436_0_hhr1y4_l_cal.fit',
+      'https://polaris.slooh.com/chile/1/highmag/2018/01/30/0355_ngc1365/ngc1365_20180130_035434_0_rsstlu_l_cal.fit',
+    ];
+    window.JS9.Load(urls[ind]);
+  };
+
   render() {
     const { children, aboutData } = this.props;
 
     return (
       <div>
+        <button onClick={() => this.loadJS9(0)}>Load FITS file 1</button>
+        <button onClick={() => this.loadJS9(1)}>Load FITS file 2</button>
+        <button onClick={() => this.loadJS9(2)}>Load FITS file 3</button>
+
+        <hr />
+
+        <div className="JS9Menubar"></div>
+        <div className="JS9Toolbar"></div>
+        <div className="JS9"></div>
+        <div>
+          <div className="JS9Colorbar"></div>
+        </div>
+
+        <hr />
+
         {aboutData.aboutSloohSectionsList && (
           <Fragment>
             <PageHeader
@@ -31,7 +55,9 @@ class About extends Component {
               title={aboutData.aboutSloohPageHeading1}
             />
 
-            <Navigation aboutSloohSectionsList={aboutData.aboutSloohSectionsList} />
+            <Navigation
+              aboutSloohSectionsList={aboutData.aboutSloohSectionsList}
+            />
 
             {cloneElement(children)}
           </Fragment>
@@ -50,5 +76,5 @@ const mapDispatchToProps = { fetchAboutDataAction };
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(About);
