7edf61c497bd637e773000ba5ed4d9a12e57ea88
Sviatoslav Nazar <sviatoslav.nazar@inoxoft.com>
2019-11-04 18:58:52 +0200

    Animation module

--- app/modules/quests/components/quest-modules/animation/index.tsx
+++ app/modules/quests/components/quest-modules/animation/index.tsx
@@ -1,6 +1,7 @@
 import React, { RefObject } from 'react';
 import { fabric } from 'fabric';
 import { Button } from 'react-bootstrap';
+import cx from 'classnames';
 import {
   IQuestStepModule,
   IQuestAnimation,
@@ -408,100 +409,88 @@ export class AnimationModule extends React.PureComponent<
     } = questAnimation;
     const { frameList } = questAnimationFrames;
 
+    const moveBtnEvents = {
+      onTouchStart: this.moveDownPress,
+      onTouchEnd: this.moveDownRelease,
+      onMouseDown: this.moveDownPress,
+      onMouseUp: this.moveDownRelease,
+    };
+
     return (
       <div className="animation-module">
-        {activeAnimationStep === ANIMATION_STEPS.EDIT && (
-          <>
-            <div className="animation-box">
-              <h6>{caption}</h6>
-              <h4>{`${objectName} ${imageDate} ${imageTime}`}</h4>
-              <div className="vertical-line" />
-              <div
-                id="animationCanvasContainer"
-                ref={node => {
-                  this.canvasContainer = node;
-                }}
-              >
-                <canvas id="c" />
-              </div>
-
-              <div className="animation-controls">
-                <div className="controls-block">
-                  <div className="buttons-block">
-                    <Button
-                      className="move-btn move-btn-left"
-                      onTouchStart={this.moveLeftPress}
-                      onTouchEnd={this.moveLeftRelease}
-                      onMouseDown={this.moveLeftPress}
-                      onMouseUp={this.moveLeftRelease}
-                    >
-                      <div className="icon icon-slider-left" />
-                    </Button>
-                    <Button
-                      className="move-btn move-btn-right"
-                      onTouchStart={this.moveRigthPress}
-                      onTouchEnd={this.moveRigthRelease}
-                      onMouseDown={this.moveRigthPress}
-                      onMouseUp={this.moveRigthRelease}
-                    >
-                      <div className="icon icon-slider-right" />
-                    </Button>
-                  </div>
-                  <p>X: {xOffset}</p>
-                </div>
-
-                <div className="controls-block">
-                  <div className="buttons-block">
-                    <Button
-                      className="move-btn move-btn-up"
-                      onTouchStart={this.moveTopPress}
-                      onTouchEnd={this.moveTopRelease}
-                      onMouseDown={this.moveTopPress}
-                      onMouseUp={this.moveTopRelease}
-                    >
-                      <div className="icon icon-slider-left" />
-                    </Button>
-                    <Button
-                      className="move-btn move-btn-down"
-                      onTouchStart={this.moveDownPress}
-                      onTouchEnd={this.moveDownRelease}
-                      onMouseDown={this.moveDownPress}
-                      onMouseUp={this.moveDownRelease}
-                    >
-                      <div className="icon icon-slider-right" />
-                    </Button>
-                  </div>
-                  <p>Y: {yOffset}</p>
-                </div>
-
-                <div className="controls-block">
-                  <div className="buttons-block">
-                    <Button className="zoom-btn" onClick={this.zoomInCanvas}>
-                      <div className="icon icon-plus" />
-                    </Button>
-                    <Button className="zoom-btn" onClick={this.zoomOutCanvas}>
-                      <div className="icon icon-minus" />
-                    </Button>
-                  </div>
-                  <p>
-                    {zoom || magnificationDefault}
-                    {magnificationUnitsCaption}
-                  </p>
-                </div>
-
-                <div className="controls-block">
-                  <Button onClick={() => {}}>{playButtonCaption}</Button>
-                </div>
-              </div>
+        <div
+          className={cx('animation-edit', {
+            visible: activeAnimationStep === ANIMATION_STEPS.EDIT,
+          })}
+        >
+          <div className="animation-box">
+            <h6>{caption}</h6>
+            <h4>{`${objectName} ${imageDate} ${imageTime}`}</h4>
+            <div className="vertical-line" />
+            <div
+              id="animationCanvasContainer"
+              ref={node => {
+                this.canvasContainer = node;
+              }}
+            >
+              <canvas id="c" />
             </div>
 
-            <FrameList
-              frameList={frameList}
-              activeFrame={activeFrame}
-              setActiveFrame={this.setActiveFrame}
-            />
-          </>
-        )}
+            <div className="animation-controls">
+              <div className="controls-block">
+                <div className="buttons-block">
+                  <Button className="move-btn move-btn-left" {...moveBtnEvents}>
+                    <div className="icon icon-slider-left" />
+                  </Button>
+                  <Button
+                    className="move-btn move-btn-right"
+                    {...moveBtnEvents}
+                  >
+                    <div className="icon icon-slider-right" />
+                  </Button>
+                </div>
+                <p>X: {xOffset}</p>
+              </div>
+
+              <div className="controls-block">
+                <div className="buttons-block">
+                  <Button className="move-btn move-btn-up" {...moveBtnEvents}>
+                    <div className="icon icon-slider-left" />
+                  </Button>
+                  <Button className="move-btn move-btn-down" {...moveBtnEvents}>
+                    <div className="icon icon-slider-right" />
+                  </Button>
+                </div>
+                <p>Y: {yOffset}</p>
+              </div>
+
+              <div className="controls-block">
+                <div className="buttons-block">
+                  <Button className="zoom-btn" onClick={this.zoomInCanvas}>
+                    <div className="icon icon-plus" />
+                  </Button>
+                  <Button className="zoom-btn" onClick={this.zoomOutCanvas}>
+                    <div className="icon icon-minus" />
+                  </Button>
+                </div>
+                <p>
+                  {zoom || magnificationDefault}
+                  {magnificationUnitsCaption}
+                </p>
+              </div>
+
+              <div className="controls-block">
+                <Button onClick={() => {}}>{playButtonCaption}</Button>
+              </div>
+            </div>
+          </div>
+
+          <FrameList
+            frameList={frameList}
+            activeFrame={activeFrame}
+            setActiveFrame={this.setActiveFrame}
+          />
+        </div>
       </div>
     );
   }
