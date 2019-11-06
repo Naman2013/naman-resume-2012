92219be5bf3faa8790ba435506c749c16de338ad
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-11-06 16:03:34 +0200

    fixed spacing

--- app/modules/leaderboard/components/leaderboard-page/index.tsx
+++ app/modules/leaderboard/components/leaderboard-page/index.tsx
@@ -4,8 +4,8 @@ import Nav from 'app/components/common/nav';
 import HubHeader from 'app/components/common/HubHeader';
 import {LeaderboardResponse} from 'app/modules/leaderboard/types';
 import {IRouter} from 'app/common/types';
-import {Col, Container, Row} from 'react-bootstrap';
 import {LeaderboardTable} from 'app/modules/leaderboard/components/leaderboard-table';
+import './index.scss';
 
 interface RouteParams {
   tab: string;
@@ -59,24 +59,22 @@ export const LeaderboardPage: React.FC<LeaderboardPageProps> = React.memo(
             </div>
           )}
         />
-        <Container>
-          <Row className="mt-5">
-            <Col md={6}>
-              <LeaderboardTable
-                header={leaderboardData.PageHeadingAllTime}
-                tableData={tableByTab[routeParams.tab]?.left}
-                highlightCID={userData.customerUUID}
-              />
-            </Col>
-            <Col md={6}>
-              <LeaderboardTable
-                header={leaderboardData.PageHeadingThirtyDay}
-                tableData={tableByTab[routeParams.tab]?.right}
-                highlightCID={userData.customerUUID}
-              />
-            </Col>
-          </Row>
-        </Container>
+        <div className="leaderboard-table-wrapper mt-5">
+          <div>
+            <LeaderboardTable
+              header={leaderboardData.PageHeadingAllTime}
+              tableData={tableByTab[routeParams.tab]?.left}
+              highlightCID={userData.customerUUID}
+            />
+          </div>
+          <div>
+            <LeaderboardTable
+              header={leaderboardData.PageHeadingThirtyDay}
+              tableData={tableByTab[routeParams.tab]?.right}
+              highlightCID={userData.customerUUID}
+            />
+          </div>
+        </div>
       </>
     );
   }
