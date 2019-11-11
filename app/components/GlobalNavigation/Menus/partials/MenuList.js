a6031bcd2a8646ffff2741d35dc99d82709e7317
auroraeos <anastasiia.bortniichuk@inoxoft.com>
2019-11-11 14:24:50 +0200

    SV4FED-1352 [Alerts], Tablet: User is able to move the content of the 'Alerts' menu

--- app/components/GlobalNavigation/Menus/partials/MenuList.js
+++ app/components/GlobalNavigation/Menus/partials/MenuList.js
@@ -4,35 +4,35 @@ import PropTypes from 'prop-types';
 const propTypes = {
   items: PropTypes.shape({
     render: PropTypes.func.isRequired,
-    content: PropTypes.arrayOf(PropTypes.shape({
-      _ID: PropTypes.string.isRequired,
-    })),
+    content: PropTypes.arrayOf(
+      PropTypes.shape({
+        _ID: PropTypes.string.isRequired,
+      })
+    ),
   }).isRequired,
 };
 
 const MenuList = ({ items }) => (
   <div className="root">
     <ul className="menu-list">
-      {
-        items.content.map(item => (
-          <li key={item._ID}>
-            { items.render(item) }
-          </li>
-        ))
-      }
+      {items.content.map(item => (
+        <li key={item._ID}>{items.render(item)}</li>
+      ))}
     </ul>
 
-    <style jsx>{`
-      .menu-list {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-      }
+    <style jsx>
+      {`
+        .menu-list {
+          overflow-x: hidden;
+          list-style-type: none;
+          padding: 0;
+          margin: 0;
+        }
 
-      .menu-list > li:first-child {
-        margin-top: 20px;
-      }
-    `}
+        .menu-list > li:first-child {
+          margin-top: 20px;
+        }
+      `}
     </style>
   </div>
 );
