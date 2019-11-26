d5fcd8661b582147a4e6b5702e72aa9327f82a86
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-11-26 13:44:57 +0200

    SV4FED-1404 About Slooh: Memberships Page For Indivudual/Group Purchase Tabs

---
+++ app/pages/registration/MembershipPlansList.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import styles from 'app/pages/registration/MembershipPlansList.style';
+import UpgradeModal from 'app/modules/account-settings/containers/upgrade-modal';
+import SubscriptionPlanCardSmall from './partials/SubscriptionPlanCardSmall';
+
+export default class MembershipPlansList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedPlan: null,
+    };
+    window.localStorage.removeItem('selectedPlanId');
+  }
+
+  setSelectedPlan(
+    subscriptionPlanId,
+    isAstronomyClubFlag,
+    isClassroomFlag,
+    triggerUpgradeFlow,
+    selectedPlan
+  ) {
+    window.localStorage.setItem('selectedPlanId', subscriptionPlanId);
+    window.localStorage.setItem('isAstronomyClub', isAstronomyClubFlag);
+    window.localStorage.setItem('isClassroom', isClassroomFlag);
+
+    const isAstronomyClub =
+      window.localStorage.getItem('isAstronomyClub') === 'true';
+    const isClassroom = window.localStorage.getItem('isClassroom') === 'true';
+
+    if (triggerUpgradeFlow) {
+      this.setUpgradeModalOpen(selectedPlan);
+    } else if (isClassroom) {
+      /* Teacher Subscription Plans should prompt for School Selection */
+      browserHistory.push('/join/step1SchoolSelection');
+    } else {
+      /* move to step 2 in the join flow */
+      browserHistory.push('/join/step2');
+    }
+  }
+
+  setUpgradeModalOpen = selectedPlan => this.setState({ selectedPlan });
+
+  viewPlanDetails(subscriptionPlanId, isAstronomyClub, isClassroom) {
+    window.localStorage.setItem('selectedPlanId', subscriptionPlanId);
+    window.localStorage.setItem('isAstronomyClub', isAstronomyClub);
+    window.localStorage.setItem('isClassroom', isClassroom);
+
+    /* move to Plan Details in the Join Flow */
+    browserHistory.push('/join/membershipPlanDetailsStep');
+  }
+
+  render() {
+    const { selectedPlan } = this.state;
+    const { plans } = this.props;
+
+    let subscriptionPlansCallSource = 'join';
+    if (selectedPlan && selectedPlan.triggerUpgradeFlow === true) {
+      subscriptionPlansCallSource = 'upgrade';
+    }
+
+    return (
+      <>
+        {selectedPlan && (
+          <UpgradeModal
+            show={selectedPlan}
+            preSelectedPlan={selectedPlan}
+            subscriptionPlansCallSource={subscriptionPlansCallSource}
+            onHide={() => this.setUpgradeModalOpen(false)}
+          />
+        )}
+
+        <ul className="subscription-plans-list">
+          {plans.map(subscriptionPlan => (
+            <li
+              key={`subscriptionplan-tile-${subscriptionPlan.planID}`}
+              className="subscription-plans-list-item"
+            >
+              <SubscriptionPlanCardSmall
+                {...subscriptionPlan}
+                viewPlanDetails={() =>
+                  this.viewPlanDetails(
+                    subscriptionPlan.planID,
+                    subscriptionPlan.isAstronomyClub,
+                    subscriptionPlan.isClassroom
+                  )
+                }
+                setSelectedPlan={() =>
+                  this.setSelectedPlan(
+                    subscriptionPlan.planID,
+                    subscriptionPlan.isAstronomyClub,
+                    subscriptionPlan.isClassroom,
+                    subscriptionPlan.triggerUpgradeFlow,
+                    subscriptionPlan
+                  )
+                }
+              />
+            </li>
+          ))}
+        </ul>
+
+        <style jsx>{styles}</style>
+      </>
+    );
+  }
+}
