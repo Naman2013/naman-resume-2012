afeca77be140f4eba94905fb6823c735b81aceb6
auroraeos <anastasiia.bortniichuk@inoxoft.com>
2019-12-01 12:22:51 +0200

    Rich Text Editor cursor issue

--- app/components/rich-text-editor/RichTextEditor.js
+++ app/components/rich-text-editor/RichTextEditor.js
@@ -87,7 +87,7 @@ class RichTextEditor extends React.Component {
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    const { editorValue, value } = nextProps;
+    const { editorValue, value, hasInitialValue } = nextProps;
     const { editorState } = prevState;
 
     if (value) {
@@ -96,11 +96,13 @@ class RichTextEditor extends React.Component {
       };
     }
 
-    let editorStateNew = editorState;
-    if (editorValue && !editorState.getCurrentContent().hasText()) {
+    let editorStateNew;
+    if (hasInitialValue && !editorState.getCurrentContent().hasText()) {
       editorStateNew = getEditorStateFromHtml(editorValue);
-    } else if (!editorValue) {
+    } else if (!editorValue && editorState.getCurrentContent().hasText()) {
       editorStateNew = EditorState.createEmpty(editorStateDecorator);
+    } else {
+      editorStateNew = editorState;
     }
 
     return {
