e8041bc9d7c8c3286439429d060bb5a6450e98dd
Todd Reisel <treisel@coastalbt.com>
2020-01-20 15:03:16 -0500

    updated the app for Google Optimize in nova

--- webpack.config.js
+++ webpack.config.js
@@ -113,7 +113,7 @@ module.exports = {
       // both options are optional
       filename: isProduction ? '[name].[hash].css' : '[name].css',
       chunkFilename: isProduction ? '[id].[hash].css' : '[id].css',
-      disable: !isProduction,
+      disable: true, //!isProduction,
     }),
     new CopyWebpackPlugin([
       { from: publicPath, to: outPath },
