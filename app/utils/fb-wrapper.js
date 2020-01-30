3f99e7e36948d837d3afd6e2a141cf53f9090436
Todd Reisel <treisel@coastalbt.com>
2020-01-30 11:06:23 -0500

    added Facebook Pageview and Custom event tracking for purchase confirmation page.

--- app/utils/fb-wrapper.js
+++ app/utils/fb-wrapper.js
@@ -5,11 +5,7 @@
 export default function fireSloohFBEvent({ location }) {
   if (typeof fbq === 'undefined') { return; }
 
-  //Track all Pageviews in Facebook
-  //fbq('track', 'PageView'),
-
   /* 1/30/2020 - Track Facebook - Complete Registration Events */
-  console.log(location);
   if (location == "/join/purchaseConfirmation/join") {
 	  fbq('track', 'CompleteRegistration');
   }
