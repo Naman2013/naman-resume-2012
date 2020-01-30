5f3be7ee196466815a6605297a4e0235d6398694
Todd Reisel <treisel@coastalbt.com>
2020-01-30 11:09:05 -0500

    debugging facebook confirmation page tracking.

--- app/utils/fb-wrapper.js
+++ app/utils/fb-wrapper.js
@@ -5,6 +5,9 @@
 export default function fireSloohFBEvent({ location }) {
   if (typeof fbq === 'undefined') { return; }
 
+  //Track all Pageviews in Facebook
+  //fbq('track', 'PageView'),
+
   /* 1/30/2020 - Track Facebook - Complete Registration Events */
   if (location == "/join/purchaseConfirmation/join") {
 	  fbq('track', 'CompleteRegistration');
