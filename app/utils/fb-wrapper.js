1927ffb8d01b5f766c91669fd74747aa1c5e5e51
Todd Reisel <todd@slooh.com>
2020-03-25 15:30:09 -0400

    facebook pixel track logging.

--- app/utils/fb-wrapper.js
+++ app/utils/fb-wrapper.js
@@ -3,13 +3,16 @@
 */
 
 export default function fireSloohFBEvent({ pagePath }) {
-  if (typeof fbq === 'undefined') { return; }
+  if (typeof fbq === 'undefined') {
+	console.log('Facebook pixel fbq function not found.....');
+ 	return; 
+  }
 
   //Track all Pageviews in Facebook
   //fbq('track', 'PageView'),
 
-  /* 1/30/2020 - Track Facebook - Complete Registration Events */
-  if (pagePath == "/join/purchaseConfirmation/join") {
-	  fbq('track', 'CompleteRegistration');
-  }
+    /* 1/30/2020 - Track Facebook - Complete Registration Events */
+    if (pagePath == "/join/purchaseConfirmation/join") {
+  	  fbq('track', 'CompleteRegistration');
+    }
 }
