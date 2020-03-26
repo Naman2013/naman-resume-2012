600a3593ef91a31333fc1b2458bb397c76412afd
Todd Reisel <todd@slooh.com>
2020-03-26 12:51:48 -0400

    commiting FB purchase event test using Astronomer for now.

--- app/utils/fb-wrapper.js
+++ app/utils/fb-wrapper.js
@@ -4,15 +4,28 @@
 
 export default function fireSloohFBEvent({ pagePath }) {
   if (typeof fbq === 'undefined') {
-	console.log('Facebook pixel fbq function not found.....');
+	//console.log('Facebook pixel fbq function not found.....');
+	//ideally send a Sentry error...
  	return; 
   }
 
-  //Track all Pageviews in Facebook
-  //fbq('track', 'PageView'),
-
     /* 1/30/2020 - Track Facebook - Complete Registration Events */
+
+    /* 3/26/2020 - Track Facebook - Changed from Complete Registration Events to Purchase Events */
+
     if (pagePath == "/join/purchaseConfirmation/join") {
-  	  fbq('track', 'CompleteRegistration');
+	fbq('track', 'Purchase',
+  		{
+    			value: 300.00,
+    			currency: 'USD',
+    			contents: [
+      				{
+        				id: 'Astronomer',
+        				quantity: 1
+      				}
+			],
+    			content_type: 'product'
+  		}
+	);
     }
 }
