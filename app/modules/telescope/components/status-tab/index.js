efd0b9bdc36a53c8af287795e22d8939a787d529
Senthil Manohar <senthilmnhr87@gmail.com>
2020-04-20 20:26:12 +0530

    status message alignment fix

--- app/modules/telescope/components/status-tab/index.js
+++ app/modules/telescope/components/status-tab/index.js
@@ -37,9 +37,9 @@ statusMessage: ""
   return (
     <div className="animated fadeIn faster status-tab">
       
-      <div className="telescope-views">
-	{activeTelescopeStatus.showStatusMessage === true && <div style={{marginLeft: "100px", fontSize: "1.3em", paddingBottom: "30px"}}>{activeTelescopeStatus.statusMessage}</div>}
+      <div className="telescope-views">	
         <Container>
+        {activeTelescopeStatus.showStatusMessage === true && <div style={{marginLeft: "10px", fontSize: "1.3em", paddingBottom: "30px"}}>{activeTelescopeStatus.statusMessage}</div>}
           <Row>
             <Col lg={4} md={12} sm={12}>
               <ConnectedAllSkyCamera
