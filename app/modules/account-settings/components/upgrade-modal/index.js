420c3f2080e83aa39e65a35190b7c6254987833d
Senthil Manohar <senthilmnhr87@gmail.com>
2020-04-28 15:38:54 +0530

    upgrade user call based upon hasPaymentIngoOnFile

--- app/modules/account-settings/components/upgrade-modal/index.js
+++ app/modules/account-settings/components/upgrade-modal/index.js
@@ -14,6 +14,10 @@ import '../../styles.scss';
 
 import React, { useEffect, useState } from 'react';
 
+import { getUserInfo, deleteSessionToken, deleteMarketingTrackingId } from 'app/modules/User';
+import { UPGRADE_CUSTOMER_ENDPOINT_URL } from 'app/services/registration/registration.js';
+import { API } from 'app/api';
+
 type TUpgradeModal = {
   show: boolean,
   onHide: Function,
@@ -46,6 +50,69 @@ const didMount = (props: TUpgradeModal) => () => {
   window.localStorage.removeItem('isAstronomyClub');
 };
 
+const upgradeUser=(plan, upsellCallSource, subscriptionPlansCallSource)=>{
+  debugger; 
+  const { _sloohatid } = getUserInfo();
+  const upgradeCustomerData = {
+    cid: getUserInfo().cid,
+    at: getUserInfo().at,
+    token: getUserInfo().token,
+    customerId: getUserInfo().cid,
+    selectedPlanId: plan.planID,
+    conditionType: subscriptionPlansCallSource,
+    upsellCallSource: upsellCallSource, 
+    // paymentMethod,
+    processUsingExistingPaymentInfo: true,
+    // paymentToken: paymentNonceTokenData,
+    // billingAddressString: paymentDataString[3],
+    // isAstronomyClub: window.localStorage.getItem('isAstronomyClub') === 'true',
+    sloohMarketingTrackingId: _sloohatid,
+  };
+  //add string aboc to this //ADD THIS BACK AFTER TESTING
+  API.post(UPGRADE_CUSTOMER_ENDPOINT_URL, upgradeCustomerData)
+    .then(response => {
+      const res = response.data;
+      if (!res.apiError) {
+        if (res.status === 'success') {
+  //fire off the Purchase Facebook Event
+  fireSloohFBPurchaseEvent( {
+    cid: getUserInfo().cid, 
+    planName: res.PlanName,
+    planCostInUSD: res.PlanCostInUSD,
+  });
+
+  //clean up any session or marketing tracking id
+  deleteSessionToken();
+  deleteMarketingTrackingId();
+
+          //Cleanup local localStorage
+          window.localStorage.removeItem('pending_cid');
+          window.localStorage.removeItem('selectedPlanId');
+          window.localStorage.removeItem('isAstronomyClub');
+
+          /* cleanup local storage */
+          window.localStorage.removeItem('accountCreationType');
+          window.localStorage.removeItem('username');
+          window.localStorage.removeItem('password');
+
+          //upgradeCustomer needs to return new "AT"
+          //reset the AT cookie so all sub-sequent APIs use the new Account Type in their Request Params
+          props.storeUserNewAT(res.newAccountTypeNbr).then(() => {
+            props.closeModal(true);
+
+           let confirmationPageURL = '/join/purchaseConfirmation/' + res.conditionType;
+           browserHistory.push( confirmationPageURL );
+
+           //browserHistory.push('/');
+          });
+        }
+      }
+    })
+    .catch(err => {
+      throw ('Error: ', err);
+    });
+}
+
 export const UpgradeModal = (props: TUpgradeModal) => {
   const [step, setStep, dispatch] = useState<TSteps>('SELECT_PLAN');
     useEffect(didMount(props), [props.subscriptionPlansCallSource]);
@@ -60,7 +127,8 @@ export const UpgradeModal = (props: TUpgradeModal) => {
     disableGoBack,
     preSelectedPlan,
     storeUserNewAT,
-    upsellCallSource,    
+    upsellCallSource, 
+       
   } = props;
 
   const [selectedPlan, setSelectedPlan] = useState(null);
@@ -88,7 +156,7 @@ export const UpgradeModal = (props: TUpgradeModal) => {
       window.location.reload();
     };
   }
-  
+  debugger;
   return (
     <>
       <Modal
@@ -107,13 +175,17 @@ export const UpgradeModal = (props: TUpgradeModal) => {
                 subscriptionPlansCallSource,
                 subscriptionPlansData,
                 selectedPlan,
-                isFetching,
+                isFetching,                
               }}
               goNext={(subscriptionPlansCallSource, selectedPlan) => {
                 if (subscriptionPlansCallSource == 'downgrade') {
                   setStep('DOWNGRADE');
                 } else {
-                  setStep('PAYMENT');
+                  debugger;
+                  if(subscriptionPlansData.hasPaymentInfoOnFile, subscriptionPlansCallSource)
+                    upgradeUser(selectedPlan,upsellCallSource);
+                  else
+                    setStep('PAYMENT');
                 }
               }}
               setSelectedPlan={setSelectedPlan}
