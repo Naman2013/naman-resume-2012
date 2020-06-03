30d85ebe9d1c03349a5840630a2d8301c293e6ca
Senthil Manohar <senthilmnhr87@gmail.com>
2020-06-04 02:30:52 +0530

    cors fix byURL for animation module

--- app/modules/quests/components/quest-modules/animation/index.tsx
+++ app/modules/quests/components/quest-modules/animation/index.tsx
@@ -263,31 +263,29 @@ export class AnimationModule extends React.PureComponent<
       visible: frameIndex === 1 || selected,
     };
     
-    fabric.util.loadImage(imageURL, (img: any): void => {
+    fabric.Image.fromURL(imageURL, (img: any): void => {
       //load image to fabric
-      const fabricImage = new fabric.Image(img, imgAttrs);
-
-      const offsetCoeff = newCanvasContainerWidth / fabricImage.get('width');
-      fabricImage.set({
+      // const fabricImage = new fabric.Image(img, imgAttrs);
+      img.set(imgAttrs);
+      const offsetCoeff = newCanvasContainerWidth / img.get('width');
+      img.set({
         left: empty ? 0 : xOffset * offsetCoeff,
         top: empty ? 0 : -yOffset * offsetCoeff,
       });
 
       //scale to canvas width
-      fabricImage.scaleToWidth(this.canvas.getWidth());
+      img.scaleToWidth(this.canvas.getWidth());
       //then add it to canvas
-      this.canvas.add(fabricImage);
+      this.canvas.add(img);
       this.canvas.renderAll();
 
       //if doesn't end of frame list -> load next image
       if (frameIndexToLoad + 1 < frameList.length) {
         this.loadImageFromUrl(frameIndexToLoad + 1, frameList);
       }
-    },null,
-    {
+    },null,{
       crossOrigin: 'anonymous'
-    }       
-    );
+    });
   };
 
   moveTop = (stepSize: number): IAnimationFrame => {
