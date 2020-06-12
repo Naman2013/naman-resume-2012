978956c4c8994cc8424a04a755dd154aa8154267
Senthil Manohar <senthilmnhr87@gmail.com>
2020-06-13 01:50:46 +0530

    hambugger bug fix

--- app/components/GlobalNavigation/Menu.js
+++ app/components/GlobalNavigation/Menu.js
@@ -68,7 +68,7 @@ const Menu = props => {
             transition-property: left, right;
             transition-duration: 0.15s;
             transition-timing-function: ease-in-out;
-            display: flex;
+            display: grid;
           }
 
           .menu-list {
