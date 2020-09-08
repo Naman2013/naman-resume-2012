58e2d8bc82631f1400ec234b540d9a579b0f5663
Senthil Manohar <senthilmnhr87@gmail.com>
2020-09-09 03:11:17 +0530

    rollbacked the hardcoded quest id

--- app/modules/new-dashboard/components/quest-map/index.js
+++ app/modules/new-dashboard/components/quest-map/index.js
@@ -98,8 +98,8 @@ export class QuestMap extends Component{
                 token, 
                 at, 
                 cid,
-                // questId: feature.getId(),
-                questId: 194,
+                questId: feature.getId(),
+                // questId: 194,
                 questUUID: 'ae699819-c1df-11ea-a953-062dce25bfa1',
                 questVersion: 1.1
               }).then(response=>{
