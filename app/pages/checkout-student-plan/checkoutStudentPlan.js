190cba467e60ef804aaefc74cf04be0fc657a155
senthilmnhr87@gmail.com <senthilmnhr87@gmail.com>
2020-12-15 02:29:00 +0530

    moved subscription api call top level

--- app/pages/checkout-student-plan/checkoutStudentPlan.js
+++ app/pages/checkout-student-plan/checkoutStudentPlan.js
@@ -4,6 +4,9 @@ import CheckoutPlan from './common/checkoutPlan';
 import Accordion from './accordion/accordion';
 import { Container, Row, Col, CardSubtitle, CardText, Button } from 'react-bootstrap';
 import TitleHeader from './component/title-header';
+import { Spinner } from 'app/components/spinner/index';
+import { SUBSCRIPTION_PLANS_ENDPOINT_URL, JOIN_PAGE_ENDPOINT_URL } from 'app/services/registration/registration.js';
+import Request from 'app/components/common/network/Request';
 
 class checkoutStudentPlan extends Component {
     static defaultProps = {};
@@ -14,24 +17,41 @@ class checkoutStudentPlan extends Component {
 
         return (
             <>
-                <div className="check-out-stu">
-                    <Container>
-                        <TitleHeader
-                            heading="Join the Slooh Universe"
-                            subHeading="Complete your order"
-                        />
-                        <Row>
-                            <Col md="4">
-                                <CheckoutPlan />
-                            </Col>
-                            <Col md="8">
-                                <Accordion />
-                            </Col>
+                <Request
+                    serviceURL={SUBSCRIPTION_PLANS_ENDPOINT_URL}
+                    requestBody={{
+                        callSource: 'joinByGuestLanding',
+                        enableHiddenPlanHashCode: window.localStorage.getItem(
+                            'enableHiddenPlanHashCode'
+                        ),
+                    }}
+                    // serviceResponseHandler={this.handleJoinPageServiceResponse}
+                    render={({ fetchingContent, serviceResponse: joinPageRes }) => (
+                        <div className="check-out-stu">                            
+                            <Spinner loading={fetchingContent} />
+                            {!fetchingContent && (
+                                <Container>
+                                    <TitleHeader
+                                        heading="Join the Slooh Universe"
+                                        subHeading="Complete your order"
+                                    />
+                                    <Row>
+                                        <Col md="4">
+                                            <CheckoutPlan 
+                                                supscriptionResponse={joinPageRes}
+                                            />
+                                        </Col>
+                                        <Col md="8">
+                                            <Accordion />
+                                        </Col>
 
-                        </Row>
-                    </Container>
+                                    </Row>
+                                </Container>
+                            )}
+                        </div>
+                    )}/>
 
-                </div>
+                
             </>
         );
     }
