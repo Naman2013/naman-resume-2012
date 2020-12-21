e5c6e8b1072d69f02352487cd07fe2706b8a1d91
arpit <arpit@slooh.com>
2020-12-22 00:38:39 +0530

    replacing the image url

--- app/components/GlobalNavigation/Menus/SloohGiftCard/SloohGiftCard.js
+++ app/components/GlobalNavigation/Menus/SloohGiftCard/SloohGiftCard.js
@@ -8,7 +8,7 @@ const SloohGiftCard = () => (
       <h2>Slooh Gift Cards are now <br></br>available!</h2>
       <h4>Click the link below and give the gift of the universe</h4>
       <a href="https://www.amazon.com/Slooh-Apprentice-Membership/dp/B01MDNJXIR/ref=sr_1_1?dchild=1&keywords=slooh+apprentice+membership&qid=1606755292&sr=8-1" target="_blank">
-        <img src="https://vega.slooh.com/assets/v4/dashboard-new/guest-dashboard/gift-of-the-universe.png" alt="" />
+        <img src="https://vega.slooh.com/assets/v4/dashboard-new/guest-dashboard/gift-of-the-universe.jpg" alt="" />
       </a>
     </div>
     
