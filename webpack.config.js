0d34e44c172f12c7aa36f9c1ec3261d469c47add
senthilmnhr87@gmail.com <senthilmnhr87@gmail.com>
2021-01-29 01:33:10 +0530

    handled nested fields for dropdown on checkout page

--- webpack.config.js
+++ webpack.config.js
@@ -139,22 +139,22 @@ module.exports = {
     },
     proxy: {
       '/getHosted*.php': {
-        target: 'https://zeta.slooh.com',
+        target: 'https://orion.slooh.com',
         changeOrigin: true,
         secure: true,
       },
       '/api/**': {
-        target: 'https://zeta.slooh.com',
+        target: 'https://orion.slooh.com',
         changeOrigin: true,
         secure: true,
       },
       '/sloohapp/**': {
-        target: 'https://zeta.slooh.com',
+        target: 'https://orion.slooh.com',
         changeOrigin: true,
         secure: true,
       },
       '/sse/**': {
-        target: 'https://zeta.slooh.com',
+        target: 'https://orion.slooh.com',
         changeOrigin: true,
         secure: true,
       },
