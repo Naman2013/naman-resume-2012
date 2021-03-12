d2709018e8b91150ba71d1671db956d00b264146
arpit <arpit@slooh.com>
2021-03-12 22:58:30 +0530

    show data acc. to tab change

--- app/modules/new-dashboard/components/community-exploration/index.js
+++ app/modules/new-dashboard/components/community-exploration/index.js
@@ -1,178 +1,202 @@
-import { Component, PureComponent } from 'react';
-import React from "react";
-import './style.css';
-import { ImageSlider } from '../image-slider';
-import { getCommunityExploration, getActivityFeed, getObservations } from '../../dashboardApi';
+import React, { Component, PureComponent } from 'react';
 
+import './style.css';
 import { getUserInfo } from 'app/modules/User';
+import { Spinner } from 'app/components/spinner/index';
+import { ImageSlider } from '../image-slider';
+import {
+  getCommunityExploration,
+  getActivityFeed,
+  getObservations,
+} from '../../dashboardApi';
+
 import { RecentCommunityActivities } from '../recent-community-activities';
 import { SectionDivider } from '../section-divider';
 import { TabHeader } from '../tab-header';
-import { Spinner } from 'app/components/spinner/index';
-
-
-
-
-
 
 export class CommunityExploration extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      communityExploration: undefined,
+      activeHeading: 'Featured',
+      ActivitiesFeed: '',
+      isFetching: false,
+      communityExplorationAll: '',
+    };
+    this.getActivityFeedList();
+    this.getObservationsList('alltime');
+    this.getObservationsList('featured');
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            communityExploration: undefined,
-            activeHeading: "Featured",
-            ActivitiesFeed: '',
-            isFetching: false,
-            observationData:''
-        }
-        this.getActivityFeedList();
-        this.getObservationsList('featured');
-        // this.getCommunityObservationAction();
-
-    }
-
-    componentWillMount() {
-
-         this.getActivityFeedList();
-        this.getObservationsList('featured');
-    }
+    // this.getCommunityObservationAction();
+  }
 
   /*   componentDidMount() {
-        this.getObservationsList('featured');
-        this.getActivityFeedList();
-    } */
+            this.getObservationsList('featured');
+            this.getActivityFeedList();
+        } */
 
+  timerId = null;
 
+  /*  getCommunityObservationAction = () => {
+           const { at, cid, token } = getUserInfo();
+           getCommunityExploration({ at, cid, token, }).then(response => {
+               const res = response.data;
+               if (!res.apiError) {
+                   const { timestamp, expires } = res;
+                   const duration = (expires - timestamp) * 1000;
+                   console.log("Community Exploration Duration" + duration);
+                   if (this.timerId !== null)
+                       clearTimeout(this.timerId);
+                   this.timerId = setTimeout(this.getCommunityObservationAction, duration);
+                   this.setState({ communityExploration: res });
+               }
+               else
+                   this.props.validateResponseAccess(res);
+           });
+       } */
 
-    timerId = null;
+  getObservationsList = viewType => {
+    this.setState({ isFetching: true });
+    const { at, cid, token } = getUserInfo();
+    getObservations({ at, cid, token, viewType }).then(response => {
+      const res = response.data;
+      if (!res.apiError) {
+        const { timestamp, expires } = res;
+        const duration = (expires - timestamp) * 1000;
+        console.log(`Community Exploration Duration${duration}`);
+        if (this.timerId !== null) {
+          clearTimeout(this.timerId);
+        }
+        if (viewType === 'alltime') {
+          this.setState({ communityExploration: res, isFetching: false });
+        } else {
+          this.setState({ isFetching: false, communityExplorationAll: res });
+        }
+      }
+      const { validateResponseAccess } = this.props;
+      validateResponseAccess(res);
+    });
+  };
 
-   /*  getCommunityObservationAction = () => {
-        const { at, cid, token } = getUserInfo();
-        getCommunityExploration({ at, cid, token, }).then(response => {
-            const res = response.data;
-            if (!res.apiError) {
-                const { timestamp, expires } = res;
-                const duration = (expires - timestamp) * 1000;
-                console.log("Community Exploration Duration" + duration);
-                if (this.timerId !== null)
-                    clearTimeout(this.timerId);
-                this.timerId = setTimeout(this.getCommunityObservationAction, duration);
-                this.setState({ communityExploration: res });
-            }
-            else
-                this.props.validateResponseAccess(res);
-        });
-    } */
+  getActivityFeedList = () => {
+    this.setState({ isFetching: true });
+    const { at, cid, token } = getUserInfo();
+    getActivityFeed({ at, cid, token }).then(response => {
+      const res = response.data;
+      if (!res.apiError) {
+        const { timestamp, expires } = res;
+        const duration = (expires - timestamp) * 1000;
+        console.log(`CommunityFeed Exploration Duration${duration}`);
+        if (this.timerId !== null) {
+          clearTimeout(this.timerId);
+        }
+        this.setState({ ActivitiesFeed: res, isFetching: false });
+      }
+      const { validateResponseAccess } = this.props;
+      validateResponseAccess(res);
+    });
+  };
 
-
-    getObservationsList = (viewType) => {
-        this.setState({ isFetching:true })
-        const { at, cid, token } = getUserInfo();
-        getObservations({ at, cid, token, viewType }).then(response => {
-            const res = response.data;
-            if (!res.apiError) {
-                const { timestamp, expires } = res;
-                const duration = (expires - timestamp) * 1000;
-                console.log("Community Exploration Duration" + duration);
-                if (this.timerId !== null)
-                    clearTimeout(this.timerId);
-                this.setState({ communityExploration: res,isFetching:false });
-            }
-            this.props.validateResponseAccess(res);
-
-        })
+  componentWillUnmount() {
+    if (this.timerId !== null) {
+      clearTimeout(this.timerId);
     }
+  }
 
-    getActivityFeedList = () => {
-        this.setState({ isFetching:true })
-        const { at, cid, token } = getUserInfo();
-        getActivityFeed({ at, cid, token }).then(response => {
-            const res = response.data;
-            if (!res.apiError) {
-                
-                this.setState({ ActivitiesFeed: res, isFetching:false });
-            }
-            this.props.validateResponseAccess(res);
-        })
+  startTimer = () => {
+    this.getCommunityObservationAction();
+  };
+
+  stopTimer = () => {
+    if (this.timerId !== null) {
+      clearTimeout(this.timerId);
     }
+  };
 
-    componentWillUnmount() {
-        if (this.timerId !== null)
-            clearTimeout(this.timerId);
-    }
+  onTabChange = title => {
+    let viewType = title === 'All' ? 'alltime' : 'featured';
+    this.getObservationsList(viewType);
+    this.setState({ activeHeading: title });
+  };
 
-    startTimer = () => {
-        this.getCommunityObservationAction();
-    }
+  render() {
+    const {
+      communityExploration,
+      activeHeading,
+      ActivitiesFeed,
+      isFetching,
+      communityExplorationAll,
+    } = this.state;
+    const { onClickItem, scrollToRef, validateResponseAccess } = this.props;
+    let feedList = ActivitiesFeed.activitiesList
+      ? ActivitiesFeed.activitiesList
+      : '';
 
-    stopTimer = () => {
-        if (this.timerId !== null)
-            clearTimeout(this.timerId);
-    }
+    console.log('nnnnnnn', activeHeading);
 
-
-
-
-
-    onTabChange = (title) => {
-
-        let viewType = title == 'All' ? 'alltime' : 'featured';
-        this.getObservationsList(viewType);
-        this.setState({ activeHeading: title });
-    };
-
-    render() {
-        const { communityExploration, activeHeading, ActivitiesFeed,isFetching} = this.state;
-        const { onClickItem, scrollToRef, validateResponseAccess } = this.props;
-        let feedList = ActivitiesFeed.activitiesList ? ActivitiesFeed.activitiesList: '';
-
-        
-        
-        return (
-            <div className="explore-main">
-                <h2 className="photo-hub-heading">{"Community"}</h2>
-                {communityExploration && (
-                    <div>
-                        {/* <br/> */}
-                        {communityExploration.featuredObservations.sectionHeading && (
-                            <h2 className="recent-heading">{communityExploration.featuredObservations.sectionHeading}</h2>
-                        )}
-                        <TabHeader
-                            headings={["Featured", "All"]}
-                            activeHeading={activeHeading}
-                            spaceequally={false}
-                            theme={"dark"}
-                            onTabChange={this.onTabChange}
-                            
-
-                        />
-                        {communityExploration.featuredObservations.sectionSubHeading && (
-                            <h4 className="title-subHeading">{communityExploration.featuredObservations.sectionSubHeading}</h4>
-                        )}
-                        <Spinner
-                                loading={isFetching}
-                                text="Please wait...loading discussions"
-                        />
-                        { communityExploration &&
-                        <ImageSlider
-                            communityExploration={communityExploration}
-                            onClickItem={onClickItem}
-                            scrollToRef={scrollToRef}
-                            startTimer={this.startTimer}
-                            stopTimer={this.stopTimer}
-                            validateResponseAccess={validateResponseAccess}
-                        />
-                        }
-                        <SectionDivider />
-                        {ActivitiesFeed.activitiesList && <RecentCommunityActivities
-                            heading={"Recent Community Activities"}
-                            activities={ActivitiesFeed}
-                            onClickItem={onClickItem}
-                        />}
-                    </div>
-                )}
-            </div>
-        );
-    }
+    return (
+      <div className="explore-main">
+        <h2 className="photo-hub-heading">Community</h2>
+        {communityExploration && (
+          <div>
+            {/* <br/> */}
+            {communityExploration.featuredObservations.sectionHeading && (
+              <h2 className="recent-heading">
+                {communityExploration.featuredObservations.sectionHeading}
+              </h2>
+            )}
+            <TabHeader
+              headings={['Featured', 'All']}
+              activeHeading={activeHeading}
+              spaceequally={false}
+              theme="dark"
+              onTabChange={this.onTabChange}
+            />
+            {communityExploration.featuredObservations.sectionSubHeading && (
+              <h4 className="title-subHeading">
+                {communityExploration.featuredObservations.sectionSubHeading}
+              </h4>
+            )}
+            <Spinner
+              loading={isFetching}
+              text="Please wait...loading discussions"
+            />
+            {communityExplorationAll && activeHeading === 'Featured' ? (
+              <ImageSlider
+                communityExploration={communityExplorationAll}
+                onClickItem={onClickItem}
+                scrollToRef={scrollToRef}
+                startTimer={this.startTimer}
+                stopTimer={this.stopTimer}
+                validateResponseAccess={validateResponseAccess}
+              />
+            ) : (
+              ''
+            )}
+            {communityExploration && activeHeading === 'All' ? (
+              <ImageSlider
+                communityExploration={communityExploration}
+                onClickItem={onClickItem}
+                scrollToRef={scrollToRef}
+                startTimer={this.startTimer}
+                stopTimer={this.stopTimer}
+                validateResponseAccess={validateResponseAccess}
+              />
+            ) : (
+              ''
+            )}
+            <SectionDivider />
+            {ActivitiesFeed.activitiesList && (
+              <RecentCommunityActivities
+                heading="Recent Community Activities"
+                activities={ActivitiesFeed}
+                onClickItem={onClickItem}
+              />
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
